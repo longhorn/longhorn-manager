9fe6498170907d09b6bc4cdfedd71b7839826e1a
Sheng Yang <sheng@yasker.org>
2017-11-26 11:25:47 -0800

    manager: Fix a bug that the replicas doesn't show up in the controller list will be killed
    Even it's in the creation process and we haven't add it to the controller yet.
    
    All the replicas doesn't show up in the controller list yet is assumed as a new
    replica now.

--- manager/process.go
+++ manager/process.go
@@ -234,6 +234,7 @@ func (v *ManagedVolume) syncWithEngineState(engineReps map[string]*engineapi.Rep
 				rebuildingReplicaCount++
 			} else {
 				// means engineRep.Mode == engineapi.ReplicaModeERR
+				badReplicas[addr2Replica[addr].Name] = struct{}{}
 				continue
 			}
 			if addr2Replica[addr] == nil {
@@ -241,10 +242,9 @@ func (v *ManagedVolume) syncWithEngineState(engineReps map[string]*engineapi.Rep
 			}
 			delete(addr2Replica, addr)
 		}
-		// those replicas doesn't show up in controller as WO or RW
-		for _, replica := range addr2Replica {
-			badReplicas[replica.Name] = struct{}{}
-		}
+		// those replicas doesn't show up in controller as WO or RW,
+		// assuming there are rebuilding
+		rebuildingReplicaCount += len(addr2Replica)
 	}
 
 	state := v.State
