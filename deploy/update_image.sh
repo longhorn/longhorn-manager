564b2beb8e216c7de8876414f49a74fcc336fdc2
Sheng Yang <sheng@yasker.org>
2018-08-10 20:52:06 -0700

    deploy: Add support for updating engine and ui image to update_image.sh

--- deploy/update_image.sh
+++ deploy/update_image.sh
@@ -2,17 +2,60 @@
 
 set -e
 
+while [[ $# -gt 0 ]]
+do
+	key="$1"
+	case $key in
+		-e|--engine)
+			engine="$2"
+			shift
+			shift
+			;;
+		-u|--ui)
+			ui="$2"
+			shift
+			shift
+			;;
+		*)
+			echo Unknown parameter $1
+			exit 1
+			;;
+	esac
+done
+
 base="${GOPATH}/src/github.com/rancher/longhorn-manager"
 files=`find ${base}/deploy/ |grep yaml |sort`
+
 project="rancher\/longhorn-manager"
-
 latest=`cat ${base}/bin/latest_image`
-echo latest image ${latest}
+echo latest manager image ${latest}
 
-escaped_latest=${latest//\//\\\/}
+escaped_image=${latest//\//\\\/}
 
 for f in $files
 do
-	sed -i "s/image\:\ ${project}:.*/image\:\ ${escaped_latest}/g" $f
-	sed -i "s/-\ ${project}:.*/-\ ${escaped_latest}/g" $f
+	sed -i "s/image\:\ ${project}:.*/image\:\ ${escaped_image}/g" $f
+	sed -i "s/-\ ${project}:.*/-\ ${escaped_image}/g" $f
 done
+
+if [ -n "$engine" ]; then
+	project="rancher\/longhorn-engine"
+	echo engine image $engine
+	escaped_image=${engine//\//\\\/}
+
+	for f in $files
+	do
+		sed -i "s/-\ ${project}:.*/-\ ${escaped_image}/g" $f
+	done
+fi
+
+if [ -n "$ui" ]; then
+	project="rancher\/longhorn-ui"
+	echo ui image $ui
+	escaped_image=${ui//\//\\\/}
+
+	for f in $files
+	do
+		sed -i "s/image\:\ ${project}:.*/image\:\ ${escaped_image}/g" $f
+	done
+fi
