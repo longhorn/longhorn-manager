f693bfc3fb532b2a1279b38db7b85329e2172fce
JacieChao <kathyyy@163.com>
2018-08-13 11:05:34 +0800

    API: Validate user input for disks and settings

--- manager/setting.go
+++ manager/setting.go
@@ -2,10 +2,13 @@ package manager
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
 	"github.com/rancher/longhorn-manager/types"
+	"github.com/rancher/longhorn-manager/util"
 
 	longhorn "github.com/rancher/longhorn-manager/k8s/pkg/apis/longhorn/v1alpha1"
 )
@@ -47,6 +50,10 @@ func (m *VolumeManager) ListSettingsSorted() ([]*longhorn.Setting, error) {
 }
 
 func (m *VolumeManager) CreateOrUpdateSetting(s *longhorn.Setting) (*longhorn.Setting, error) {
+	err := m.SettingValidation(s.Name, s.Value)
+	if err != nil {
+		return nil, err
+	}
 	setting, err := m.ds.UpdateSetting(s)
 	if err != nil {
 		if apierrors.IsNotFound(err) {
@@ -55,4 +62,32 @@ func (m *VolumeManager) CreateOrUpdateSetting(s *longhorn.Setting) (*longhorn.Se
 		return nil, err
 	}
 	return setting, nil
+}
+
+func (m *VolumeManager) SettingValidation(name, value string) error {
+	sName := types.SettingName(name)
+
+	switch sName {
+	case types.SettingNameBackupTarget:
+		// additional check whether have $ or , have been set in BackupTarget
+		regStr := `[\$\,]`
+		reg := regexp.MustCompile(regStr)
+		findStr := reg.FindAllString(value, -1)
+		if len(findStr) != 0 {
+			return fmt.Errorf("fail to set settings with invalid BackupTarget %s, contains %v", value, strings.Join(findStr, " or "))
+		}
+	case types.SettingNameStorageOverProvisioningPercentage:
+		// additional check whether over provisioning percentage is positive
+		value, err := util.ConvertSize(value)
+		if err != nil || value < 0 {
+			return fmt.Errorf("fail to set settings with invalid StorageOverProvisioningPercentag %v, value should be positive", value)
+		}
+	case types.SettingNameStorageMinimalAvailablePercentage:
+		// additional check whether minimal available percentage is between 0 to 100
+		value, err := util.ConvertSize(value)
+		if err != nil || value < 0 || value > 100 {
+			return fmt.Errorf("fail to set settings with invalid StorageMinimalAvailablePercentage %v, value should between 0 to 100", value)
+		}
+	}
+	return nil
 }
