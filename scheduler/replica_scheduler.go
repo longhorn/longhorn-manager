1e1b911e3ab4c41a075a82785d2afd7dd930293c
JacieChao <kathyyy@163.com>
2018-08-23 06:42:34 +0800

    controller: Tweak scheduling algorithm

--- scheduler/replica_scheduler.go
+++ scheduler/replica_scheduler.go
@@ -165,7 +165,8 @@ func (rcs *ReplicaScheduler) scheduleReplicaToDisk(replica *longhorn.Replica, di
 func (rcs *ReplicaScheduler) IsSchedulableToDisk(size int64, info *DiskSchedulingInfo) bool {
 	return info.StorageMaximum > 0 && info.StorageAvailable > 0 &&
 		(size+info.StorageScheduled) <= (info.StorageMaximum-info.StorageReserved)*(info.OverProvisioningPercentage/100) &&
-		size <= (info.StorageAvailable-info.StorageMaximum*info.MinimalAvailablePercentage/100)*info.OverProvisioningPercentage/100
+		info.StorageAvailable > info.StorageMaximum*info.MinimalAvailablePercentage/100 &&
+		info.StorageAvailable > info.StorageReserved
 }
 
 func (rcs *ReplicaScheduler) GetDiskSchedulingInfo(disk types.DiskSpec, diskStatus types.DiskStatus) (*DiskSchedulingInfo, error) {
