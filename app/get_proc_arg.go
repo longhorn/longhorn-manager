c636351bca92a0d3c655bf7b2f9c57fe0c0ec167
Shuo Wu <shuo.wu.career@gmail.com>
2019-03-26 22:54:24 +0000

    app: refactored get_proc_arg and fixed bug for detecting proc k3s

--- app/get_proc_arg.go
+++ app/get_proc_arg.go
@@ -23,162 +23,169 @@ const (
 	ArgFlexvolumePluginDir = "volume-plugin-dir"
 	ArgKubeletRootDir      = "root-dir"
 
+	ArgNameFlexvolumePluginDir = "--volume-plugin-dir"
+	ArgNameKubeletRootDir      = "--root-dir"
+	ArgNameK3SDataDir          = "--data-dir"
+	ArgShortNameK3SDataDir     = "-d"
+
 	DefaultFlexvolumeDir  = "/usr/libexec/kubernetes/kubelet-plugins/volume/exec/"
 	DefaultKubeletRootDir = "/var/lib/kubelet"
+	DefaultK3SDataDir     = "/var/lib/rancher/k3s"
 
-	DetectFlexvolumeName     = "discover-flexvolume-dir"
-	DetectKubeletRootDirName = "discover-kubelet-root-dir"
+	KubeletDetectionPodName = "discover-proc-kubelet-cmdline"
+	K3SDetectionPodName     = "discover-proc-k3s-cmdline"
 
-	DetectKubeletArgScript = `
-    find_kubelet_proc() {
+	GetKubeletCmdlineScript = `
+    find_kubelet_cmdline() {
       for proc in $(find /proc -maxdepth 1 -type d 2>/dev/null); do
         if [ ! -f $proc/cmdline ]; then
           continue
         fi
         if [[ "$(cat $proc/cmdline | tr '\000' '\n' | head -n1 | tr '/' '\n' | tail -n1)" == "kubelet" ]]; then
-          echo $proc
+          echo "Proc found: kubelet"
+          cat $proc/cmdline
+          return
         fi
       done
+      echo "Proc not found: kubelet"
     }
-    get_kubelet_arg_dir() {
-      proc=$(find_kubelet_proc)
-      if [ "$proc" != "" ]; then
-        path=$(cat $proc/cmdline | tr '\000' '\n' | grep %s | tr '=' '\n' | tail -n1)
-        if [ "$path" == "" ]; then
-          echo "No arg root-dir in proc kubelet. Will use default root-dir."
-          echo %s
-        else
-          echo $path
-        fi
-      fi
-    }
-	get_kubelet_arg_dir
+    find_kubelet_cmdline
 	`
-	DetectK3SDataDirScript = `
-    find_k3s_proc() {
+	GetK3SCmdlineScript = `
+    find_k3s_cmdline() {
       for proc in $(find /proc -maxdepth 1 -type d 2>/dev/null); do
         if [ ! -f $proc/cmdline ]; then
           continue
         fi
-        if [[ $(cat $proc/cmdline | tr '\000' '\n' | sed -n '1p') == "k3s" ]]; then
+        if [[ "$(cat $proc/cmdline | tr '\000' '\n' | head -n1 | tr '/' '\n' | tail -n1)" == "k3s" ]]; then
           proc_name=$(cat $proc/cmdline | tr '\000' '\n' | sed -n '2p')
           if [[ "$proc_name"  == "server" || "$proc_name" == "agent" ]]; then
-            echo $proc
+            echo "Proc found: k3s"
+            cat $proc/cmdline
+            return
           fi
         fi
       done
+      echo "Proc not found: k3s"
     }
-    get_k3s_data_dir() {
-      proc=$(find_k3s_proc)
-      if [ "$proc" != "" ]; then
-        path=$(cat $proc//cmdline | tr '\000' '\n' |  grep 'data-dir' -A 1 | tail -n1)
-        if [ "$path" == "" ]; then
-          path=$(cat $proc//cmdline | tr '\000' '\n' |  grep '\^-d$' -A 1 | tail -n1)
-        fi
-        if [ "$path" == "" ]; then
-          echo "No arg data-dir in proc k3s. Will use default data-dir."
-          echo '/var/lib/rancher/k3s'
-        else
-          echo $path
-        fi
-      fi
-    }
-    get_k3s_data_dir
+    find_k3s_cmdline
 	`
 )
 
 func getProcArg(kubeClient *clientset.Clientset, managerImage, name string) (string, error) {
 	switch name {
 	case ArgFlexvolumePluginDir:
-		dir, err := discoverFlexvolumeDir(kubeClient, managerImage)
+		dir, err := detectFlexvolumeDir(kubeClient, managerImage)
 		if err != nil {
 			return "", errors.Wrap(err, `failed to get arg volume-plugin-dir. Need to specify "--flexvolume-dir" in your Longhorn deployment yaml.`)
 		}
 		return dir, nil
 	case ArgKubeletRootDir:
-		dir, err := discoverKubeletRootDir(kubeClient, managerImage)
+		dir, err := detectKubeletRootDir(kubeClient, managerImage)
 		if err != nil {
 			return "", errors.Wrap(err, `failed to get arg root-dir. Need to specify "--kubelet-root-dir" in your Longhorn deployment yaml.`)
 		}
 		return dir, nil
 	}
-	return "", fmt.Errorf("Getting arg %v is not supported", name)
+	return "", fmt.Errorf("getting arg %v is not supported", name)
 }
 
-func discoverFlexvolumeDir(kubeClient *clientset.Clientset, managerImage string) (string, error) {
-	detectFlexVolumeScript := fmt.Sprintf(DetectKubeletArgScript, ArgFlexvolumePluginDir, DefaultFlexvolumeDir)
-	dir, err := discoverProcArg(kubeClient, managerImage, DetectFlexvolumeName, detectFlexVolumeScript)
+func detectFlexvolumeDir(kubeClient *clientset.Clientset, managerImage string) (string, error) {
+	kubeletCmdline, err := getProcCmdline(kubeClient, managerImage, KubeletDetectionPodName, GetKubeletCmdlineScript)
 	if err != nil {
-		return "", errors.Wrap(err, "Failed to get arg volume-plugin-dir in proc kubelet")
+		return "", errors.Wrap(err, "failed to get cmdline of proc kubelet")
+	}
+	if kubeletCmdline == "" {
+		return "", fmt.Errorf("failed to get flexvolume dir, no related proc for volume-plugin-dir detection, error out")
+	}
+	dir, err := getArgFromCmdline(kubeletCmdline, ArgNameFlexvolumePluginDir)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get arg volume-plugin-dir in cmdline of proc kubelet")
 	}
 	if dir == "" {
-		return "", fmt.Errorf("Cannot get flexvolume dir, no related proc for volume-plugin-dir detection, error out")
+		logrus.Warnf(`Cmdline of proc kubelet found: "%s". But arg "%s" not found. Hence default value will be used: "%s"`, kubeletCmdline, ArgNameFlexvolumePluginDir, DefaultFlexvolumeDir)
+		dir = DefaultFlexvolumeDir
 	}
 	return dir, nil
 }
 
-func discoverKubeletRootDir(kubeClient *clientset.Clientset, managerImage string) (string, error) {
+func detectKubeletRootDir(kubeClient *clientset.Clientset, managerImage string) (string, error) {
 	// try to detect root-dir in proc kubelet
-	detectKubeletRootDirScript := fmt.Sprintf(DetectKubeletArgScript, ArgKubeletRootDir, DefaultKubeletRootDir)
-	rootDir, err := discoverProcArg(kubeClient, managerImage, DetectKubeletRootDirName, detectKubeletRootDirScript)
+	kubeletCmdline, err := getProcCmdline(kubeClient, managerImage, KubeletDetectionPodName, GetKubeletCmdlineScript)
 	if err != nil {
-		return "", errors.Wrap(err, "Failed to get arg root-dir in proc kubelet")
+		return "", errors.Wrap(err, "failed to get cmdline of proc kubelet")
 	}
-	// no proc kubelet. then try to detect root-dir in proc k3s
-	if rootDir == "" {
-		rootDir, err = discoverProcArg(kubeClient, managerImage, DetectKubeletRootDirName, DetectK3SDataDirScript)
+	// proc kubelet exists.
+	if kubeletCmdline != "" {
+		rootDir, err := getArgFromCmdline(kubeletCmdline, ArgNameKubeletRootDir)
 		if err != nil {
-			return "", errors.Wrap(err, "Failed to get arg root-dir in proc k3s")
+			return "", errors.Wrap(err, "failed to get arg root-dir in cmdline of proc kubelet")
 		}
-		// proc k3s exists.
-		if rootDir != "" {
-			rootDir = filepath.Join(rootDir, "/agent/kubelet")
+		if rootDir == "" {
+			logrus.Warnf(`Cmdline of proc kubelet found: "%s". But arg "%s" not found. Hence default value will be used: "%s"`, kubeletCmdline, ArgNameKubeletRootDir, DefaultKubeletRootDir)
+			rootDir = DefaultKubeletRootDir
 		}
+		return rootDir, nil
 	}
-	// nothing found. error out
-	if rootDir == "" {
-		return "", fmt.Errorf("Cannot get kubelet root dir, no related proc for root-dir detection, error out")
+	// no proc kubelet. then try to detect data-dir and get root-dir in proc k3s
+	k3sCmdline, err := getProcCmdline(kubeClient, managerImage, K3SDetectionPodName, GetK3SCmdlineScript)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get cmdline of proc k3s")
 	}
-	return rootDir, nil
+	// proc k3s exists.
+	if k3sCmdline != "" {
+		dataDir, err := getArgFromCmdline(k3sCmdline, ArgNameK3SDataDir, ArgShortNameK3SDataDir)
+		if err != nil {
+			return "", errors.Wrap(err, "failed to get arg data-dir in cmdline of proc k3s")
+		}
+		if dataDir == "" {
+			logrus.Warnf(`Cmdline of proc k3s found: "%s". But arg "%s" or "%s" not found. Hence default value will be used: "%s"`, k3sCmdline, ArgNameK3SDataDir, ArgShortNameK3SDataDir, DefaultK3SDataDir)
+			dataDir = DefaultK3SDataDir
+		}
+		rootDir := filepath.Join(dataDir, "/agent/kubelet")
+		return rootDir, nil
+	}
+	// no related proc found. error out
+	return "", fmt.Errorf("failed to get kubelet root dir, no related proc for root-dir detection, error out")
 }
 
-func discoverProcArg(kubeClient *clientset.Clientset, managerImage, name, script string) (string, error) {
+func getProcCmdline(kubeClient *clientset.Clientset, managerImage, name, script string) (string, error) {
 	namespace := os.Getenv(types.EnvPodNamespace)
 	if namespace == "" {
-		return "", fmt.Errorf("Cannot detect pod namespace, environment variable %v is missing", types.EnvPodNamespace)
+		return "", fmt.Errorf("failed to detect pod namespace, environment variable %v is missing", types.EnvPodNamespace)
 	}
 
 	if err := deployDetectionPod(kubeClient, namespace, managerImage, name, script); err != nil {
-		return "", errors.Wrapf(err, "Failed to deploy kubelet arg detection pod %v", name)
+		return "", errors.Wrapf(err, "failed to deploy proc cmdline detection pod %v", name)
 	}
 
 	defer func() {
 		if err := kubeClient.CoreV1().Pods(namespace).Delete(name, &metav1.DeleteOptions{}); err != nil {
-			logrus.Warnf("Failed to delete kubelet arg detection pod %v: %v", name, err)
+			logrus.Warnf("failed to delete proc cmdline detection pod %v: %v", name, err)
 		}
 	}()
 
 	completed := false
 	for i := 0; i < DetectPodMaxPolls; i++ {
 		if pod, err := kubeClient.CoreV1().Pods(namespace).Get(name, metav1.GetOptions{}); err != nil {
-			logrus.Warnf("Failed to get kubelet arg detection pod %v: %v", name, err)
+			logrus.Warnf("failed to get proc cmdline detection pod %v: %v", name, err)
 		} else if pod.Status.Phase == v1.PodSucceeded {
 			completed = true
 			break
 		} else {
-			logrus.Debugf("kubelet arg detection pod %v in phase: %v", name, pod.Status.Phase)
+			logrus.Debugf("proc cmdline detection pod %v in phase: %v", name, pod.Status.Phase)
 		}
 		time.Sleep(1 * time.Second)
 	}
 	if !completed {
-		return "", fmt.Errorf("Kubelet arg detection pod %v didn't complete within %d seconds", name, DetectPodMaxPolls)
+		return "", fmt.Errorf("proc cmdline detection pod %v didn't complete within %d seconds", name, DetectPodMaxPolls)
 	}
 
-	kubeletArg, err := getPodLogAsString(kubeClient, namespace, name)
+	procArg, err := getPodLogAsString(kubeClient, namespace, name)
 	if err != nil {
 		return "", err
 	}
-	return kubeletArg, nil
+	return procArg, nil
 }
 
 func deployDetectionPod(kubeClient *clientset.Clientset, namespace, managerImage, name, script string) error {
@@ -216,10 +223,44 @@ func getPodLogAsString(kubeClient *clientset.Clientset, namespace, name string) 
 	if err != nil {
 		return "", err
 	}
-	logsRes := strings.Split(string(logs), "\n")
-	if strings.HasPrefix(logsRes[0], "No arg") {
-		logrus.Warnf("%s If longhorn driver doesn't work, you may want to try to set argument manually.", logsRes[0])
-		return logsRes[1], nil
+	strLogs := strings.Split(string(logs), "\n")
+	// proc not found
+	if strings.HasPrefix(strLogs[0], "Proc not found") {
+		logrus.Warn(strLogs[0])
+		return "", nil
+	} else if !strings.HasPrefix(strLogs[0], "Proc found") {
+		// unexpected result
+		return "", fmt.Errorf(string(logs))
 	}
-	return logsRes[0], nil
+	logrus.Info(strLogs[0])
+	return strLogs[1], nil
+}
+
+func getArgFromCmdline(cmdline string, argNames ...string) (string, error) {
+	cmdList := strings.Split(cmdline, "\000")
+	logrus.Infof("Try to find arg %v in cmdline: %v", argNames, cmdList)
+	var arg string
+	for idx, cmdStr := range cmdList {
+		for _, argName := range argNames {
+			if strings.HasPrefix(cmdStr, argName) {
+				if cmdStr == argName {
+					// the separator between <argName> and <argValue> is " "
+					if idx < len(cmdList)-1 {
+						arg = cmdList[idx+1]
+					}
+				} else if strings.Contains(cmdStr, "=") {
+					// the separator between <argName> and <argValue> is "="
+					argNameAndValue := strings.Split(cmdStr, "=")
+					// the splitting result should be [<argName> <argValue>]
+					if len(argNameAndValue) == 2 {
+						arg = argNameAndValue[1]
+					} else {
+						return "", fmt.Errorf("unexpected argument: %s", cmdStr)
+					}
+				}
+				return arg, nil
+			}
+		}
+	}
+	return "", nil
 }
