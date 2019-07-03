4120deb3e761a22415b5ccd3bfcc9c2926182f79
Michael William Le Nguyen <michael@mail.ttp.codes>
2019-07-02 18:00:08 -0700

    Add Instance Manager CRD

--- k8s/pkg/apis/longhorn/v1alpha1/register.go
+++ k8s/pkg/apis/longhorn/v1alpha1/register.go
@@ -33,6 +33,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&EngineImageList{},
 		&Node{},
 		&NodeList{},
+		&InstanceManager{},
+		&InstanceManagerList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
