5b263605195f06df32049a48fbb5dcbd42836a0f
Michael William Le Nguyen <michael@mail.ttp.codes>
2019-07-08 15:15:01 -0700

    Add unit test for Instance Manager Controller

--- controller/controller_test.go
+++ controller/controller_test.go
@@ -29,6 +29,10 @@ const (
 	TestManagerImage   = "longhorn-manager:latest"
 	TestServiceAccount = "longhorn-service-account"
 
+	TestEngineImageName = "ei-test"
+
+	TestInstanceManagerName = "instance-manager-engine-image-name"
+
 	TestReplica1Name = "replica-volumename-1"
 	TestReplica2Name = "replica-volumename-2"
 
