c1c14ad442ace4a80b1daef4d64fb2f9473f2d3f
Shreesha Rao <shreesha@rancher.com>
2019-07-22 22:08:09 +0000

    Engine Status refreshing RestoreStatus
    The following code changes stores the Restore Status when engine monitor
    fetches it every 5 seconds.

--- controller/engine_controller.go
+++ controller/engine_controller.go
@@ -685,6 +685,7 @@ func (m *EngineMonitor) refresh(engine *longhorn.Engine) error {
 		logrus.Errorf("failed to get Backup Restore Status: %v", err)
 		return err
 	}
+	engine.Status.RestoreStatus = rsMap
 
 	isRestoring := false
 	lastRestored := ""
