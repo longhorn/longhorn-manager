8e9d4cf8bcf84037de9eb15ba0d9a146a4968f58
Sheng Yang <sheng@yasker.org>
2019-07-30 13:25:50 -0700

    Fix failed recurring job test cases
    Missing the engine port number after the refactoring.

--- engineapi/engine.go
+++ engineapi/engine.go
@@ -29,6 +29,9 @@ func (c *EngineCollection) NewEngineClient(request *EngineClientRequest) (Engine
 	if request.EngineImage == "" {
 		return nil, fmt.Errorf("Invalid empty engine image from request")
 	}
+	if request.IP == "" || request.Port == 0 {
+		return nil, fmt.Errorf("Invalid empty IP or port from request")
+	}
 	return &Engine{
 		name:  request.VolumeName,
 		image: request.EngineImage,
