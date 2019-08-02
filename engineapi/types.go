c357bd290f20a965bec6f517d04101aefdd6e250
Sheng Yang <sheng@yasker.org>
2019-08-01 23:19:35 -0700

    Revert "Add a flag Deleting to backup object"
    This reverts commit f06134ec2efa80cbc541f2a65999666c77add6bd.
    
    Cannot pass engine integration test

--- engineapi/types.go
+++ engineapi/types.go
@@ -113,7 +113,6 @@ type Backup struct {
 	VolumeName      string            `json:"volumeName"`
 	VolumeSize      string            `json:"volumeSize"`
 	VolumeCreated   string            `json:"volumeCreated"`
-	Deleting        bool              `json:"deleting"`
 }
 
 type LauncherVolumeInfo struct {
