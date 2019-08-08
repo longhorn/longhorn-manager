82969be7dd32c97bdaf43b09c087852c76d9e149
Shuo Wu <shuo.wu.career@gmail.com>
2019-08-07 17:19:06 -0700

    controller: Add monitor in instance manager controller
    Now instance manager controller (imc) will only use actively polled results
    rather than the streaming result to update the instance map of instance
    manager (im).
    
    In detail, imc will start a monitor for each running im to update its
    instance map. And it will be updated when:
        1. 30 seconds passed since the last update. In other words, the max
           poll and update interval is 30 seconds.
        2. Received streaming notifications. The motinor will poll and update
           once even if multiple notificaitons are received in 1 second. In
           other words, the min poll and update interval is 1 second.

--- controller/instance_manager_controller.go
+++ controller/instance_manager_controller.go
@@ -24,7 +24,12 @@ import (
 	"k8s.io/kubernetes/pkg/controller"
 
 	"github.com/longhorn/longhorn-manager/datastore"
+	"github.com/longhorn/longhorn-manager/engineapi"
 	"github.com/longhorn/longhorn-manager/types"
+
+	"github.com/longhorn/longhorn-instance-manager/api"
+	"github.com/longhorn/longhorn-instance-manager/client"
+	imutil "github.com/longhorn/longhorn-instance-manager/util"
 
 	longhorn "github.com/longhorn/longhorn-manager/k8s/pkg/apis/longhorn/v1alpha1"
 	lhinformers "github.com/longhorn/longhorn-manager/k8s/pkg/client/informers/externalversions/longhorn/v1alpha1"
@@ -36,6 +41,10 @@ const (
 
 	managerLivenessProbeFailureThreshold  = 60
 	managerReadinessProbeFailureThreshold = 15
+
+	MaxPollCount = 30
+	MinPollCount = 1
+	PollInterval = 1 * time.Second
 )
 
 type InstanceManagerController struct {
@@ -51,6 +60,50 @@ type InstanceManagerController struct {
 	pStoreSynced  cache.InformerSynced
 
 	queue workqueue.RateLimitingInterface
+
+	instanceManagerMonitorMutex    *sync.RWMutex
+	instanceManagerMonitorMap      map[string]chan struct{}
+	instanceManagerMonitorRemoveCh chan string
+}
+
+type InstanceManagerMonitor struct {
+	Name         string
+	controllerID string
+
+	instanceManagerUpdater InstanceManagerUpdater
+	ds                     *datastore.DataStore
+	lock                   *sync.Mutex
+	updateNotification     bool
+	stopCh                 chan struct{}
+
+	// used to notify the controller that monitoring has stopped
+	monitoringRemoveCh chan string
+}
+
+type InstanceManagerUpdater interface {
+	GetNotifier() (InstanceManagerNotifier, error)
+	Poll() (map[string]types.InstanceProcess, error)
+}
+
+type InstanceManagerNotifier interface {
+	Recv() (struct{}, error)
+	Close()
+}
+
+type EngineManagerUpdater struct {
+	client *client.EngineManagerClient
+}
+
+type ReplicaManagerUpdater struct {
+	client *client.ProcessManagerClient
+}
+
+type EngineManagerNotifier struct {
+	stream *api.EngineStream
+}
+
+type ReplicaManagerNotifier struct {
+	stream *api.ProcessStream
 }
 
 func NewInstanceManagerController(
@@ -78,6 +131,10 @@ func NewInstanceManagerController(
 		pStoreSynced:  pInformer.Informer().HasSynced,
 
 		queue: workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "longhorn-instance-manager"),
+
+		instanceManagerMonitorMutex:    &sync.RWMutex{},
+		instanceManagerMonitorMap:      map[string]chan struct{}{},
+		instanceManagerMonitorRemoveCh: make(chan string, 1),
 	}
 
 	imInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
@@ -121,6 +178,7 @@ func NewInstanceManagerController(
 		},
 	})
 
+	go imc.monitorUpdater()
 	return imc
 }
 
@@ -272,6 +330,7 @@ func (imc *InstanceManagerController) syncInstanceManager(key string) (err error
 
 	image, err := imc.ds.GetEngineImage(im.Spec.EngineImage)
 	if err != nil {
+		im.Status.CurrentState = types.InstanceManagerStateError
 		if datastore.ErrorIsNotFound(err) {
 			logrus.Infof("Engine image %v for instance manager %v has been deleted", im.Spec.EngineImage, key)
 			return nil
@@ -291,7 +350,62 @@ func (imc *InstanceManagerController) syncInstanceManager(key string) (err error
 		return nil
 	}
 
-	if im.Status.CurrentState == types.InstanceManagerStateError {
+	// Sync instance manager status with related pod
+	if pod == nil {
+		if im.Status.CurrentState != types.InstanceManagerStateStopped {
+			im.Status.CurrentState = types.InstanceManagerStateError
+		}
+	} else {
+		switch pod.Status.Phase {
+		case v1.PodPending:
+			if im.Status.CurrentState == types.InstanceManagerStateUnknown {
+				im.Status.CurrentState = types.InstanceManagerStateStarting
+			} else if im.Status.CurrentState != types.InstanceManagerStateStarting {
+				im.Status.CurrentState = types.InstanceManagerStateError
+				logrus.Errorf("BUG: Instance Manager Pod is pending but doesn't match Instance Manager state")
+			}
+		case v1.PodRunning:
+			// Make sure readiness probe has passed.
+			for _, st := range pod.Status.ContainerStatuses {
+				if !st.Ready {
+					return nil
+				}
+			}
+
+			if im.Status.CurrentState == types.InstanceManagerStateStarting || im.Status.CurrentState == types.InstanceManagerStateUnknown {
+				nodeName := pod.Spec.NodeName
+				node, err := imc.kubeClient.CoreV1().Nodes().Get(nodeName, metav1.GetOptions{})
+				if err != nil {
+					return err
+				}
+				im.Status.CurrentState = types.InstanceManagerStateRunning
+				im.Status.IP = pod.Status.PodIP
+				im.Status.NodeBootID = node.Status.NodeInfo.BootID
+			} else if im.Status.CurrentState != types.InstanceManagerStateRunning {
+				im.Status.CurrentState = types.InstanceManagerStateError
+			}
+		default:
+			im.Status.CurrentState = types.InstanceManagerStateError
+		}
+	}
+
+	if im.Status.CurrentState == types.InstanceManagerStateRunning {
+		if !imc.isMonitoring(im.Name) {
+			switch im.Spec.Type {
+			case types.InstanceManagerTypeEngine:
+				imc.startMonitoring(im, NewEngineManagerUpdater(im))
+			case types.InstanceManagerTypeReplica:
+				imc.startMonitoring(im, NewReplicaManagerUpdater(im))
+			default:
+				logrus.Errorf("BUG: instance manager %v has invalid type %v", im.Name, im.Spec.Type)
+				im.Status.CurrentState = types.InstanceManagerStateError
+			}
+		}
+	}
+
+	// Restart instance manager automatically. Otherwise users need to take care of it then we need to expose API
+	// for operating instance manager.
+	if im.Status.CurrentState == types.InstanceManagerStateError || im.Status.CurrentState == types.InstanceManagerStateStopped {
 		if err := imc.cleanupInstanceManager(im); err != nil {
 			return err
 		}
@@ -300,52 +414,6 @@ func (imc *InstanceManagerController) syncInstanceManager(key string) (err error
 		}
 		im.Status.CurrentState = types.InstanceManagerStateStarting
 		return nil
-	}
-
-	if pod == nil {
-		if im.Status.CurrentState != types.InstanceManagerStateStopped {
-			im.Status.CurrentState = types.InstanceManagerStateError
-			return nil
-		}
-		if err := imc.createInstanceManagerPod(im, image); err != nil {
-			return err
-		}
-		im.Status.CurrentState = types.InstanceManagerStateStarting
-		return nil
-	}
-
-	switch pod.Status.Phase {
-	case v1.PodPending:
-		if im.Status.CurrentState == types.InstanceManagerStateUnknown {
-			im.Status.CurrentState = types.InstanceManagerStateStarting
-		} else if im.Status.CurrentState != types.InstanceManagerStateStarting {
-			im.Status.CurrentState = types.InstanceManagerStateError
-			logrus.Errorf("BUG: Instance Manager Pod is pending but doesn't match Instance Manager state")
-		}
-	case v1.PodRunning:
-		// Make sure readiness probe has passed.
-		for _, st := range pod.Status.ContainerStatuses {
-			if !st.Ready {
-				return nil
-			}
-		}
-		switch im.Status.CurrentState {
-		case types.InstanceManagerStateRunning:
-		case types.InstanceManagerStateStarting:
-			fallthrough
-		case types.InstanceManagerStateUnknown:
-			nodeName := pod.Spec.NodeName
-			node, err := imc.kubeClient.CoreV1().Nodes().Get(nodeName, metav1.GetOptions{})
-			if err != nil {
-				return err
-			}
-
-			im.Status.CurrentState = types.InstanceManagerStateRunning
-			im.Status.IP = pod.Status.PodIP
-			im.Status.NodeBootID = node.Status.NodeInfo.BootID
-		}
-	default:
-		im.Status.CurrentState = types.InstanceManagerStateError
 	}
 
 	return nil
@@ -375,16 +443,33 @@ func (imc *InstanceManagerController) enqueueInstanceManagerPod(pod *v1.Pod) {
 	imc.enqueueInstanceManager(im)
 }
 
-// cleanupInstanceManager cleans up the Pod that was created by the Instance Manager and marks any processes that may
-// be on that Instance Manager to Error. This is used when we need to recover from an error or prepare for deletion.
+// cleanupInstanceManager cleans up the Pod that was created by the Instance Manager, stops the monitor, and marks
+// any processes that may be on that Instance Manager to Error. This is used when we need to recover from an error
+// or prepare for deletion.
 func (imc *InstanceManagerController) cleanupInstanceManager(im *longhorn.InstanceManager) error {
 	im.Status.IP = ""
 	im.Status.NodeBootID = ""
 
+	if imc.isMonitoring(im.Name) {
+		imc.stopMonitoring(im)
+	}
+
+	// double check the instance map of instance manager.
+	// instance manager may be updated when stopping the monitor, we need to get the latest version before modifying it.
+	im, err := imc.ds.GetInstanceManager(im.Name)
+	if err != nil {
+		return err
+	}
 	for name, instance := range im.Status.Instances {
 		instance.Status.State = types.InstanceStateError
 		instance.Status.ErrorMsg = "Instance Manager errored"
 		im.Status.Instances[name] = instance
+	}
+
+	// need to update im before deleting pod
+	im, err = imc.ds.UpdateInstanceManager(im)
+	if err != nil {
+		return err
 	}
 
 	pod, err := imc.ds.GetInstanceManagerPod(im.Name)
@@ -396,6 +481,7 @@ func (imc *InstanceManagerController) cleanupInstanceManager(im *longhorn.Instan
 			return err
 		}
 	}
+
 	return nil
 }
 
@@ -567,4 +653,266 @@ func (imc *InstanceManagerController) createReplicaManagerPodSpec(im *longhorn.I
 		},
 	}
 	return podSpec, nil
+}
+
+func NewEngineManagerUpdater(im *longhorn.InstanceManager) *EngineManagerUpdater {
+	return &EngineManagerUpdater{
+		client: client.NewEngineManagerClient(imutil.GetURL(im.Status.IP, engineapi.InstanceManagerDefaultPort)),
+	}
+}
+
+func NewReplicaManagerUpdater(im *longhorn.InstanceManager) *ReplicaManagerUpdater {
+	return &ReplicaManagerUpdater{
+		client: client.NewProcessManagerClient(imutil.GetURL(im.Status.IP, engineapi.InstanceManagerDefaultPort)),
+	}
+}
+
+func (updater *EngineManagerUpdater) Poll() (map[string]types.InstanceProcess, error) {
+	result := map[string]types.InstanceProcess{}
+
+	resp, err := updater.client.EngineList()
+	if err != nil {
+		return result, err
+	}
+
+	for name, instance := range resp {
+		result[name] = *engineapi.EngineProcessToInstanceProcess(instance)
+	}
+	return result, nil
+}
+
+func (updater *EngineManagerUpdater) GetNotifier() (InstanceManagerNotifier, error) {
+	watch, err := updater.client.EngineWatch()
+	if err != nil {
+		return nil, err
+	}
+	return NewEngineManagerNotifier(watch), nil
+}
+
+func NewEngineManagerNotifier(stream *api.EngineStream) *EngineManagerNotifier {
+	return &EngineManagerNotifier{
+		stream: stream,
+	}
+}
+
+func (notifier *EngineManagerNotifier) Recv() (struct{}, error) {
+	if _, err := notifier.stream.Recv(); err != nil {
+		return struct{}{}, err
+	}
+
+	return struct{}{}, nil
+}
+
+func (notifier *EngineManagerNotifier) Close() {
+	notifier.stream.Close()
+	return
+}
+
+func (updater *ReplicaManagerUpdater) Poll() (map[string]types.InstanceProcess, error) {
+	result := map[string]types.InstanceProcess{}
+
+	resp, err := updater.client.ProcessList()
+	if err != nil {
+		return result, err
+	}
+
+	for name, instance := range resp {
+		result[name] = *engineapi.ReplicaProcessToInstanceProcess(instance)
+	}
+	return result, nil
+}
+
+func (updater *ReplicaManagerUpdater) GetNotifier() (InstanceManagerNotifier, error) {
+	watch, err := updater.client.ProcessWatch()
+	if err != nil {
+		return nil, err
+	}
+	return NewReplicaManagerNotifier(watch), nil
+}
+
+func NewReplicaManagerNotifier(stream *api.ProcessStream) *ReplicaManagerNotifier {
+	return &ReplicaManagerNotifier{
+		stream: stream,
+	}
+}
+
+func (notifier *ReplicaManagerNotifier) Recv() (struct{}, error) {
+	if _, err := notifier.stream.Recv(); err != nil {
+		return struct{}{}, err
+	}
+
+	return struct{}{}, nil
+}
+
+func (notifier *ReplicaManagerNotifier) Close() {
+	notifier.stream.Close()
+	return
+}
+
+func (imc *InstanceManagerController) monitorUpdater() {
+	for imName := range imc.instanceManagerMonitorRemoveCh {
+		imc.removeFromInstanceManagerMonitorMap(imName)
+	}
+}
+
+func (imc *InstanceManagerController) removeFromInstanceManagerMonitorMap(imName string) {
+	imc.instanceManagerMonitorMutex.Lock()
+	defer imc.instanceManagerMonitorMutex.Unlock()
+
+	stopCh, ok := imc.instanceManagerMonitorMap[imName]
+	if ok {
+		close(stopCh)
+		delete(imc.instanceManagerMonitorMap, imName)
+	}
+}
+
+func (imc *InstanceManagerController) startMonitoring(im *longhorn.InstanceManager, instanceManagerUpdater InstanceManagerUpdater) {
+	if im.Status.IP == "" {
+		// IP should be set
+		logrus.Errorf("IP of instance manager %v was not set before monitoring", im.Name)
+		return
+	}
+
+	stopCh := make(chan struct{}, 1)
+	monitor := &InstanceManagerMonitor{
+		Name:                   im.Name,
+		controllerID:           imc.controllerID,
+		ds:                     imc.ds,
+		instanceManagerUpdater: instanceManagerUpdater,
+		lock:                   &sync.Mutex{},
+		stopCh:                 stopCh,
+		// notify monitor to update the instance map
+		updateNotification: false,
+		monitoringRemoveCh: imc.instanceManagerMonitorRemoveCh,
+	}
+
+	imc.instanceManagerMonitorMutex.Lock()
+	defer imc.instanceManagerMonitorMutex.Unlock()
+
+	if _, ok := imc.instanceManagerMonitorMap[im.Name]; ok {
+		logrus.Warnf("BUG: Monitoring for %v already exists", im.Name)
+		return
+	}
+	imc.instanceManagerMonitorMap[im.Name] = stopCh
+
+	go monitor.Run()
+}
+
+func (imc *InstanceManagerController) stopMonitoring(im *longhorn.InstanceManager) {
+	imc.instanceManagerMonitorMutex.Lock()
+
+	stopCh, ok := imc.instanceManagerMonitorMap[im.Name]
+	if !ok {
+		imc.instanceManagerMonitorMutex.Unlock()
+		logrus.Warnf("instance manager %v: stop monitoring called when there is no monitoring", im.Name)
+		return
+	}
+	imc.instanceManagerMonitorMutex.Unlock()
+
+	stopCh <- struct{}{}
+	return
+}
+
+func (imc *InstanceManagerController) isMonitoring(imName string) bool {
+	imc.instanceManagerMonitorMutex.RLock()
+	defer imc.instanceManagerMonitorMutex.RUnlock()
+
+	_, ok := imc.instanceManagerMonitorMap[imName]
+	return ok
+}
+
+func (m *InstanceManagerMonitor) Run() {
+	logrus.Debugf("Start monitoring instance manager %v", m.Name)
+	defer func() {
+		m.monitoringRemoveCh <- m.Name
+		logrus.Debugf("Stop monitoring instance manager %v", m.Name)
+	}()
+
+	notifier, err := m.instanceManagerUpdater.GetNotifier()
+	if err != nil {
+		logrus.Errorf("failed to start notifier for the monitor of instance manager %v: %v", m.Name, err)
+		return
+	}
+
+	go func() {
+		for {
+			if _, err := notifier.Recv(); err != nil {
+				logrus.Errorf("error receiving next item in engine watch: %v", err)
+			} else {
+				m.lock.Lock()
+				m.updateNotification = true
+				m.lock.Unlock()
+			}
+			time.Sleep(time.Second)
+		}
+	}()
+
+	timer := 0
+	ticker := time.Tick(MinPollCount * PollInterval)
+	for {
+		select {
+		case <-ticker:
+			needUpdate := false
+
+			m.lock.Lock()
+			timer++
+			// sync instance map at least every 30s, at most every 1s
+			if timer >= MaxPollCount || m.updateNotification {
+				needUpdate = true
+				m.updateNotification = false
+				timer = 0
+			}
+			m.lock.Unlock()
+
+			if needUpdate {
+				if err := m.pollAndUpdateInstanceMap(); err != nil {
+					logrus.Error(err)
+				}
+			}
+		case <-m.stopCh:
+			notifier.Close()
+			if err := m.updateInstanceMapForCleanup(); err != nil {
+				logrus.Errorf("failed to mark existing instances to error when stopping instance manager monitor: %v", err)
+			}
+			return
+		}
+	}
+}
+
+func (m *InstanceManagerMonitor) pollAndUpdateInstanceMap() error {
+	resp, err := m.instanceManagerUpdater.Poll()
+	if err != nil {
+		return fmt.Errorf("failed to poll instance info to update instance manager %v: %v", m.Name, err)
+	}
+
+	im, err := m.ds.GetInstanceManager(m.Name)
+	if err != nil {
+		return fmt.Errorf("failed to list processes to update instance manager %v: %v", m.Name, err)
+	}
+
+	im.Status.Instances = resp
+	if _, err := m.ds.UpdateInstanceManager(im); err != nil {
+		return fmt.Errorf("failed to update instance map for instance manager %v: %v", m.Name, err)
+	}
+
+	return nil
+}
+
+func (m *InstanceManagerMonitor) updateInstanceMapForCleanup() error {
+	im, err := m.ds.GetInstanceManager(m.Name)
+	if err != nil {
+		return fmt.Errorf("failed to get instance manager %v to cleanup instance map: %v", m.Name, err)
+	}
+
+	for name, instance := range im.Status.Instances {
+		instance.Status.State = types.InstanceStateError
+		instance.Status.ErrorMsg = "Instance Manager errored"
+		im.Status.Instances[name] = instance
+	}
+
+	if _, err := m.ds.UpdateInstanceManager(im); err != nil {
+		return fmt.Errorf("failed to update instance map for instance manager %v: %v", m.Name, err)
+	}
+
+	return nil
 }
