592678393760d8a0f2ca798767205e7b7ca35b74
Shuo Wu <shuo@rancher.com>
2019-08-20 16:24:10 -0700

    Add deployment lister for datastore
    Signed-off-by: Shuo Wu <shuo@rancher.com>

--- controller/node_controller_test.go
+++ controller/node_controller_test.go
@@ -57,6 +57,7 @@ func newTestNodeController(lhInformerFactory lhinformerfactory.SharedInformerFac
 	kubeNodeInformer := kubeInformerFactory.Core().V1().Nodes()
 	cronJobInformer := kubeInformerFactory.Batch().V1beta1().CronJobs()
 	daemonSetInformer := kubeInformerFactory.Apps().V1beta2().DaemonSets()
+	deploymentInformer := kubeInformerFactory.Apps().V1beta2().Deployments()
 	persistentVolumeInformer := kubeInformerFactory.Core().V1().PersistentVolumes()
 	persistentVolumeClaimInformer := kubeInformerFactory.Core().V1().PersistentVolumeClaims()
 
@@ -64,7 +65,7 @@ func newTestNodeController(lhInformerFactory lhinformerfactory.SharedInformerFac
 		volumeInformer, engineInformer, replicaInformer,
 		engineImageInformer, nodeInformer, settingInformer, imInformer,
 		lhClient,
-		podInformer, cronJobInformer, daemonSetInformer,
+		podInformer, cronJobInformer, daemonSetInformer, deploymentInformer,
 		persistentVolumeInformer, persistentVolumeClaimInformer,
 		kubeClient, TestNamespace)
 
