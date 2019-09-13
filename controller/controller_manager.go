f02dc45299d129b55a04e75dbeca347911b5094f
Shuo Wu <shuo@rancher.com>
2019-09-12 19:39:02 -0700

    controller: Engine image controller starts to listen node change
    The change of `node.Spec.Disks` may lead to the deletion of replica
    manager.
    
    Signed-off-by: Shuo Wu <shuo@rancher.com>

--- controller/controller_manager.go
+++ controller/controller_manager.go
@@ -95,7 +95,7 @@ func StartControllers(stopCh chan struct{}, controllerID, serviceAccount, manage
 		kubeClient, namespace, controllerID,
 		serviceAccount, managerImage)
 	ic := NewEngineImageController(ds, scheme,
-		engineImageInformer, volumeInformer, daemonSetInformer,
+		engineImageInformer, volumeInformer, daemonSetInformer, nodeInformer,
 		kubeClient, namespace, controllerID, serviceAccount)
 	nc := NewNodeController(ds, scheme,
 		nodeInformer, settingInformer, podInformer, replicaInformer, kubeNodeInformer,
