53e8da77de874e92d263d52f0370974761d389f6
Shuo Wu <shuo@rancher.com>
2020-02-28 13:14:22 +0800

    engineapi: Remove typo for VersionGet()
    Signed-off-by: Shuo Wu <shuo@rancher.com>

--- engineapi/instance_manager.go
+++ engineapi/instance_manager.go
@@ -214,7 +214,7 @@ func (c *InstanceManagerClient) EngineProcessUpgrade(engineName, volumeName, eng
 	return c.parseProcess(engineProcess), nil
 }
 
-func (c *InstanceManagerClient) VersionGet() (majorVersion int, minorVersion int, err error) {
+func (c *InstanceManagerClient) VersionGet() (int, int, error) {
 	output, err := c.grpcClient.VersionGet()
 	if err != nil {
 		return 0, 0, err
