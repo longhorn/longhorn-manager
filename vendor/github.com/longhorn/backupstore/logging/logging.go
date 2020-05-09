c644117667c231c64b478a071142070776f5c26f
Joshua Moody <joshua.moody@rancher.com>
2020-05-08 22:16:32 -0700

    Update longhorn/backupstore to v0.0.0-20200509050526-150a317f1405
    Signed-off-by: Joshua Moody <joshua.moody@rancher.com>

--- vendor/github.com/longhorn/backupstore/logging/logging.go
+++ vendor/github.com/longhorn/backupstore/logging/logging.go
@@ -12,6 +12,9 @@ const (
 	LogFieldVolumeName   = "volume_name"
 	LogFieldOrigVolume   = "original_volume"
 	LogFieldSnapshot     = "snapshot"
+	LogFieldBackup       = "backup"
+	LogFieldBackupType   = "backup_type"
+	LogFieldLastBackup   = "last_backup"
 	LogFieldLastSnapshot = "last_snapshot"
 	LogEventBackupURL    = "backup_url"
 	LogFieldDestURL      = "dest_url"
@@ -20,6 +23,7 @@ const (
 
 	LogFieldEvent        = "event"
 	LogEventBackup       = "backup"
+	LogEventList         = "list"
 	LogEventRestore      = "restore"
 	LogEventRestoreIncre = "restore_incrementally"
 	LogEventCompare      = "compare"
@@ -30,6 +34,7 @@ const (
 	LogReasonFallback = "fallback"
 
 	LogFieldObject    = "object"
+	LogObjectBackup   = "backup"
 	LogObjectSnapshot = "snapshot"
 	LogObjectConfig   = "config"
 )
