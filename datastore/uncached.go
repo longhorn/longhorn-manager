753af28733f8ed3328bc14dee478b39b4090dd05
Chin-Ya Huang <chin-ya.huang@suse.com>
2020-08-19 08:49:02 +0800

    Add func/method comments to datastore
    * datastore.go
    * kubernetes.go
    * longhorn.go
    * uncached.go
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- datastore/uncached.go
+++ datastore/uncached.go
@@ -6,57 +6,132 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-// Event list is uncached but current lister doesn't field selector, so we have no choice but read from API directly
-
+// GetLonghornEventList returns an uncached list of longhorn events for the
+// given namespace directly from the API server.
+// Using cached informers should be preferred but current lister doesn't have a
+// field selector.
+// Direct retrieval from the API server should only be used for one-shot tasks.
+// For example, support bundle creation
 func (s *DataStore) GetLonghornEventList() (*corev1.EventList, error) {
 	return s.kubeClient.CoreV1().Events(s.namespace).List(metav1.ListOptions{FieldSelector: "involvedObject.apiVersion=longhorn.io/v1beta1"})
 }
 
-// The following APIs are not cached, should only be used in e.g. the support bundle
-
+// GetAllPodsList returns an uncached list of pods for the given namespace
+// directly from the API server.
+// Using cached informers should be preferred but current lister doesn't have a
+// field selector.
+// Direct retrieval from the API server should only be used for one-shot tasks.
+// For example, support bundle creation
 func (s *DataStore) GetAllPodsList() (runtime.Object, error) {
 	return s.kubeClient.CoreV1().Pods(s.namespace).List(metav1.ListOptions{})
 }
 
+// GetAllServicesList returns an uncached list of services for the given
+// namespace directly from the API server.
+// Using cached informers should be preferred but current lister doesn't have a
+// field selector.
+// Direct retrieval from the API server should only be used for one-shot tasks.
+// For example, support bundle creation
 func (s *DataStore) GetAllServicesList() (runtime.Object, error) {
 	return s.kubeClient.CoreV1().Services(s.namespace).List(metav1.ListOptions{})
 }
+
+// GetAllDeploymentsList returns an uncached list of deployments for the given
+// namespace directly from the API server.
+// Using cached informers should be preferred but current lister doesn't have a
+// field selector.
+// Direct retrieval from the API server should only be used for one-shot tasks.
+// For example, support bundle creation
 func (s *DataStore) GetAllDeploymentsList() (runtime.Object, error) {
 	return s.kubeClient.AppsV1().Deployments(s.namespace).List(metav1.ListOptions{})
 }
 
+// GetAllDaemonSetsList returns an uncached list of daemonsets for the given
+// namespace directly from the API server.
+// Using cached informers should be preferred but current lister doesn't have a
+// field selector.
+// Direct retrieval from the API server should only be used for one-shot tasks.
+// For example, support bundle creation
 func (s *DataStore) GetAllDaemonSetsList() (runtime.Object, error) {
 	return s.kubeClient.AppsV1().DaemonSets(s.namespace).List(metav1.ListOptions{})
 }
 
+// GetAllStatefulSetsList returns an uncached list of statefulsets for the given
+// namespace directly from the API server.
+// Using cached informers should be preferred but current lister doesn't have a
+// field selector.
+// Direct retrieval from the API server should only be used for one-shot tasks.
+// For example, support bundle creation
 func (s *DataStore) GetAllStatefulSetsList() (runtime.Object, error) {
 	return s.kubeClient.AppsV1().StatefulSets(s.namespace).List(metav1.ListOptions{})
 }
 
+// GetAllJobsList returns an uncached list of jobs for the given namespace
+// directly from the API server.
+// Using cached informers should be preferred but current lister doesn't have a
+// field selector.
+// Direct retrieval from the API server should only be used for one-shot tasks.
+// For example, support bundle creation
 func (s *DataStore) GetAllJobsList() (runtime.Object, error) {
 	return s.kubeClient.BatchV1().Jobs(s.namespace).List(metav1.ListOptions{})
 }
 
+// GetAllCronJobsList returns an uncached list of cronjobs for the given
+// namespace directly from the API server.
+// Using cached informers should be preferred but current lister doesn't have a
+// field selector.
+// Direct retrieval from the API server should only be used for one-shot tasks.
+// For example, support bundle creation
 func (s *DataStore) GetAllCronJobsList() (runtime.Object, error) {
 	return s.kubeClient.BatchV1beta1().CronJobs(s.namespace).List(metav1.ListOptions{})
 }
 
+// GetAllNodesList returns an uncached list of nodes for the given namespace
+// directly from the API server.
+// Using cached informers should be preferred but current lister doesn't have a
+// field selector.
+// Direct retrieval from the API server should only be used for one-shot tasks.
+// For example, support bundle creation
 func (s *DataStore) GetAllNodesList() (runtime.Object, error) {
 	return s.kubeClient.CoreV1().Nodes().List(metav1.ListOptions{})
 }
 
+// GetLonghornNamespace returns an uncached namespace object for the given
+// namespace directly from the API server.
+// Using cached informers should be preferred but current lister doesn't have a
+// field selector.
+// Direct retrieval from the API server should only be used for one-shot tasks.
+// For example, support bundle creation
 func (s *DataStore) GetLonghornNamespace() (*corev1.Namespace, error) {
 	return s.kubeClient.CoreV1().Namespaces().Get(s.namespace, metav1.GetOptions{})
 }
 
+// GetAllEventsList returns an uncached list of events for the given namespace
+// directly from the API server.
+// Using cached informers should be preferred but current lister doesn't have a
+// field selector.
+// Direct retrieval from the API server should only be used for one-shot tasks.
+// For example, support bundle creation
 func (s *DataStore) GetAllEventsList() (runtime.Object, error) {
 	return s.kubeClient.CoreV1().Events(s.namespace).List(metav1.ListOptions{})
 }
 
+// GetAllConfigMaps returns an uncached list of configmaps for the given
+// namespace directly from the API server.
+// Using cached informers should be preferred but current lister doesn't have a
+// field selector.
+// Direct retrieval from the API server should only be used for one-shot tasks.
+// For example, support bundle creation
 func (s *DataStore) GetAllConfigMaps() (runtime.Object, error) {
 	return s.kubeClient.CoreV1().ConfigMaps(s.namespace).List(metav1.ListOptions{})
 }
 
+// GetAllVolumeAttachments returns an uncached list of volumeattachments for
+// the given namespace directly from the API server.
+// Using cached informers should be preferred but current lister doesn't have a
+// field selector.
+// Direct retrieval from the API server should only be used for one-shot tasks.
+// For example, support bundle creation
 func (s *DataStore) GetAllVolumeAttachments() (runtime.Object, error) {
 	return s.kubeClient.StorageV1().VolumeAttachments().List(metav1.ListOptions{})
 }
