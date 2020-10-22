5f9fba7f944d6dd5ce53197fcba41b4200dc6c6c
Sheng Yang <sheng.yang@rancher.com>
2020-10-22 11:14:31 -0700

    build: Add update-vendor.sh
    Use this script to update vendor, to avoid missing the custom patches
    
    Signed-off-by: Sheng Yang <sheng.yang@rancher.com>

--- update-longhorn-deps.sh
+++ update-longhorn-deps.sh
@@ -3,5 +3,4 @@
 go get github.com/longhorn/go-iscsi-helper@master
 go get github.com/longhorn/backupstore@master
 go get github.com/longhorn/longhorn-engine@master
-go mod tidy
-go mod vendor
+./update-vendor.sh
