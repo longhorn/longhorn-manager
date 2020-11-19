7a2388de48589cb2979f3caee3fbc6ae6e08b9a0
Phan Le <phan.le@rancher.com>
2020-11-19 11:08:05 -0800

    Update toleration annotation for existing Longhorn components
    We need to do this so that Longhorn doesn't accidentally restart
    all components during the upgrade to v1.1.0
    
    Longhorn #1874
    
    Signed-off-by: Phan Le <phan.le@rancher.com>

--- upgrade/v102to110/upgrade.go
+++ upgrade/v102to110/upgrade.go
@@ -1,6 +1,7 @@
 package v102to110
 
 import (
+	"encoding/json"
 	"fmt"
 	"path/filepath"
 	"strings"
@@ -12,6 +13,7 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	clientset "k8s.io/client-go/kubernetes"
 
 	lhclientset "github.com/longhorn/longhorn-manager/k8s/pkg/client/clientset/versioned"
@@ -46,6 +48,9 @@ func UpgradeInstanceManagerPods(namespace string, lhClient *lhclientset.Clientse
 
 	for _, pod := range imPodsList.Items {
 		if err := upgradeInstanceMangerPodOwnerRef(&pod, kubeClient, namespace); err != nil {
+			return err
+		}
+		if err := updateIMPodLastAppliedTolerationsAnnotation(&pod, kubeClient, namespace); err != nil {
 			return err
 		}
 	}
@@ -162,4 +167,134 @@ func UpgradeReplicas(namespace string, lhClient *lhclientset.Clientset) (err err
 		}
 	}
 	return nil
+}
+
+func UpdateDeploymentAndDaemonset(namespace string, kubeClient *clientset.Clientset) (err error) {
+	defer func() {
+		err = errors.Wrapf(err, upgradeLogPrefix+"UpdateLastAppliedTolerationsAnnotation failed")
+	}()
+
+	if err := updateDeploymentLastAppliedTolerationsAnnotation(kubeClient, namespace); err != nil {
+		return err
+	}
+	if err := updateDaemonsetLastAppliedTolerationsAnnotation(kubeClient, namespace); err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateDaemonsetLastAppliedTolerationsAnnotation(kubeClient *clientset.Clientset, namespace string) (err error) {
+	daemonsetList, err := kubeClient.AppsV1().DaemonSets(namespace).List(metav1.ListOptions{})
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil
+		}
+		return errors.Wrapf(err, "failed to list Longhorn daemonsets for toleration annotation update")
+	}
+	for _, ds := range daemonsetList.Items {
+		dsp := &ds
+		needToUpdate, err := needToUpdateTolerationAnnotation(dsp)
+		if err != nil {
+			return err
+		}
+		if !needToUpdate {
+			continue
+		}
+		appliedTolerations := getNonDefaultTolerationList(dsp.Spec.Template.Spec.Tolerations)
+		appliedTolerationsByte, err := json.Marshal(appliedTolerations)
+		if err != nil {
+			return err
+		}
+		if err := util.SetAnnotation(dsp, types.GetLonghornLabelKey(types.LastAppliedTolerationAnnotationKeySuffix), string(appliedTolerationsByte)); err != nil {
+			return err
+		}
+		if _, err := kubeClient.AppsV1().DaemonSets(namespace).Update(dsp); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func updateDeploymentLastAppliedTolerationsAnnotation(kubeClient *clientset.Clientset, namespace string) (err error) {
+	deploymentList, err := kubeClient.AppsV1().Deployments(namespace).List(metav1.ListOptions{})
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil
+		}
+		return errors.Wrapf(err, "failed to list Longhorn deployments for toleration annotation update")
+	}
+
+	for _, dp := range deploymentList.Items {
+		dpp := &dp
+		needToUpdate, err := needToUpdateTolerationAnnotation(dpp)
+		if err != nil {
+			return err
+		}
+		if !needToUpdate {
+			continue
+		}
+		appliedTolerations := getNonDefaultTolerationList(dpp.Spec.Template.Spec.Tolerations)
+		appliedTolerationsByte, err := json.Marshal(appliedTolerations)
+		if err != nil {
+			return err
+		}
+		if err := util.SetAnnotation(dpp, types.GetLonghornLabelKey(types.LastAppliedTolerationAnnotationKeySuffix), string(appliedTolerationsByte)); err != nil {
+			return err
+		}
+		if _, err := kubeClient.AppsV1().Deployments(namespace).Update(dpp); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func updateIMPodLastAppliedTolerationsAnnotation(pod *v1.Pod, kubeClient *clientset.Clientset, namespace string) (err error) {
+	needToUpdate, err := needToUpdateTolerationAnnotation(pod)
+	if err != nil {
+		return err
+	}
+	if !needToUpdate {
+		return nil
+	}
+	appliedTolerations := getNonDefaultTolerationList(pod.Spec.Tolerations)
+	appliedTolerationsByte, err := json.Marshal(appliedTolerations)
+	if err != nil {
+		return err
+	}
+	if err := util.SetAnnotation(pod, types.GetLonghornLabelKey(types.LastAppliedTolerationAnnotationKeySuffix), string(appliedTolerationsByte)); err != nil {
+		return err
+	}
+	if _, err = kubeClient.CoreV1().Pods(namespace).Update(pod); err != nil {
+		return errors.Wrapf(err, "failed to update toleration annotation for instance manager pod %v", pod.GetName())
+	}
+	return nil
+}
+
+func getNonDefaultTolerationList(tolerations []v1.Toleration) []v1.Toleration {
+	result := []v1.Toleration{}
+	for _, t := range tolerations {
+		if !util.IsKubernetesDefaultToleration(t) {
+			result = append(result, t)
+		}
+	}
+	return result
+}
+
+func needToUpdateTolerationAnnotation(obj runtime.Object) (bool, error) {
+	objMeta, err := meta.Accessor(obj)
+	if err != nil {
+		return false, err
+	}
+
+	annos := objMeta.GetAnnotations()
+	if annos == nil {
+		return true, nil
+	}
+
+	_, ok := annos[types.GetLonghornLabelKey(types.LastAppliedTolerationAnnotationKeySuffix)]
+	if ok {
+		return false, nil
+	}
+
+	return true, nil
 }
