a1603986d98f1007e7859e2e486f1d8e958f70de
JenTing Hsiao <jenting.hsiao@suse.com>
2021-06-28 13:07:54 +0800

    Add a new backup_target_controller
    Add a new backup_target_controller which is responsible for
    creating/deleting the BackupVolume CR.
    
    Signed-off-by: JenTing Hsiao <jenting.hsiao@suse.com>

---
+++ controller/backup_target_controller.go
@@ -0,0 +1,377 @@
+package controller
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+
+	v1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/apimachinery/pkg/util/wait"
+	clientset "k8s.io/client-go/kubernetes"
+	v1core "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/kubernetes/pkg/controller"
+
+	"github.com/longhorn/longhorn-manager/datastore"
+	"github.com/longhorn/longhorn-manager/engineapi"
+	"github.com/longhorn/longhorn-manager/types"
+
+	longhorn "github.com/longhorn/longhorn-manager/k8s/pkg/apis/longhorn/v1beta1"
+	lhinformers "github.com/longhorn/longhorn-manager/k8s/pkg/client/informers/externalversions/longhorn/v1beta1"
+)
+
+type BackupTargetController struct {
+	*baseController
+
+	// which namespace controller is running with
+	namespace string
+	// use as the OwnerID of the controller
+	controllerID string
+
+	kubeClient    clientset.Interface
+	eventRecorder record.EventRecorder
+
+	ds *datastore.DataStore
+
+	btStoreSynced cache.InformerSynced
+}
+
+func NewBackupTargetController(
+	logger logrus.FieldLogger,
+	ds *datastore.DataStore,
+	scheme *runtime.Scheme,
+	backupTargetInformer lhinformers.BackupTargetInformer,
+	kubeClient clientset.Interface,
+	controllerID string,
+	namespace string) *BackupTargetController {
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartLogging(logrus.Infof)
+	// TODO: remove the wrapper when every clients have moved to use the clientset.
+	eventBroadcaster.StartRecordingToSink(&v1core.EventSinkImpl{
+		Interface: v1core.New(kubeClient.CoreV1().RESTClient()).Events(""),
+	})
+
+	btc := &BackupTargetController{
+		baseController: newBaseController("longhorn-backup-target", logger),
+
+		namespace:    namespace,
+		controllerID: controllerID,
+
+		ds: ds,
+
+		kubeClient:    kubeClient,
+		eventRecorder: eventBroadcaster.NewRecorder(scheme, v1.EventSource{Component: "longhorn-backup-target-controller"}),
+
+		btStoreSynced: backupTargetInformer.Informer().HasSynced,
+	}
+
+	backupTargetInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    btc.enqueueBackupTarget,
+		UpdateFunc: func(old, cur interface{}) { btc.enqueueBackupTarget(cur) },
+	})
+
+	return btc
+}
+
+func (btc *BackupTargetController) enqueueBackupTarget(obj interface{}) {
+	key, err := controller.KeyFunc(obj)
+	if err != nil {
+		utilruntime.HandleError(fmt.Errorf("couldn't get key for object %#v: %v", obj, err))
+		return
+	}
+
+	btc.queue.AddRateLimited(key)
+}
+
+func (btc *BackupTargetController) Run(workers int, stopCh <-chan struct{}) {
+	defer utilruntime.HandleCrash()
+	defer btc.queue.ShutDown()
+
+	btc.logger.Infof("Start Longhorn Backup Target controller")
+	defer btc.logger.Infof("Shutting down Longhorn Backup Target controller")
+
+	if !cache.WaitForNamedCacheSync(btc.name, stopCh, btc.btStoreSynced) {
+		return
+	}
+	for i := 0; i < workers; i++ {
+		go wait.Until(btc.worker, time.Second, stopCh)
+	}
+	<-stopCh
+}
+
+func (btc *BackupTargetController) worker() {
+	for btc.processNextWorkItem() {
+	}
+}
+
+func (btc *BackupTargetController) processNextWorkItem() bool {
+	key, quit := btc.queue.Get()
+	if quit {
+		return false
+	}
+	defer btc.queue.Done(key)
+	err := btc.syncHandler(key.(string))
+	btc.handleErr(err, key)
+	return true
+}
+
+func (btc *BackupTargetController) syncHandler(key string) (err error) {
+	defer func() {
+		err = errors.Wrapf(err, "%v: fail to sync %v", btc.name, key)
+	}()
+
+	namespace, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		return err
+	}
+	if namespace != btc.namespace {
+		// Not ours, skip it
+		return nil
+	}
+	if name != types.DefaultBackupTargetName {
+		// For now, we only support a default backup target
+		// We've to enhance it once we support multiple backup targets
+		// https://github.com/longhorn/longhorn/issues/2317
+		return nil
+	}
+	return btc.reconcile(name)
+}
+
+func (btc *BackupTargetController) handleErr(err error, key interface{}) {
+	if err == nil {
+		btc.queue.Forget(key)
+		return
+	}
+
+	if btc.queue.NumRequeues(key) < maxRetries {
+		btc.logger.WithError(err).Warnf("Error syncing Longhorn backup target %v", key)
+		btc.queue.AddRateLimited(key)
+		return
+	}
+
+	utilruntime.HandleError(err)
+	btc.logger.WithError(err).Warnf("Dropping Longhorn backup target %v out of the queue", key)
+	btc.queue.Forget(key)
+}
+
+func getLoggerForBackupTarget(logger logrus.FieldLogger, backupTarget *longhorn.BackupTarget) *logrus.Entry {
+	return logger.WithFields(
+		logrus.Fields{
+			"url":      backupTarget.Spec.BackupTargetURL,
+			"cred":     backupTarget.Spec.CredentialSecret,
+			"interval": backupTarget.Spec.PollInterval.Duration,
+		},
+	)
+}
+
+func (btc *BackupTargetController) reconcile(name string) (err error) {
+	backupTarget, err := btc.ds.GetBackupTarget(name)
+	if err != nil {
+		if !apierrors.IsNotFound(err) {
+			return err
+		}
+		return nil
+	}
+
+	// Check the responsible node
+	defaultEngineImage, err := btc.ds.GetSettingValueExisted(types.SettingNameDefaultEngineImage)
+	if err != nil {
+		return err
+	}
+	isResponsible, err := btc.isResponsibleFor(backupTarget, defaultEngineImage)
+	if err != nil {
+		return nil
+	}
+	if !isResponsible {
+		return nil
+	}
+	if backupTarget.Status.OwnerID != btc.controllerID {
+		backupTarget.Status.OwnerID = btc.controllerID
+		backupTarget, err = btc.ds.UpdateBackupTargetStatus(backupTarget)
+		if err != nil {
+			// we don't mind others coming first
+			if apierrors.IsConflict(errors.Cause(err)) {
+				return nil
+			}
+			return err
+		}
+	}
+
+	log := getLoggerForBackupTarget(btc.logger, backupTarget)
+
+	// Check the controller should run synchronization
+	if !backupTarget.Status.LastSyncedAt.IsZero() &&
+		backupTarget.Status.LastSyncedAt.Time.After(backupTarget.Spec.SyncRequestedAt.Time) {
+		return nil
+	}
+
+	syncTime := &metav1.Time{Time: time.Now().UTC()}
+	defer func() {
+		if err != nil {
+			return
+		}
+		// There is no need to do deep equal since this controller
+		// always updates timestamp `backupTarget.Status.LastSyncedAt`
+		backupTarget.Status.LastSyncedAt = syncTime
+		if _, err := btc.ds.UpdateBackupTargetStatus(backupTarget); err != nil && apierrors.IsConflict(errors.Cause(err)) {
+			log.WithError(err).Debugf("Requeue %v due to conflict", name)
+			btc.enqueueBackupTarget(backupTarget)
+		}
+	}()
+
+	if backupTarget.Spec.BackupTargetURL == "" {
+		backupTarget.Status.Available = false
+		// Clean up all BackupVolume CRs
+		if err := btc.cleanupBackupVolumes(); err != nil {
+			log.WithError(err).Error("Error deleting backup volumes")
+			return err
+		}
+		return nil
+	}
+
+	// Initialize a backup target client
+	credential, err := btc.ds.GetCredentialFromSecret(backupTarget.Spec.CredentialSecret)
+	if err != nil {
+		return err
+	}
+	backupTargetClient, err := engineapi.NewBackupTargetClient(defaultEngineImage, backupTarget.Spec.BackupTargetURL, credential)
+	if err != nil {
+		backupTarget.Status.Available = false
+		log.WithError(err).Error("Error init backup target client")
+		return nil // Ignore error to allow status update as well as preventing enqueue
+	}
+
+	// Get a list of all the backup volumes that are stored in the backup target
+	res, err := backupTargetClient.ListBackupVolumeNames()
+	if err != nil {
+		backupTarget.Status.Available = false
+		log.WithError(err).Error("Error listing backup volumes from backup target")
+		return nil // Ignore error to allow status update as well as preventing enqueue
+	}
+	backupStoreBackupVolumes := sets.NewString(res...)
+
+	// Get a list of all the backup volumes that exist as custom resources in the cluster
+	clusterBackupVolumes, err := btc.ds.ListBackupVolumes()
+	if err != nil {
+		log.WithError(err).Error("Error listing backup volumes in the cluster, proceeding with pull into cluster")
+		return err
+	}
+
+	clusterBackupVolumesSet := sets.NewString()
+	for _, b := range clusterBackupVolumes {
+		clusterBackupVolumesSet.Insert(b.Name)
+	}
+
+	// TODO: add a unit test, separate to a function
+	// Get a list of backup volumes that *are* in the backup target and *aren't* in the cluster
+	// and create the BackupVolume CR in the cluster
+	backupVolumesToPull := backupStoreBackupVolumes.Difference(clusterBackupVolumesSet)
+	if count := backupVolumesToPull.Len(); count > 0 {
+		log.Infof("Found %d backup volumes in the backup target that do not exist in the cluster and need to be pulled", count)
+	}
+	for backupVolumeName := range backupVolumesToPull {
+		backupVolume := &longhorn.BackupVolume{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: backupVolumeName,
+			},
+			Spec: types.BackupVolumeSpec{
+				SyncRequestedAt: syncTime,
+			},
+		}
+		if _, err = btc.ds.CreateBackupVolume(backupVolume); err != nil && !apierrors.IsAlreadyExists(err) {
+			log.WithError(err).Errorf("Error creating backup volume %s into cluster", backupVolumeName)
+			return err
+		}
+	}
+
+	// TODO: add a unit test, separate to a function
+	// Get a list of backup volumes that *are* in the cluster and *aren't* in the backup target
+	// and delete the BackupVolume CR in the cluster
+	backupVolumesToDelete := clusterBackupVolumesSet.Difference(backupStoreBackupVolumes)
+	if count := backupVolumesToDelete.Len(); count > 0 {
+		log.Infof("Found %d backup volumes in the backup target that do not exist in the backup target and need to be deleted", count)
+	}
+	for backupVolumeName := range backupVolumesToDelete {
+		log.WithField("backupVolume", backupVolumeName).Info("Attempting to delete backup volume from cluster")
+		if err = btc.ds.DeleteBackupVolume(backupVolumeName); err != nil {
+			log.WithError(err).Errorf("Error deleting backup volume %s from cluster", backupVolumeName)
+			return err
+		}
+	}
+
+	// Update the BackupVolume CR spec.syncRequestAt to request the
+	// backup_volume_controller to reconcile the BackupVolume CR
+	for backupVolumeName, backupVolume := range clusterBackupVolumes {
+		backupVolume.Spec.SyncRequestedAt = syncTime
+		if _, err = btc.ds.UpdateBackupVolume(backupVolume); err != nil && !apierrors.IsConflict(errors.Cause(err)) {
+			log.WithError(err).Errorf("Error updating backup volume %s spec", backupVolumeName)
+		}
+	}
+
+	// Update the backup target status
+	backupTarget.Status.Available = true
+	return nil
+}
+
+func (btc *BackupTargetController) isResponsibleFor(bt *longhorn.BackupTarget, defaultEngineImage string) (bool, error) {
+	var err error
+	defer func() {
+		err = errors.Wrap(err, "error while checking isResponsibleFor")
+	}()
+
+	isResponsible := isControllerResponsibleFor(btc.controllerID, btc.ds, bt.Name, "", bt.Status.OwnerID)
+
+	readyNodesWithEI, err := btc.ds.ListReadyNodesWithEngineImage(defaultEngineImage)
+	if err != nil {
+		return false, err
+	}
+	// No node in the system has the default engine image,
+	// Fall back to the default logic where we pick a running node to be the owner
+	if len(readyNodesWithEI) == 0 {
+		return isResponsible, nil
+	}
+
+	currentOwnerEngineAvailable, err := btc.ds.CheckEngineImageReadiness(defaultEngineImage, bt.Status.OwnerID)
+	if err != nil {
+		return false, err
+	}
+	currentNodeEngineAvailable, err := btc.ds.CheckEngineImageReadiness(defaultEngineImage, btc.controllerID)
+	if err != nil {
+		return false, err
+	}
+
+	isPreferredOwner := currentNodeEngineAvailable && isResponsible
+	continueToBeOwner := currentNodeEngineAvailable && btc.controllerID == bt.Status.OwnerID
+	requiresNewOwner := currentNodeEngineAvailable && !currentOwnerEngineAvailable
+
+	return isPreferredOwner || continueToBeOwner || requiresNewOwner, nil
+}
+
+// cleanupBackupVolumes deletes all BackupVolume CRs
+func (btc *BackupTargetController) cleanupBackupVolumes() error {
+	clusterBackupVolumes, err := btc.ds.ListBackupVolumes()
+	if err != nil {
+		return err
+	}
+
+	var errs []string
+	for backupVolumeName := range clusterBackupVolumes {
+		if err = btc.ds.DeleteBackupVolume(backupVolumeName); err != nil {
+			errs = append(errs, err.Error())
+			continue
+		}
+	}
+	if len(errs) > 0 {
+		return errors.New(strings.Join(errs, ","))
+	}
+	return nil
+}
