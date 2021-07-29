cafb67c839a108df56fb9d8bdc37dba864f23dd1
Joshua Moody <joshua.moody@suse.com>
2021-07-29 19:19:40 +0200

    Remove duplicate engine::info call to extract volume endpoint
    Since we already require the volume info any way this is an unnecessary
    process/engine client socket creation in the monitoring loop.
    
    Longhorn #2818
    
    Signed-off-by: Joshua Moody <joshua.moody@suse.com>

--- engineapi/enginesim.go
+++ engineapi/enginesim.go
@@ -191,10 +191,6 @@ func (e *EngineSimulator) Info() (*Volume, error) {
 	return nil, fmt.Errorf("Not implemented")
 }
 
-func (e *EngineSimulator) Endpoint() (string, error) {
-	return "", fmt.Errorf("Not implemented")
-}
-
 func (e *EngineSimulator) Expand(size int64) error {
 	return fmt.Errorf("Not implemented")
 }
