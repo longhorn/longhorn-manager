rancher/lm-rewrite

Do disk file cleanup based on HA requirement
Longhorn #2530

Signed-off-by: Shuo Wu <shuo.wu@suse.com>

manager/backingimage.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
+	"reflect"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/longhorn/longhorn-manager/datastore"
@@ -168,12 +170,35 @@ func (m *VolumeManager) DeleteBackingImage(name string) error {
 	return nil
 }
 
-func (m *VolumeManager) CleanUpBackingImageInDisks(name string, disks []string) (*longhorn.BackingImage, error) {
-	defer logrus.Infof("Cleaning up backing image %v in disks %+v", name, disks)
-	bi, err := m.GetBackingImage(name)
+func (m *VolumeManager) CleanUpBackingImageDiskFiles(name string, diskFileList []string) (bi *longhorn.BackingImage, err error) {
+	defer logrus.Infof("Cleaning up backing image %v in diskFileList %+v", name, diskFileList)
+
+	bi, err = m.GetBackingImage(name)
 	if err != nil {
 		return nil, errors.Wrapf(err, "unable to get backing image %v", name)
 	}
+	if bi.DeletionTimestamp != nil {
+		logrus.Infof("Deleting backing image %v, there is no need to do disk cleanup for it", name)
+		return bi, nil
+	}
+	bids, err := m.GetBackingImageDataSource(name)
+	if err != nil {
+		if !apierrors.IsNotFound(err) {
+			return nil, errors.Wrapf(err, "unable to get backing image data source %v", name)
+		}
+		logrus.Warnf("Cannot find backing image data source %v, will ignore it and continue clean up", name)
+	}
+
+	existingBI := bi.DeepCopy()
+	defer func() {
+		if err == nil {
+			if !reflect.DeepEqual(bi.Spec, existingBI.Spec) {
+				bi, err = m.ds.UpdateBackingImage(bi)
+				return
+			}
+		}
+	}()
+
 	replicas, err := m.ds.ListReplicasByBackingImage(name)
 	if err != nil {
 		return nil, err
@@ -182,11 +207,73 @@ func (m *VolumeManager) CleanUpBackingImageInDisks(name string, disks []string) 
 	for _, r := range replicas {
 		disksInUse[r.Spec.DiskID] = struct{}{}
 	}
-	for _, id := range disks {
-		if _, exists := disksInUse[id]; exists {
-			return nil, fmt.Errorf("cannot clean up backing image %v in disk %v since there is at least one replica using it", name, id)
-		}
-		delete(bi.Spec.Disks, id)
+	if bids != nil && !bids.Spec.Started {
+		disksInUse[bids.Spec.DiskUUID] = struct{}{}
 	}
-	return m.ds.UpdateBackingImage(bi)
+	cleanupFileMap := map[string]struct{}{}
+	for _, diskUUID := range diskFileList {
+		if _, exists := disksInUse[diskUUID]; exists {
+			return nil, fmt.Errorf("cannot clean up backing image %v in disk %v since there is at least one replica using it", name, diskUUID)
+		}
+		if _, exists := bi.Spec.Disks[diskUUID]; !exists {
+			continue
+		}
+		delete(bi.Spec.Disks, diskUUID)
+		cleanupFileMap[diskUUID] = struct{}{}
+	}
+
+	var readyActiveFileCount, handlingActiveFileCount, failedActiveFileCount int
+	var readyCleanupFileCount, handlingCleanupFileCount, failedCleanupFileCount int
+	for diskUUID := range existingBI.Spec.Disks {
+		// Consider non-existing files as pending backing image files.
+		fileStatus, exists := bi.Status.DiskFileStatusMap[diskUUID]
+		if !exists {
+			fileStatus = &types.BackingImageDiskFileStatus{}
+		}
+		switch fileStatus.State {
+		case types.BackingImageStateReady:
+			if _, exists := cleanupFileMap[diskUUID]; !exists {
+				readyActiveFileCount++
+			} else {
+				readyCleanupFileCount++
+			}
+		case types.BackingImageStateFailed:
+			if _, exists := cleanupFileMap[diskUUID]; !exists {
+				failedActiveFileCount++
+			} else {
+				failedCleanupFileCount++
+			}
+		default:
+			if _, exists := cleanupFileMap[diskUUID]; !exists {
+				handlingActiveFileCount++
+			} else {
+				handlingCleanupFileCount++
+			}
+		}
+	}
+
+	// TODO: Make `haBackingImageCount` configure when introducing HA backing image feature
+	haBackingImageCount := 1
+	if haBackingImageCount <= readyActiveFileCount {
+		return bi, nil
+	}
+	if readyCleanupFileCount > 0 {
+		return nil, fmt.Errorf("failed to do cleanup since there will be no enough ready files for HA after the deletion")
+	}
+
+	if haBackingImageCount <= readyActiveFileCount+handlingCleanupFileCount {
+		return bi, nil
+	}
+	if handlingCleanupFileCount > 0 {
+		return nil, fmt.Errorf("failed to do cleanup since there will be no enough ready/in-progress/pending files for HA after the deletion")
+	}
+
+	if haBackingImageCount <= readyActiveFileCount+handlingCleanupFileCount+failedCleanupFileCount {
+		return bi, nil
+	}
+	if failedCleanupFileCount > 0 {
+		return nil, fmt.Errorf("cannot do cleanup since there are no enough files for HA")
+	}
+
+	return bi, nil
 }

longhorn/longhorn-manager

manager/backingimage.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
+	"reflect"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/longhorn/longhorn-manager/datastore"
@@ -168,12 +170,35 @@ func (m *VolumeManager) DeleteBackingImage(name string) error {
 	return nil
 }
 
-func (m *VolumeManager) CleanUpBackingImageInDisks(name string, disks []string) (*longhorn.BackingImage, error) {
-	defer logrus.Infof("Cleaning up backing image %v in disks %+v", name, disks)
-	bi, err := m.GetBackingImage(name)
+func (m *VolumeManager) CleanUpBackingImageDiskFiles(name string, diskFileList []string) (bi *longhorn.BackingImage, err error) {
+	defer logrus.Infof("Cleaning up backing image %v in diskFileList %+v", name, diskFileList)
+
+	bi, err = m.GetBackingImage(name)
 	if err != nil {
 		return nil, errors.Wrapf(err, "unable to get backing image %v", name)
 	}
+	if bi.DeletionTimestamp != nil {
+		logrus.Infof("Deleting backing image %v, there is no need to do disk cleanup for it", name)
+		return bi, nil
+	}
+	bids, err := m.GetBackingImageDataSource(name)
+	if err != nil {
+		if !apierrors.IsNotFound(err) {
+			return nil, errors.Wrapf(err, "unable to get backing image data source %v", name)
+		}
+		logrus.Warnf("Cannot find backing image data source %v, will ignore it and continue clean up", name)
+	}
+
+	existingBI := bi.DeepCopy()
+	defer func() {
+		if err == nil {
+			if !reflect.DeepEqual(bi.Spec, existingBI.Spec) {
+				bi, err = m.ds.UpdateBackingImage(bi)
+				return
+			}
+		}
+	}()
+
 	replicas, err := m.ds.ListReplicasByBackingImage(name)
 	if err != nil {
 		return nil, err
@@ -182,11 +207,73 @@ func (m *VolumeManager) CleanUpBackingImageInDisks(name string, disks []string) 
 	for _, r := range replicas {
 		disksInUse[r.Spec.DiskID] = struct{}{}
 	}
-	for _, id := range disks {
-		if _, exists := disksInUse[id]; exists {
-			return nil, fmt.Errorf("cannot clean up backing image %v in disk %v since there is at least one replica using it", name, id)
-		}
-		delete(bi.Spec.Disks, id)
+	if bids != nil && !bids.Spec.FileTransferred {
+		disksInUse[bids.Spec.DiskUUID] = struct{}{}
 	}
-	return m.ds.UpdateBackingImage(bi)
+	cleanupFileMap := map[string]struct{}{}
+	for _, diskUUID := range diskFileList {
+		if _, exists := disksInUse[diskUUID]; exists {
+			return nil, fmt.Errorf("cannot clean up backing image %v in disk %v since there is at least one replica using it", name, diskUUID)
+		}
+		if _, exists := bi.Spec.Disks[diskUUID]; !exists {
+			continue
+		}
+		delete(bi.Spec.Disks, diskUUID)
+		cleanupFileMap[diskUUID] = struct{}{}
+	}
+
+	var readyActiveFileCount, handlingActiveFileCount, failedActiveFileCount int
+	var readyCleanupFileCount, handlingCleanupFileCount, failedCleanupFileCount int
+	for diskUUID := range existingBI.Spec.Disks {
+		// Consider non-existing files as pending backing image files.
+		fileStatus, exists := bi.Status.DiskFileStatusMap[diskUUID]
+		if !exists {
+			fileStatus = &types.BackingImageDiskFileStatus{}
+		}
+		switch fileStatus.State {
+		case types.BackingImageStateReady:
+			if _, exists := cleanupFileMap[diskUUID]; !exists {
+				readyActiveFileCount++
+			} else {
+				readyCleanupFileCount++
+			}
+		case types.BackingImageStateFailed:
+			if _, exists := cleanupFileMap[diskUUID]; !exists {
+				failedActiveFileCount++
+			} else {
+				failedCleanupFileCount++
+			}
+		default:
+			if _, exists := cleanupFileMap[diskUUID]; !exists {
+				handlingActiveFileCount++
+			} else {
+				handlingCleanupFileCount++
+			}
+		}
+	}
+
+	// TODO: Make `haBackingImageCount` configure when introducing HA backing image feature
+	haBackingImageCount := 1
+	if haBackingImageCount <= readyActiveFileCount {
+		return bi, nil
+	}
+	if readyCleanupFileCount > 0 {
+		return nil, fmt.Errorf("failed to do cleanup since there will be no enough ready files for HA after the deletion")
+	}
+
+	if haBackingImageCount <= readyActiveFileCount+handlingCleanupFileCount {
+		return bi, nil
+	}
+	if handlingCleanupFileCount > 0 {
+		return nil, fmt.Errorf("failed to do cleanup since there will be no enough ready/in-progress/pending files for HA after the deletion")
+	}
+
+	if haBackingImageCount <= readyActiveFileCount+handlingCleanupFileCount+failedCleanupFileCount {
+		return bi, nil
+	}
+	if failedCleanupFileCount > 0 {
+		return nil, fmt.Errorf("cannot do cleanup since there are no enough files for HA")
+	}
+
+	return bi, nil
 }