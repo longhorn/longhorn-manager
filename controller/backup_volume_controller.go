3df7b91311fbe7936515a332d641b5c54cd5ae66
JenTing Hsiao <jenting.hsiao@suse.com>
2021-09-15 12:17:16 +0800

    Change to millisecond-level precision
    Change the BackupTarget/BackupVolume/Backup CR from second-level
    precision to millisecond-level precision.
    It fixes if more than two controllers set the `spec.syncRequestedAt`
    within a short time (within 1 second), the second one would not be handled.
    
    Signed-off-by: JenTing Hsiao <jenting.hsiao@suse.com>

--- controller/backup_volume_controller.go
+++ controller/backup_volume_controller.go
@@ -237,7 +237,7 @@ func (bvc *BackupVolumeController) reconcile(backupVolumeName string) (err error
 		return bvc.ds.RemoveFinalizerForBackupVolume(backupVolume)
 	}
 
-	syncTime := metav1.Time{Time: time.Now().UTC()}
+	syncTime := time.Now().UTC()
 	existingBackupVolume := backupVolume.DeepCopy()
 	defer func() {
 		if err != nil {
@@ -254,7 +254,7 @@ func (bvc *BackupVolumeController) reconcile(backupVolumeName string) (err error
 
 	// Check the controller should run synchronization
 	if !backupVolume.Status.LastSyncedAt.IsZero() &&
-		!backupVolume.Spec.SyncRequestedAt.After(backupVolume.Status.LastSyncedAt.Time) {
+		!backupVolume.Spec.SyncRequestedAt.After(backupVolume.Status.LastSyncedAt) {
 		return nil
 	}
 
