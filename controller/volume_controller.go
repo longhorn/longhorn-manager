92924ed5cc7b85592ae189ccfa6386a536a794ce
Shuo Wu <shuo.wu@suse.com>
2021-09-15 19:34:40 +0800

    controller: skip replica replenishment during detaching
    Otherwise, when there are not enough nodes for a attached volume,
    the volume controller will blindly create new replicas with
    scheduling failure then immediately fail & clean up those replicas
    during detachment.
    
    Longhorn 2460
    
    Signed-off-by: Shuo Wu <shuo.wu@suse.com>

--- controller/volume_controller.go
+++ controller/volume_controller.go
@@ -1486,6 +1486,10 @@ func (vc *VolumeController) replenishReplicas(v *longhorn.Volume, e *longhorn.En
 		return nil
 	}
 
+	if (len(rs) != 0) && v.Status.State != types.VolumeStateAttached {
+		return nil
+	}
+
 	if e == nil {
 		return fmt.Errorf("BUG: replenishReplica needs a valid engine")
 	}
@@ -1534,7 +1538,6 @@ func (vc *VolumeController) replenishReplicas(v *longhorn.Volume, e *longhorn.En
 				reusableFailedReplica.Name, vc.backoff.Get(reusableFailedReplica.Name).Seconds())
 		}
 		if vc.scheduler.RequireNewReplica(rs, v, hardNodeAffinity) {
-			log.Debugf("A new replica will be replenished during rebuilding")
 			if err := vc.createReplica(v, e, rs, hardNodeAffinity, !newVolume); err != nil {
 				return err
 			}
@@ -2030,6 +2033,8 @@ func (vc *VolumeController) createEngine(v *longhorn.Volume) (*longhorn.Engine, 
 }
 
 func (vc *VolumeController) createReplica(v *longhorn.Volume, e *longhorn.Engine, rs map[string]*longhorn.Replica, hardNodeAffinity string, isRebuildingReplica bool) error {
+	log := getLoggerForVolume(vc.logger, v)
+
 	replica := &longhorn.Replica{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:            types.GenerateReplicaNameForVolume(v.Name),
@@ -2050,6 +2055,7 @@ func (vc *VolumeController) createReplica(v *longhorn.Volume, e *longhorn.Engine
 		},
 	}
 	if isRebuildingReplica {
+		log.Debugf("A new replica %v will be replenished during rebuilding", replica.Name)
 		replica.Spec.RebuildRetryCount++
 	}
 
