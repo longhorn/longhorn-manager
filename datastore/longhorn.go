1fa428fe8f87c9e80980fd259fa3980e13ef118c
JenTing Hsiao <jenting.hsiao@suse.com>
2021-10-28 13:21:57 +0800

    controller: remove backupStatus from engine_controller
    Signed-off-by: JenTing Hsiao <jenting.hsiao@suse.com>

--- datastore/longhorn.go
+++ datastore/longhorn.go
@@ -2285,7 +2285,6 @@ func (s *DataStore) ResetMonitoringEngineStatus(e *longhorn.Engine) (*longhorn.E
 	e.Status.Endpoint = ""
 	e.Status.LastRestoredBackup = ""
 	e.Status.ReplicaModeMap = nil
-	e.Status.BackupStatus = nil
 	e.Status.RestoreStatus = nil
 	e.Status.PurgeStatus = nil
 	e.Status.RebuildStatus = nil
