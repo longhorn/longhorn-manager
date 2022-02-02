56ea1c0fc034b49a0a56bb3278b8a295be791962
Phan Le <phan.le@suse.com>
2022-02-02 13:28:23 -0800

    Expose controller workqueue metrics
    Longhorn-3083
    
    Signed-off-by: Phan Le <phan.le@suse.com>

--- api/router.go
+++ api/router.go
@@ -12,7 +12,7 @@ import (
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
-	"github.com/longhorn/longhorn-manager/monitoring"
+	"github.com/longhorn/longhorn-manager/monitoring/registry"
 )
 
 var (
@@ -49,7 +49,7 @@ func NewRouter(s *Server) *mux.Router {
 	versionsHandler := api.VersionsHandler(schemas, "v1")
 	versionHandler := api.VersionHandler(schemas, "v1")
 	r.Methods("GET").Path("/").Handler(versionsHandler)
-	r.Methods("GET").Path("/metrics").Handler(monitoring.Handler())
+	r.Methods("GET").Path("/metrics").Handler(registry.Handler())
 	r.Methods("GET").Path("/v1").Handler(versionHandler)
 	r.Methods("GET").Path("/v1/apiversions").Handler(versionsHandler)
 	r.Methods("GET").Path("/v1/apiversions/v1").Handler(versionHandler)
