4c2e9b292aca611f1bdf4b5e7075dfca1d098aca
Chin-Ya Huang <chin-ya.huang@suse.com>
2022-04-13 12:23:37 +0800

    cluster-autoscaler: annotate safe-to-evict to deployments
    Longhorn-2203
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- types/deploy.go
+++ types/deploy.go
@@ -4,6 +4,7 @@ const (
 	LonghornManagerDaemonSetName            = "longhorn-manager"
 	LonghornAdmissionWebhookDeploymentName  = "longhorn-admission-webhook"
 	LonghornConversionWebhookDeploymentName = "longhorn-conversion-webhook"
+	LonghornUIDeploymentName                = "longhorn-ui"
 
 	DriverDeployerName = "longhorn-driver-deployer"
 	CSIAttacherName    = "csi-attacher"
