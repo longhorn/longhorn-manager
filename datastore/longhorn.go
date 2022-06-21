3791c26b63fd35b7edaf2f799f5946189651026e
Derek Su <derek.su@suse.com>
2022-06-21 11:47:44 +0800

    Avoid validation failure while syncing configmap to setting CRs
    The validation failure while syncing configmap to setting CRs will lead to
    that the longhorn-manager cannot start successfully.
    To avoid the failure, the unavailable default settings are filtered and
    only sync the available ones.
    
    Longhorn 4160
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- datastore/longhorn.go
+++ datastore/longhorn.go
@@ -43,7 +43,7 @@ var (
 	VerificationRetryCounts = 20
 )
 
-func (s *DataStore) UpdateCustomizedSettings(defaultImage map[types.SettingName]string) error {
+func (s *DataStore) UpdateCustomizedSettings(defaultImages map[types.SettingName]string) error {
 	defaultSettingCM, err := s.GetConfigMap(s.namespace, types.DefaultDefaultSettingConfigMapName)
 	if err != nil {
 		return err
@@ -54,11 +54,13 @@ func (s *DataStore) UpdateCustomizedSettings(defaultImage map[types.SettingName]
 		return err
 	}
 
-	if err := s.applyCustomizedDefaultSettingsToDefinitions(customizedDefaultSettings); err != nil {
+	availableCustomizedDefaultSettings := s.filterCustomizedDefaultSettings(customizedDefaultSettings)
+
+	if err := s.applyCustomizedDefaultSettingsToDefinitions(availableCustomizedDefaultSettings); err != nil {
 		return err
 	}
 
-	if err := s.syncSettingsWithDefaultImages(defaultImage); err != nil {
+	if err := s.syncSettingsWithDefaultImages(defaultImages); err != nil {
 		return err
 	}
 
@@ -66,7 +68,7 @@ func (s *DataStore) UpdateCustomizedSettings(defaultImage map[types.SettingName]
 		return err
 	}
 
-	return s.syncSettingCRsWithCustomizedDefaultSettings(customizedDefaultSettings, defaultSettingCM.ResourceVersion)
+	return s.syncSettingCRsWithCustomizedDefaultSettings(availableCustomizedDefaultSettings, defaultSettingCM.ResourceVersion)
 }
 
 func (s *DataStore) createNonExistingSettingCRsWithDefaultSetting(configMapResourceVersion string) error {
@@ -95,8 +97,21 @@ func (s *DataStore) createNonExistingSettingCRsWithDefaultSetting(configMapResou
 	return nil
 }
 
-func (s *DataStore) syncSettingsWithDefaultImages(defaultImage map[types.SettingName]string) error {
-	for name, value := range defaultImage {
+func (s *DataStore) filterCustomizedDefaultSettings(customizedDefaultSettings map[string]string) map[string]string {
+	availableCustomizedDefaultSettings := make(map[string]string)
+
+	for name, value := range customizedDefaultSettings {
+		if err := s.ValidateSetting(string(name), value); err == nil {
+			availableCustomizedDefaultSettings[name] = value
+		} else {
+			logrus.WithError(err).Errorf("invalid customized default setting %v with value %v, will continue applying other customized settings", name, value)
+		}
+	}
+	return availableCustomizedDefaultSettings
+}
+
+func (s *DataStore) syncSettingsWithDefaultImages(defaultImages map[types.SettingName]string) error {
+	for name, value := range defaultImages {
 		if err := s.createOrUpdateSetting(name, value, ""); err != nil {
 			return err
 		}
@@ -105,11 +120,6 @@ func (s *DataStore) syncSettingsWithDefaultImages(defaultImage map[types.Setting
 }
 
 func (s *DataStore) createOrUpdateSetting(name types.SettingName, value, defaultSettingCMResourceVersion string) error {
-	err := s.ValidateSetting(string(name), value)
-	if err != nil {
-		return err
-	}
-
 	setting, err := s.GetSettingExact(name)
 	if err != nil {
 		if !ErrorIsNotFound(err) {
