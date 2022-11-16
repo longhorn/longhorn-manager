ceb63f701311debf68ff363b43b2c6426d88e925
Chin-Ya Huang <chin-ya.huang@suse.com>
2022-11-16 15:21:35 +0800

    feat(system-backup/restore): mutate SystemRestore
    Ref: 1455
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- webhook/server/mutation.go
+++ webhook/server/mutation.go
@@ -22,6 +22,7 @@ import (
 	"github.com/longhorn/longhorn-manager/webhook/resources/snapshot"
 	"github.com/longhorn/longhorn-manager/webhook/resources/supportbundle"
 	"github.com/longhorn/longhorn-manager/webhook/resources/systembackup"
+	"github.com/longhorn/longhorn-manager/webhook/resources/systemrestore"
 	"github.com/longhorn/longhorn-manager/webhook/resources/volume"
 )
 
@@ -44,6 +45,7 @@ func Mutation(client *client.Client) (http.Handler, []admission.Resource, error)
 		replica.NewMutator(client.Datastore),
 		supportbundle.NewMutator(client.Datastore),
 		systembackup.NewMutator(client.Datastore),
+		systemrestore.NewMutator(client.Datastore),
 	}
 
 	router := webhook.NewRouter()
