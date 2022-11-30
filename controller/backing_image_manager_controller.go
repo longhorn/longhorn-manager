07ac92debcc8817c0a7a9a591f0b0f26bde032f0
Chin-Ya Huang <chin-ya.huang@suse.com>
2022-11-30 08:54:00 +0800

    refactor: align ReadinessProbe time-relate settings
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>
    (cherry picked from commit d4d5f8943f62cfe84c4745fd93a796c3e0789033)

--- controller/backing_image_manager_controller.go
+++ controller/backing_image_manager_controller.go
@@ -818,6 +818,7 @@ func (c *BackingImageManagerController) generateBackingImageManagerPodManifest(b
 						InitialDelaySeconds: datastore.PodProbeInitialDelay,
 						TimeoutSeconds:      datastore.PodProbeTimeoutSeconds,
 						PeriodSeconds:       datastore.PodProbePeriodSeconds,
+						FailureThreshold:    datastore.PodLivenessProbeFailureThreshold,
 					},
 					VolumeMounts: []v1.VolumeMount{
 						{
