a35adb48d1bd8f010bb26fdbb2ef27fd66ad71e2
James Lu <james.lu@suse.com>
2023-02-14 08:53:41 +0800

    fix: incorrect router retry mechanism
    Keep the http request body and copy to the request body again when
    the retry occurred.
    
    Ref: 5259
    
    Signed-off-by: James Lu <james.lu@suse.com>

--- api/router.go
+++ api/router.go
@@ -2,38 +2,20 @@ package api
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/pkg/errors"
 	"github.com/rancher/go-rancher/api"
 	"github.com/rancher/go-rancher/client"
 	"github.com/sirupsen/logrus"
 
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-
 	"github.com/longhorn/longhorn-manager/metrics_collector/registry"
-)
-
-var (
-	RetryCounts   = 5
-	RetryInterval = 100 * time.Millisecond
 )
 
 type HandleFuncWithError func(http.ResponseWriter, *http.Request) error
 
 func HandleError(s *client.Schemas, t HandleFuncWithError) http.Handler {
 	return api.ApiHandler(s, http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		var err error
-		for i := 0; i < RetryCounts; i++ {
-			err = t(rw, req)
-			if !apierrors.IsConflict(errors.Cause(err)) {
-				break
-			}
-			logrus.WithError(err).Warn("Retry API call due to conflict")
-			time.Sleep(RetryInterval)
-		}
-		if err != nil {
+		if err := t(rw, req); err != nil {
 			logrus.Warnf("HTTP handling error %v", err)
 			apiContext := api.GetApiContext(req)
 			apiContext.WriteErr(err)
