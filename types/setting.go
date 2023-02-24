6c8250e1b99f69ff75c6f7b6d44d56a27e6f9ae8
James Lu <james.lu@suse.com>
2023-02-24 14:26:40 +0800

    feat(disk): add global setting
    Add a global setting 'storage-reserved-percentage` to reserved disk
    storage for better user experience when adding a newly created
    longhorn node's disk
    
    Default value is 30 for `storage-reserved-percentage`
    
    Ref: 4773
    
    Signed-off-by: James Lu <james.lu@suse.com>

--- types/setting.go
+++ types/setting.go
@@ -50,6 +50,7 @@ const (
 	SettingNameReplicaAutoBalance                                       = SettingName("replica-auto-balance")
 	SettingNameStorageOverProvisioningPercentage                        = SettingName("storage-over-provisioning-percentage")
 	SettingNameStorageMinimalAvailablePercentage                        = SettingName("storage-minimal-available-percentage")
+	SettingNameStorageReservedPercentage                                = SettingName("storage-reserved-percentage")
 	SettingNameUpgradeChecker                                           = SettingName("upgrade-checker")
 	SettingNameCurrentLonghornVersion                                   = SettingName("current-longhorn-version")
 	SettingNameLatestLonghornVersion                                    = SettingName("latest-longhorn-version")
@@ -121,6 +122,7 @@ var (
 		SettingNameReplicaAutoBalance,
 		SettingNameStorageOverProvisioningPercentage,
 		SettingNameStorageMinimalAvailablePercentage,
+		SettingNameStorageReservedPercentage,
 		SettingNameUpgradeChecker,
 		SettingNameCurrentLonghornVersion,
 		SettingNameLatestLonghornVersion,
@@ -217,6 +219,7 @@ var (
 		SettingNameReplicaAutoBalance:                                       SettingDefinitionReplicaAutoBalance,
 		SettingNameStorageOverProvisioningPercentage:                        SettingDefinitionStorageOverProvisioningPercentage,
 		SettingNameStorageMinimalAvailablePercentage:                        SettingDefinitionStorageMinimalAvailablePercentage,
+		SettingNameStorageReservedPercentage:                                SettingDefinitionStorageReservedPercentage,
 		SettingNameUpgradeChecker:                                           SettingDefinitionUpgradeChecker,
 		SettingNameCurrentLonghornVersion:                                   SettingDefinitionCurrentLonghornVersion,
 		SettingNameLatestLonghornVersion:                                    SettingDefinitionLatestLonghornVersion,
@@ -460,6 +463,16 @@ var (
 		Required:    true,
 		ReadOnly:    false,
 		Default:     "25",
+	}
+
+	SettingDefinitionStorageReservedPercentage = SettingDefinition{
+		DisplayName: "Storage Reserved Percentage",
+		Description: "The reserved percentage determines what percentage of disk space will not be used on a newly created Longhorn node.",
+		Category:    SettingCategoryScheduling,
+		Type:        SettingTypeInt,
+		Required:    true,
+		ReadOnly:    false,
+		Default:     "30",
 	}
 
 	SettingDefinitionUpgradeChecker = SettingDefinition{
@@ -1172,6 +1185,8 @@ func ValidateSetting(name, value string) (err error) {
 		if value < 0 {
 			return fmt.Errorf("value %v should be positive", value)
 		}
+	case SettingNameStorageReservedPercentage:
+		fallthrough
 	case SettingNameStorageMinimalAvailablePercentage:
 		if _, err := strconv.Atoi(value); err != nil {
 			return errors.Wrapf(err, "value %v is not a number", value)
