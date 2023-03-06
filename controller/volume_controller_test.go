4e3412f52e4c9b2d6dcdbc730c28109ce5ff44c2
Derek Su <derek.su@suse.com>
2023-03-06 10:59:45 +0800

    test: update TestVolumeLifeCycle
    Longhorn 5464
    Longhorn 5477
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- controller/volume_controller_test.go
+++ controller/volume_controller_test.go
@@ -807,6 +807,7 @@ func (s *TestSuite) TestVolumeLifeCycle(c *C) {
 	for _, r := range tc.expectReplicas {
 		if r.Spec.NodeID == TestNode2 {
 			r.Spec.DesireState = longhorn.InstanceStateStopped
+			r.Spec.FailedAt = getTestNow()
 		} else {
 			r.Spec.DesireState = longhorn.InstanceStateRunning
 		}
