365896058d07da750692328b0c6d5a94c7079da0
James Lu <james.lu@suse.com>
2023-03-20 15:44:03 +0800

    fix(recurring job): new job type `backup-force-create`
    Introduce a new recurring job type `backup-force-create` to
    take a new snapshot and do the backup forcibly when cleaning up
    old snapshots failed.
    
    Ref: 4898
    
    Signed-off-by: James Lu <james.lu@suse.com>
    (cherry picked from commit b147011cf12192b361e8bb31573903b29b393fab)

--- datastore/longhorn.go
+++ datastore/longhorn.go
@@ -3774,6 +3774,7 @@ func ValidateRecurringJob(job longhorn.RecurringJobSpec) error {
 
 func isValidRecurringJobTask(task longhorn.RecurringJobType) bool {
 	return task == longhorn.RecurringJobTypeBackup ||
+		task == longhorn.RecurringJobTypeBackupForceCreate ||
 		task == longhorn.RecurringJobTypeSnapshot ||
 		task == longhorn.RecurringJobTypeSnapshotForceCreate ||
 		task == longhorn.RecurringJobTypeSnapshotCleanup ||
