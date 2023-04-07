bc66ef8e3f93b9e091adff50166e42ec46a5a822
Eric Weber <eric.weber@suse.com>
2023-04-07 16:10:48 -0500

    Bump CSI sidecars for Longhorn v1.3 and v1.4
    Signed-off-by: Eric Weber <eric.weber@suse.com>

--- csi/deployment.go
+++ csi/deployment.go
@@ -21,7 +21,7 @@ import (
 
 const (
 	DefaultCSIAttacherImage            = "longhornio/csi-attacher:v4.2.0"
-	DefaultCSIProvisionerImage         = "longhornio/csi-provisioner:v2.1.2"
+	DefaultCSIProvisionerImage         = "longhornio/csi-provisioner:v2.2.2"
 	DefaultCSIResizerImage             = "longhornio/csi-resizer:v1.7.0"
 	DefaultCSISnapshotterImage         = "longhornio/csi-snapshotter:v3.0.3"
 	DefaultCSINodeDriverRegistrarImage = "longhornio/csi-node-driver-registrar:v2.7.0"
