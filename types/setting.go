f11440b14477ce2271354553a38631371db75aab
Phan Le <phan.le@suse.com>
2023-04-16 22:08:25 -0700

    Mark the setting allow-node-drain-with-last-healthy-replica as deprecated
    longhorn-5585
    
    Signed-off-by: Phan Le <phan.le@suse.com>

--- types/setting.go
+++ types/setting.go
@@ -593,7 +593,7 @@ var (
 	}
 
 	SettingDefinitionAllowNodeDrainWithLastHealthyReplica = SettingDefinition{
-		DisplayName: "Allow Node Drain with the Last Healthy Replica",
+		DisplayName: "Allow Node Drain with the Last Healthy Replica (Deprecated)",
 		Description: "By default, Longhorn will block `kubectl drain` action on a node if the node contains the last healthy replica of a volume.\n\n" +
 			"If this setting is enabled, Longhorn will **not** block `kubectl drain` action on a node even if the node contains the last healthy replica of a volume.",
 		Category: SettingCategoryGeneral,
