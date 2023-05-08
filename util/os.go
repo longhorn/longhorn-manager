083f4f32a7fcceab60ca5715aec93c9512d0a3da
Chin-Ya Huang <chin-ya.huang@suse.com>
2023-05-08 13:26:07 +0800

    feat(upgrade-checker): collect OS distro
    Ref: 5235
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- util/os.go
+++ util/os.go
@@ -1,9 +1,17 @@
 package util
 
 import (
+	"bufio"
 	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
 
 	iscsiutil "github.com/longhorn/go-iscsi-helper/util"
+)
+
+const (
+	OsReleasePath = "/etc/os-release"
 )
 
 // GetHostKernelRelease retrieves the kernel release version of the host.
@@ -15,4 +23,24 @@ func GetHostKernelRelease() (string, error) {
 		return "", err
 	}
 	return RemoveNewlines(output), nil
+}
+
+// GetHostOSDistro retrieves the operating system distribution of the host.
+func GetHostOSDistro() (string, error) {
+	initiatorNSPath := iscsiutil.GetHostNamespacePath(HostProcPath)
+	mountPath := fmt.Sprintf("--mount=%s/mnt", initiatorNSPath)
+	output, err := Execute([]string{}, "nsenter", mountPath, "cat", OsReleasePath)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to read %v on host", OsReleasePath)
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(string(output)))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "ID=") {
+			osDistro := RemoveNewlines(strings.TrimPrefix(line, "ID="))
+			return strings.ReplaceAll(osDistro, `"`, ""), nil
+		}
+	}
+	return "", fmt.Errorf("failed to find ID field in %v", OsReleasePath)
 }
