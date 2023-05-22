04d7d3d6f7d724407d0b6c9b9e59799aa9c73215
Chin-Ya Huang <chin-ya.huang@suse.com>
2023-05-22 19:18:44 +0800

    fix(replica-auto-balance): filter node-selector
    ref: 5971
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- controller/volume_controller.go
+++ controller/volume_controller.go
@@ -2145,7 +2145,7 @@ func (vc *VolumeController) getReplicaCountForAutoBalanceBestEffort(v *longhorn.
 func (vc *VolumeController) getReplicaCountForAutoBalanceZone(v *longhorn.Volume, e *longhorn.Engine, rs map[string]*longhorn.Replica) (int, map[string][]string, error) {
 	log := getLoggerForVolume(vc.logger, v).WithField("replicaAutoBalanceType", "zone")
 
-	readyNodes, err := vc.listReadySchedulableAndScheduledNodes(rs, log)
+	readyNodes, err := vc.listReadySchedulableAndScheduledNodes(v, rs, log)
 	if err != nil {
 		return 0, nil, err
 	}
@@ -2249,10 +2249,19 @@ func (vc *VolumeController) getReplicaCountForAutoBalanceZone(v *longhorn.Volume
 	return adjustCount, zoneExtraRs, err
 }
 
-func (vc *VolumeController) listReadySchedulableAndScheduledNodes(rs map[string]*longhorn.Replica, log logrus.FieldLogger) (map[string]*longhorn.Node, error) {
+func (vc *VolumeController) listReadySchedulableAndScheduledNodes(volume *longhorn.Volume, rs map[string]*longhorn.Replica, log logrus.FieldLogger) (map[string]*longhorn.Node, error) {
 	readyNodes, err := vc.ds.ListReadyAndSchedulableNodes()
 	if err != nil {
 		return nil, err
+	}
+
+	filteredReadyNodes := readyNodes
+	if len(volume.Spec.NodeSelector) != 0 {
+		for nodeName, node := range readyNodes {
+			if !util.IsTagsEqual(node.Spec.Tags, volume.Spec.NodeSelector) {
+				delete(filteredReadyNodes, nodeName)
+			}
+		}
 	}
 
 	// Including unschedulable node because the replica is already scheduled and running
@@ -2262,7 +2271,7 @@ func (vc *VolumeController) listReadySchedulableAndScheduledNodes(rs map[string]
 			continue
 		}
 
-		_, exist := readyNodes[r.Spec.NodeID]
+		_, exist := filteredReadyNodes[r.Spec.NodeID]
 		if exist {
 			continue
 		}
@@ -2281,20 +2290,20 @@ func (vc *VolumeController) listReadySchedulableAndScheduledNodes(rs map[string]
 			continue
 		}
 
-		readyNodes[r.Spec.NodeID] = node
+		filteredReadyNodes[r.Spec.NodeID] = node
 		log.WithFields(logrus.Fields{
 			"replica": r.Name,
 			"node":    node.Name,
 		}).Debugf("Including unschedulable node because the replica is scheduled and running")
 	}
 
-	return readyNodes, nil
+	return filteredReadyNodes, nil
 }
 
 func (vc *VolumeController) getReplicaCountForAutoBalanceNode(v *longhorn.Volume, e *longhorn.Engine, rs map[string]*longhorn.Replica) (int, map[string][]string, error) {
 	log := getLoggerForVolume(vc.logger, v).WithField("replicaAutoBalanceType", "node")
 
-	readyNodes, err := vc.listReadySchedulableAndScheduledNodes(rs, log)
+	readyNodes, err := vc.listReadySchedulableAndScheduledNodes(v, rs, log)
 	if err != nil {
 		return 0, nil, err
 	}
