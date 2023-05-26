09d0f82af99cc9e8aadefdca07579bebe9329330
Chin-Ya Huang <chin-ya.huang@suse.com>
2023-05-26 09:22:02 +0800

    test: IsSelectorInTag
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>
    (cherry picked from commit a481876af5ced7fa1e8a734f22d66fb741a4d0c0)

--- types/types_test.go
+++ types/types_test.go
@@ -5,8 +5,27 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/sirupsen/logrus"
+
 	corev1 "k8s.io/api/core/v1"
+
+	. "gopkg.in/check.v1"
 )
+
+const (
+	TestErrResultFmt = "Unexpected result for test case: %s"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type TestSuite struct {
+}
+
+var _ = Suite(&TestSuite{})
+
+func (s *TestSuite) SetUpTest(c *C) {
+	logrus.SetLevel(logrus.DebugLevel)
+}
 
 func TestParseToleration(t *testing.T) {
 	type testCase struct {
@@ -98,5 +117,48 @@ func TestParseToleration(t *testing.T) {
 		if test.expectError && err == nil {
 			t.Errorf("unexpected error: %v", err)
 		}
+	}
+}
+
+func (s *TestSuite) TestIsSelectorsInTags(c *C) {
+	type testCase struct {
+		inputTags      []string
+		inputSelectors []string
+
+		expected bool
+	}
+	testCases := map[string]testCase{
+		"selectors exist": {
+			inputTags:      []string{"aaa", "bbb", "ccc"},
+			inputSelectors: []string{"aaa", "bbb", "ccc"},
+			expected:       true,
+		},
+		"selectors mis-matched": {
+			inputTags:      []string{"aaa", "bbb", "ccc"},
+			inputSelectors: []string{"aaa", "b", "ccc"},
+			expected:       false,
+		},
+		"selectors empty": {
+			inputTags:      []string{"aaa", "bbb", "ccc"},
+			inputSelectors: []string{},
+			expected:       true,
+		},
+		"tags unsorted": {
+			inputTags:      []string{"bbb", "aaa", "ccc"},
+			inputSelectors: []string{"aaa", "bbb", "ccc"},
+			expected:       true,
+		},
+		"tags empty": {
+			inputTags:      []string{},
+			inputSelectors: []string{"aaa", "bbb", "ccc"},
+			expected:       false,
+		},
+	}
+
+	for testName, testCase := range testCases {
+		fmt.Printf("testing %v\n", testName)
+
+		actual := IsSelectorsInTags(testCase.inputTags, testCase.inputSelectors)
+		c.Assert(actual, Equals, testCase.expected, Commentf(TestErrResultFmt, testName))
 	}
 }
