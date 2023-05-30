9b2e8b8146c785a6ddc2ad0dbc38fefef9b03d9b
Derek Su <derek.su@suse.com>
2023-05-31 07:42:24 +0800

    spdk/webhook: mutate and validate resources
    Signed-off-by: Derek Su <derek.su@suse.com>

---
+++ webhook/resources/replica/validator.go
@@ -0,0 +1,70 @@
+package replica
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+
+	admissionregv1 "k8s.io/api/admissionregistration/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/longhorn/longhorn-manager/datastore"
+	longhorn "github.com/longhorn/longhorn-manager/k8s/pkg/apis/longhorn/v1beta2"
+	"github.com/longhorn/longhorn-manager/types"
+	"github.com/longhorn/longhorn-manager/webhook/admission"
+	werror "github.com/longhorn/longhorn-manager/webhook/error"
+)
+
+type replicaValidator struct {
+	admission.DefaultValidator
+	ds *datastore.DataStore
+}
+
+func NewValidator(ds *datastore.DataStore) admission.Validator {
+	return &replicaValidator{ds: ds}
+}
+
+func (r *replicaValidator) Resource() admission.Resource {
+	return admission.Resource{
+		Name:       "replicas",
+		Scope:      admissionregv1.NamespacedScope,
+		APIGroup:   longhorn.SchemeGroupVersion.Group,
+		APIVersion: longhorn.SchemeGroupVersion.Version,
+		ObjectType: &longhorn.Replica{},
+		OperationTypes: []admissionregv1.OperationType{
+			admissionregv1.Create,
+			admissionregv1.Update,
+		},
+	}
+}
+
+func (r *replicaValidator) Create(request *admission.Request, newObj runtime.Object) error {
+	replica := newObj.(*longhorn.Replica)
+
+	if replica.Spec.BackendStoreDriver == longhorn.BackendStoreDriverTypeSPDK {
+		spdkEnabled, err := r.ds.GetSettingAsBool(types.SettingNameSpdk)
+		if err != nil {
+			err = errors.Wrapf(err, "failed to get spdk setting")
+			return werror.NewInvalidError(err.Error(), "")
+		}
+		if !spdkEnabled {
+			return werror.NewInvalidError("SPDK data engine is not enabled", "")
+		}
+	}
+
+	return nil
+}
+
+func (r *replicaValidator) Update(request *admission.Request, oldObj runtime.Object, newObj runtime.Object) error {
+	oldReplica := oldObj.(*longhorn.Replica)
+	newReplica := newObj.(*longhorn.Replica)
+
+	if oldReplica.Spec.BackendStoreDriver != "" {
+		if oldReplica.Spec.BackendStoreDriver != newReplica.Spec.BackendStoreDriver {
+			err := fmt.Errorf("changing backend store driver for replica %v is not supported", oldReplica.Name)
+			return werror.NewInvalidError(err.Error(), "")
+		}
+	}
+
+	return nil
+}
