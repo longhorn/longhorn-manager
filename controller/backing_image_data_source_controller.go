d7048a86690d26d3d13a59bd9f69a018509b9e08
Eric Weber <eric.weber@suse.com>
2023-06-15 15:53:14 -0500

    Run backing-image-manager and backing-image-ds pods as privileged
    Signed-off-by: Eric Weber <eric.weber@suse.com>
    (cherry picked from commit f8f84baf00701072f4d091ecc73baf438954b7a8)

--- controller/backing_image_data_source_controller.go
+++ controller/backing_image_data_source_controller.go
@@ -611,6 +611,7 @@ func (c *BackingImageDataSourceController) generateBackingImageDataSourcePodMani
 		cmd = append(cmd, "--checksum", bids.Spec.Checksum)
 	}
 
+	privileged := true
 	podSpec := &v1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:            types.GetBackingImageDataSourcePodName(bids.Name),
@@ -656,6 +657,9 @@ func (c *BackingImageDataSourceController) generateBackingImageDataSourcePodMani
 								},
 							},
 						},
+					},
+					SecurityContext: &v1.SecurityContext{
+						Privileged: &privileged,
 					},
 				},
 			},
