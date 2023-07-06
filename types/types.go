3ea2dcdac678fe13c42880fda33d980db2561a80
Derek Su <derek.su@suse.com>
2023-07-06 12:06:55 +0800

    Fix strict-local volume restore stuck in attaching state
    For a volume being restored, the spec.nodeID is empyty but the status.ownerID is set.
    Thus, set r.Spec.HardNodeAffinity to volume.status.ownerID instead.
    
    Longhorn 6239
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- types/types.go
+++ types/types.go
@@ -239,7 +239,7 @@ const (
 )
 
 // SettingsRelatedToVolume should match the items in datastore.GetLabelsForVolumesFollowsGlobalSettings
-//   TODO: May need to add the data locality check
+// TODO: May need to add the data locality check
 var SettingsRelatedToVolume = map[string]string{
 	string(SettingNameReplicaAutoBalance):                  LonghornLabelValueIgnored,
 	string(SettingNameSnapshotDataIntegrity):               LonghornLabelValueIgnored,
