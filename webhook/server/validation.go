36c44cb97b50e8c4103ffe6e3cb955f628bc7b29
Eric Weber <eric.weber@suse.com>
2023-09-18 14:18:02 -0500

    Refuse to create a duplicate engine
    Longhorn 6682
    
    Signed-off-by: Eric Weber <eric.weber@suse.com>
    Signed-off-by: Phan Le <phan.le@suse.com>
    (cherry picked from commit 22e1e12a4c3bd086f8ec47b6df4e56b727e185f9)

--- webhook/server/validation.go
+++ webhook/server/validation.go
@@ -10,6 +10,7 @@ import (
 	"github.com/longhorn/longhorn-manager/util/client"
 	"github.com/longhorn/longhorn-manager/webhook/admission"
 	"github.com/longhorn/longhorn-manager/webhook/resources/backingimage"
+	"github.com/longhorn/longhorn-manager/webhook/resources/engine"
 	"github.com/longhorn/longhorn-manager/webhook/resources/node"
 	"github.com/longhorn/longhorn-manager/webhook/resources/orphan"
 	"github.com/longhorn/longhorn-manager/webhook/resources/recurringjob"
@@ -39,6 +40,7 @@ func Validation(client *client.Client) (http.Handler, []admission.Resource, erro
 		supportbundle.NewValidator(client.Datastore),
 		systembackup.NewValidator(client.Datastore),
 		systemrestore.NewValidator(client.Datastore),
+		engine.NewValidator(client.Datastore),
 	}
 
 	router := webhook.NewRouter()
