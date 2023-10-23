f5796176881e382a21f9af7e5463ad0ea094747c
Derek Su <derek.su@suse.com>
2023-10-23 16:23:56 +0800

    Eliminate multiple client creations
    Longhorn 6866
    Longhorn 6936
    
    Signed-off-by: Derek Su <derek.su@suse.com>
    (cherry picked from commit 5e0868b84aca3cc601ad00cc0e37f8bf01d3efd6)

--- app/recovery_backend.go
+++ app/recovery_backend.go
@@ -1,40 +1,18 @@
 package app
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/sirupsen/logrus"
 
-	"k8s.io/client-go/tools/clientcmd"
-
 	"github.com/longhorn/longhorn-manager/recovery_backend/server"
-	"github.com/longhorn/longhorn-manager/types"
-	"github.com/longhorn/longhorn-manager/util"
 	"github.com/longhorn/longhorn-manager/util/client"
 )
 
-func startRecoveryBackend(ctx context.Context, serviceAccount, kubeconfigPath string) error {
+func startRecoveryBackend(clients *client.Clients) error {
 	logrus.Info("Starting longhorn recovery-backend server")
 
-	namespace := util.GetNamespace(types.EnvPodNamespace)
-
-	cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
-	if err != nil {
-		return fmt.Errorf("failed to get client config: %v", err)
-	}
-
-	client, err := client.NewClient(ctx, cfg, namespace, true)
-	if err != nil {
-		return err
-	}
-
-	if err := client.Start(ctx); err != nil {
-		return err
-	}
-
-	s := server.New(namespace, client.Datastore)
+	s := server.New(clients.Namespace, clients.Datastore)
 	router := http.Handler(server.NewRouter(s))
 	go func() {
 		if err := s.ListenAndServe(router); err != nil {
--- webhook/server/mutation.go
+++ webhook/server/mutation.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/rancher/wrangler/pkg/webhook"
 
-	"github.com/longhorn/longhorn-manager/util/client"
+	"github.com/longhorn/longhorn-manager/datastore"
 	"github.com/longhorn/longhorn-manager/webhook/admission"
 	"github.com/longhorn/longhorn-manager/webhook/resources/backingimage"
 	"github.com/longhorn/longhorn-manager/webhook/resources/backingimagedatasource"
@@ -26,26 +26,26 @@ import (
 	"github.com/longhorn/longhorn-manager/webhook/resources/volumeattachment"
 )
 
-func Mutation(client *client.Client) (http.Handler, []admission.Resource, error) {
+func Mutation(ds *datastore.DataStore) (http.Handler, []admission.Resource, error) {
 	resources := []admission.Resource{}
 	mutators := []admission.Mutator{
-		backup.NewMutator(client.Datastore),
-		backingimage.NewMutator(client.Datastore),
-		backingimagemanager.NewMutator(client.Datastore),
-		backingimagedatasource.NewMutator(client.Datastore),
-		node.NewMutator(client.Datastore),
-		volume.NewMutator(client.Datastore),
-		engine.NewMutator(client.Datastore),
-		recurringjob.NewMutator(client.Datastore),
-		engineimage.NewMutator(client.Datastore),
-		orphan.NewMutator(client.Datastore),
-		sharemanager.NewMutator(client.Datastore),
-		backupvolume.NewMutator(client.Datastore),
-		snapshot.NewMutator(client.Datastore),
-		replica.NewMutator(client.Datastore),
-		supportbundle.NewMutator(client.Datastore),
-		systembackup.NewMutator(client.Datastore),
-		volumeattachment.NewMutator(client.Datastore),
+		backup.NewMutator(ds),
+		backingimage.NewMutator(ds),
+		backingimagemanager.NewMutator(ds),
+		backingimagedatasource.NewMutator(ds),
+		node.NewMutator(ds),
+		volume.NewMutator(ds),
+		engine.NewMutator(ds),
+		recurringjob.NewMutator(ds),
+		engineimage.NewMutator(ds),
+		orphan.NewMutator(ds),
+		sharemanager.NewMutator(ds),
+		backupvolume.NewMutator(ds),
+		snapshot.NewMutator(ds),
+		replica.NewMutator(ds),
+		supportbundle.NewMutator(ds),
+		systembackup.NewMutator(ds),
+		volumeattachment.NewMutator(ds),
 	}
 
 	router := webhook.NewRouter()
