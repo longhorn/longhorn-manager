5168fc386857cf59d8908c617642717be908193b
Chin-Ya Huang <chin-ya.huang@suse.com>
2023-10-26 11:56:55 +0800

    fix: add trace log to invalid state error
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>
    (cherry picked from commit ea219f04a7a48f79616879502f5ecf78edc7a3b5)
    
    Fix backport conflicts.
    
    Signed-off-by: James Munson <james.munson@suse.com>

--- controller/utils.go
+++ controller/utils.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"github.com/longhorn/longhorn-manager/types"
 	"github.com/sirupsen/logrus"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -8,6 +9,7 @@ import (
 
 func handleReconcileErrorLogging(logger logrus.FieldLogger, err error, mesg string) {
 	if types.ErrorIsInvalidState(err) {
+		logger.WithError(err).Trace(mesg)
 		return
 	}
 
