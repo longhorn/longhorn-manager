2c68a7abfe0e8c8991914a61dc8d81d5924d2567
davidko <dko@suse.com>
2023-12-12 23:15:27 +0800

    refactor: move non-entry app functions away from app pkg
    Signed-off-by: davidko <dko@suse.com>
    (cherry picked from commit ac3a130101fc0a660e0ea57dc8d73a00b02e4e3b)

--- recovery_backend/recovery_backend.go
+++ recovery_backend/recovery_backend.go
@@ -1,4 +1,4 @@
-package app
+package recoverybackend
 
 import (
 	"net/http"
@@ -9,7 +9,7 @@ import (
 	"github.com/longhorn/longhorn-manager/util/client"
 )
 
-func startRecoveryBackend(clients *client.Clients) error {
+func StartRecoveryBackend(clients *client.Clients) error {
 	logrus.Info("Starting longhorn recovery-backend server")
 
 	s := server.New(clients.Namespace, clients.Datastore)
