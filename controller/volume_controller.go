bbbaa0e35626256e80853b46b388493bc4e2cf76
James Lu <james.lu@suse.com>
2023-12-18 16:41:18 +0800

    feat(backup): methods to get backup volume CRs
    List backup volume CRs by the label `backup-target` that is a backup
    target name and get last updated backup volume CR by a volume name.
    
    Ref: 5411
    
    Signed-off-by: James Lu <james.lu@suse.com>

--- controller/volume_controller.go
+++ controller/volume_controller.go
@@ -4264,7 +4264,7 @@ func (c *VolumeController) ReconcileBackupVolumeState(volume *longhorn.Volume) e
 		backupVolumeName = volume.Name
 	}
 
-	bv, err := c.ds.GetBackupVolumeRO(backupVolumeName)
+	bv, err := c.ds.GetLastUpdatedBackupVolumeWithVolumeNameRO(backupVolumeName)
 	if err != nil && !apierrors.IsNotFound(err) {
 		return errors.Wrapf(err, "failed to get backup volume %s for volume %v", backupVolumeName, volume.Name)
 	}
--- types/types.go
+++ types/types.go
@@ -160,6 +160,7 @@ const (
 	LonghornLabelManagedBy                  = "managed-by"
 	LonghornLabelSnapshotForCloningVolume   = "for-cloning-volume"
 	LonghornLabelBackingImageDataSource     = "backing-image-data-source"
+	LonghornLabelBackupTarget               = "backup-target"
 	LonghornLabelBackupVolume               = "backup-volume"
 	LonghornLabelRecurringJob               = "job"
 	LonghornLabelRecurringJobGroup          = "job-group"
@@ -516,6 +517,12 @@ func GetBackingImageDataSourceLabels(name, nodeID, diskUUID string) map[string]s
 		labels[GetLonghornLabelKey(LonghornLabelNode)] = nodeID
 	}
 	return labels
+}
+
+func GetBackupTargetLabels(backupTargetName string) map[string]string {
+	return map[string]string{
+		LonghornLabelBackupTarget: backupTargetName,
+	}
 }
 
 func GetBackupVolumeLabels(volumeName string) map[string]string {
