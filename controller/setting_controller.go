30ac1a36baaa1d9499792ecc7f46992da99e0872
Chin-Ya Huang <chin-ya.huang@suse.com>
2023-12-20 13:10:24 +0800

    fix(telemetry): wrong average volume sizes due to v2 volume counts
    ref: longhorn-7380
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- controller/setting_controller.go
+++ controller/setting_controller.go
@@ -1541,6 +1541,12 @@ func (info *ClusterInfo) collectVolumesInfo() error {
 		return errors.Wrapf(err, "failed to list Longhorn Volumes")
 	}
 	volumeCount := len(volumesRO)
+	volumeCountV1 := 0
+	for _, volume := range volumesRO {
+		if volume.Spec.BackendStoreDriver == longhorn.BackendStoreDriverTypeV1 {
+			volumeCountV1++
+		}
+	}
 
 	var totalVolumeSize int
 	var totalVolumeActualSize int
@@ -1565,6 +1571,8 @@ func (info *ClusterInfo) collectVolumesInfo() error {
 		}
 		backendStoreDriverCountStruct[util.StructName(fmt.Sprintf(ClusterInfoVolumeBackendStoreDriverCountFmt, backendStoreDriver))]++
 
+		// TODO: Remove this condition when v2 volume actual size is implemented.
+		//       https://github.com/longhorn/longhorn/issues/5947
 		isVolumeUsingV2DataEngine := volume.Spec.BackendStoreDriver == longhorn.BackendStoreDriverTypeV2
 		if !isVolumeUsingV2DataEngine {
 			totalVolumeSize += int(volume.Spec.Size)
@@ -1626,19 +1634,21 @@ func (info *ClusterInfo) collectVolumesInfo() error {
 	info.structFields.fields.AppendCounted(snapshotDataIntegrityCountStruct)
 	info.structFields.fields.AppendCounted(unmapMarkSnapChainRemovedCountStruct)
 
-	var avgVolumeSnapshotCount int
+	// TODO: Use the total volume count instead when v2 volume actual size is implemented.
+	//       https://github.com/longhorn/longhorn/issues/5947
 	var avgVolumeSize int
 	var avgVolumeActualSize int
+	if volumeCountV1 > 0 && totalVolumeSize > 0 {
+		avgVolumeSize = totalVolumeSize / volumeCountV1
+
+		if totalVolumeActualSize > 0 {
+			avgVolumeActualSize = totalVolumeActualSize / volumeCountV1
+		}
+	}
+
+	var avgVolumeSnapshotCount int
 	var avgVolumeNumOfReplicas int
 	if volumeCount > 0 {
-		if totalVolumeSize > 0 {
-			avgVolumeSize = totalVolumeSize / volumeCount
-
-			if totalVolumeActualSize > 0 {
-				avgVolumeActualSize = totalVolumeActualSize / volumeCount
-			}
-		}
-
 		avgVolumeNumOfReplicas = totalVolumeNumOfReplicas / volumeCount
 
 		snapshotsRO, err := info.ds.ListSnapshotsRO(labels.Everything())
