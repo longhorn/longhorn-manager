3d87000f687c3e29f2abb685e941dc152e8d3ce7
Eric Weber <eric.weber@suse.com>
2024-01-16 14:00:02 -0600

    Add upgrade logic for LastFailedAt and LastHealthyAt
    Longhorn 7425
    
    Signed-off-by: Eric Weber <eric.weber@suse.com>
    (cherry picked from commit 8df86d9d3e7b20ad5976d2e3438dafadd71fa40f)

---
+++ upgrade/v16xto170/upgrade.go
@@ -0,0 +1,56 @@
+package v16xto170
+
+import (
+	"github.com/pkg/errors"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	clientset "k8s.io/client-go/kubernetes"
+
+	lhclientset "github.com/longhorn/longhorn-manager/k8s/pkg/client/clientset/versioned"
+	upgradeutil "github.com/longhorn/longhorn-manager/upgrade/util"
+)
+
+const (
+	upgradeLogPrefix = "upgrade from v1.6.x to v1.7.0: "
+)
+
+func UpgradeResources(namespace string, lhClient *lhclientset.Clientset, kubeClient *clientset.Clientset, resourceMaps map[string]interface{}) error {
+	// We will probably need to upgrade other resources as well. See upgradeReplicas or previous Longhorn versions for
+	// examples.
+	return upgradeReplicas(namespace, lhClient, resourceMaps)
+}
+
+func UpgradeResourcesStatus(namespace string, lhClient *lhclientset.Clientset, kubeClient *clientset.Clientset, resourceMaps map[string]interface{}) error {
+	// Currently there are no statuses to upgrade. See UpgradeResources -> upgradeVolumes or previous Longhorn versions
+	// for examples.
+	return nil
+}
+
+func upgradeReplicas(namespace string, lhClient *lhclientset.Clientset, resourceMaps map[string]interface{}) (err error) {
+	defer func() {
+		err = errors.Wrapf(err, upgradeLogPrefix+"upgrade replica failed")
+	}()
+
+	replicaMap, err := upgradeutil.ListAndUpdateReplicasInProvidedCache(namespace, lhClient, resourceMaps)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil
+		}
+		return errors.Wrapf(err, "failed to list all existing Longhorn replicas during the replica upgrade")
+	}
+
+	for _, r := range replicaMap {
+		if r.Spec.LastHealthyAt == "" {
+			// We could attempt to figure out if the replica is currently RW in an engine and set its
+			// Spec.LastHealthyAt = now, but it is safer and easier to start updating it after the upgrade.
+			r.Spec.LastHealthyAt = r.Spec.HealthyAt
+		}
+		if r.Spec.LastFailedAt == "" {
+			// There is no way for us to know the right time for Spec.LastFailedAt if the replica isn't currently
+			// failed. Start updating it after the upgrade.
+			r.Spec.LastFailedAt = r.Spec.FailedAt
+		}
+	}
+
+	return nil
+}
