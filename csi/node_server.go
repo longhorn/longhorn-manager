d5a741ea7e77e31b43eb70613c0a8cf5101bde03
Chin-Ya Huang <chin-ya.huang@suse.com>
2024-01-25 14:27:58 +0800

    fix(csi): do not return error when unmount is successful
    longhorn-7694
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>
    (cherry picked from commit c7d9d7c22808a57e7c387459278634fe667124b0)

--- csi/node_server.go
+++ csi/node_server.go
@@ -182,7 +182,7 @@ func (ns *NodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublis
 	}
 
 	isMnt, err := ensureMountPoint(targetPath, mounter)
-	if err != nil && !strings.HasPrefix(err.Error(), errorUnmountCorruptMountPoint) {
+	if err != nil {
 		msg := fmt.Sprintf("Failed to prepare mount point for volume %v error %v", volumeID, err)
 		log.Error(msg)
 		return nil, status.Error(codes.Internal, msg)
