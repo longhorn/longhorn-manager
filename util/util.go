ae043563e1d8a3eeeb0e8a4ad03a4875f6d46684
Shuo Wu <shuo.wu@suse.com>
2024-01-30 10:52:54 +0800

    Revert "Improve and clarify lastHealthyAt and lastFailedAt functionality"
    This reverts commit 6e360568cc7bd4d4eb89a6864662c7185f207a83.
    
    Signed-off-by: Shuo Wu <shuo.wu@suse.com>

--- util/util.go
+++ util/util.go
@@ -261,18 +261,18 @@ func TimestampWithinLimit(latest time.Time, ts string, limit time.Duration) bool
 	return deadline.After(latest)
 }
 
-// TimestampAfterTimestamp returns true if timestamp1 is after timestamp2. It returns false otherwise and an error if
-// either timestamp cannot be parsed.
-func TimestampAfterTimestamp(timestamp1 string, timestamp2 string) (bool, error) {
-	time1, err := time.Parse(time.RFC3339, timestamp1)
+func TimestampAfterTimestamp(after string, before string) bool {
+	afterT, err := time.Parse(time.RFC3339, after)
 	if err != nil {
-		return false, errors.Wrapf(err, "cannot parse timestamp %v", timestamp1)
+		logrus.Errorf("Cannot parse after time %v", after)
+		return false
 	}
-	time2, err := time.Parse(time.RFC3339, timestamp2)
+	beforeT, err := time.Parse(time.RFC3339, before)
 	if err != nil {
-		return false, errors.Wrapf(err, "cannot parse timestamp %v", timestamp2)
+		logrus.Errorf("Cannot parse before time %v", before)
+		return false
 	}
-	return time1.After(time2), nil
+	return afterT.After(beforeT)
 }
 
 func ValidateString(name string) bool {
