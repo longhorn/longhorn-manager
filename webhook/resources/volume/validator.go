fde42bdb58dcddf391c2a0ab5acca68e0bd252f4
Aaron Dewes <aaron.dewes@protonmail.com>
2024-02-25 00:31:49 +0100

    Support changing volume size

--- webhook/resources/volume/validator.go
+++ webhook/resources/volume/validator.go
@@ -244,12 +244,6 @@ func (v *volumeValidator) Update(request *admission.Request, oldObj runtime.Obje
 
 	if datastore.IsDataEngineV2(newVolume.Spec.DataEngine) {
 		// TODO: remove this check when we support the following features for SPDK volumes
-		if oldVolume.Spec.Size != newVolume.Spec.Size {
-			err := fmt.Errorf("changing volume size for volume %v is not supported for data engine %v",
-				newVolume.Name, newVolume.Spec.DataEngine)
-			return werror.NewInvalidError(err.Error(), "")
-		}
-
 		if oldVolume.Spec.NumberOfReplicas != newVolume.Spec.NumberOfReplicas {
 			err := fmt.Errorf("changing number of replicas for volume %v is not supported for data engine %v",
 				newVolume.Name, newVolume.Spec.DataEngine)
