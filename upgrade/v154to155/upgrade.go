b847d04ba821a271c990156b3b8fe5bbc66d1118
Eric Weber <eric.weber@suse.com>
2024-03-08 12:51:43 -0600

    Track replica mode transition times with replicaTransitionTimeMap
    Use this engine status field  to avoid frequent updates to the replica
    lastHealthyAt field.
    
    Longhorn 8114
    
    Signed-off-by: Eric Weber <eric.weber@suse.com>
    (cherry picked from commit 65bdc09182d4e7c7803f54542ef48f6ac0bdc463)

---
+++ upgrade/v154to155/upgrade.go
@@ -0,0 +1,52 @@
+package v154to155
+
+import (
+	"github.com/pkg/errors"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	clientset "k8s.io/client-go/kubernetes"
+
+	lhclientset "github.com/longhorn/longhorn-manager/k8s/pkg/client/clientset/versioned"
+	upgradeutil "github.com/longhorn/longhorn-manager/upgrade/util"
+	"github.com/longhorn/longhorn-manager/util"
+)
+
+const (
+	upgradeLogPrefix = "upgrade from v1.5.4 to v1.5.5: "
+)
+
+func UpgradeResources(namespace string, lhClient *lhclientset.Clientset, kubeClient *clientset.Clientset, resourceMaps map[string]interface{}) error {
+	// Currently there are no resources to upgrade. See UpgradeResourceStatus -> upgradeEngineStatus or previous
+	// Longhorn versions for examples.
+	return nil
+}
+
+func UpgradeResourcesStatus(namespace string, lhClient *lhclientset.Clientset, kubeClient *clientset.Clientset, resourceMaps map[string]interface{}) error {
+	// We will probably need to upgrade other resource status as well. See upgradeEngineStatus or previous Longhorn
+	// versions for examples.
+	return upgradeEngineStatus(namespace, lhClient, resourceMaps)
+}
+
+func upgradeEngineStatus(namespace string, lhClient *lhclientset.Clientset, resourceMaps map[string]interface{}) (err error) {
+	defer func() {
+		err = errors.Wrapf(err, upgradeLogPrefix+"upgrade engines failed")
+	}()
+
+	engineMap, err := upgradeutil.ListAndUpdateEnginesInProvidedCache(namespace, lhClient, resourceMaps)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil
+		}
+		return errors.Wrapf(err, "failed to list all existing Longhorn engines during the engine status upgrade")
+	}
+
+	for _, e := range engineMap {
+		for replicaName := range e.Status.ReplicaModeMap {
+			// We don't have any historical information to rely on. Starting at the time of the upgrade.
+			if _, ok := e.Status.ReplicaTransitionTimeMap[replicaName]; !ok {
+				e.Status.ReplicaTransitionTimeMap[replicaName] = util.Now()
+			}
+		}
+	}
+
+	return nil
+}
