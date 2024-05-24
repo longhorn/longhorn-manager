f9fa0a9fdf554735c95c0f500806de53b49e69f2
Chin-Ya Huang <chin-ya.huang@suse.com>
2024-05-24 16:00:18 +0800

    feat(support-bundle): add support-bundle-node-collection-timeout setting
    longhorn/longhorn-8623
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>
    (cherry picked from commit ebb2b560019aa96e949492be5eede24ad0f83b79)

--- types/setting.go
+++ types/setting.go
@@ -101,6 +101,7 @@ const (
 	SettingNameRecurringFailedJobsHistoryLimit                          = SettingName("recurring-failed-jobs-history-limit")
 	SettingNameRecurringJobMaxRetention                                 = SettingName("recurring-job-max-retention")
 	SettingNameSupportBundleFailedHistoryLimit                          = SettingName("support-bundle-failed-history-limit")
+	SettingNameSupportBundleNodeCollectionTimeout                       = SettingName("support-bundle-node-collection-timeout")
 	SettingNameDeletingConfirmationFlag                                 = SettingName("deleting-confirmation-flag")
 	SettingNameEngineReplicaTimeout                                     = SettingName("engine-replica-timeout")
 	SettingNameSnapshotDataIntegrity                                    = SettingName("snapshot-data-integrity")
@@ -183,6 +184,7 @@ var (
 		SettingNameRecurringFailedJobsHistoryLimit,
 		SettingNameRecurringJobMaxRetention,
 		SettingNameSupportBundleFailedHistoryLimit,
+		SettingNameSupportBundleNodeCollectionTimeout,
 		SettingNameDeletingConfirmationFlag,
 		SettingNameEngineReplicaTimeout,
 		SettingNameSnapshotDataIntegrity,
@@ -291,6 +293,7 @@ var (
 		SettingNameRecurringFailedJobsHistoryLimit:                          SettingDefinitionRecurringFailedJobsHistoryLimit,
 		SettingNameRecurringJobMaxRetention:                                 SettingDefinitionRecurringJobMaxRetention,
 		SettingNameSupportBundleFailedHistoryLimit:                          SettingDefinitionSupportBundleFailedHistoryLimit,
+		SettingNameSupportBundleNodeCollectionTimeout:                       SettingDefinitionSupportBundleNodeCollectionTimeout,
 		SettingNameDeletingConfirmationFlag:                                 SettingDefinitionDeletingConfirmationFlag,
 		SettingNameEngineReplicaTimeout:                                     SettingDefinitionEngineReplicaTimeout,
 		SettingNameSnapshotDataIntegrity:                                    SettingDefinitionSnapshotDataIntegrity,
@@ -991,6 +994,17 @@ var (
 		Default:  "1",
 	}
 
+	SettingDefinitionSupportBundleNodeCollectionTimeout = SettingDefinition{
+		DisplayName: "Timeout for Support Bundle Node Collection",
+		Description: "In minutes. The timeout for collecting node bundles for support bundle generation. The default value is 30.\n\n" +
+			"When the timeout is reached, the support bundle generation will proceed without requiring the collection of node bundles. \n\n",
+		Category: SettingCategoryGeneral,
+		Type:     SettingTypeInt,
+		Required: true,
+		ReadOnly: false,
+		Default:  "30",
+	}
+
 	SettingDefinitionDeletingConfirmationFlag = SettingDefinition{
 		DisplayName: "Deleting Confirmation Flag",
 		Description: "This flag is designed to prevent Longhorn from being accidentally uninstalled which will lead to data lost. \n\n" +
@@ -1422,6 +1436,8 @@ func ValidateSetting(name, value string) (err error) {
 	case SettingNameConcurrentAutomaticEngineUpgradePerNodeLimit:
 		fallthrough
 	case SettingNameSupportBundleFailedHistoryLimit:
+		fallthrough
+	case SettingNameSupportBundleNodeCollectionTimeout:
 		fallthrough
 	case SettingNameBackupstorePollInterval:
 		fallthrough
