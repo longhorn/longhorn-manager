fd9b061b99ffcfdc4033cceb5a1444164ac397e3
Eric Weber <eric.weber@suse.com>
2024-06-04 14:46:49 -0500

    Fix TooManySnapshots error message
    Longhorn 8668
    
    Signed-off-by: Eric Weber <eric.weber@suse.com>

--- controller/volume_controller.go
+++ controller/volume_controller.go
@@ -1713,11 +1713,15 @@ func (c *VolumeController) reconcileLogRequest(e *longhorn.Engine, rs map[string
 	}
 }
 
-func (c *VolumeController) reconcileVolumeCondition(v *longhorn.Volume, e *longhorn.Engine, rs map[string]*longhorn.Replica, log *logrus.Entry) error {
-	if len(e.Status.Snapshots) > VolumeSnapshotsWarningThreshold {
+func (c *VolumeController) reconcileVolumeCondition(v *longhorn.Volume, e *longhorn.Engine,
+	rs map[string]*longhorn.Replica, log *logrus.Entry) error {
+	numSnapshots := len(e.Status.Snapshots) - 1 // Counting volume-head here would be confusing.
+	if numSnapshots > VolumeSnapshotsWarningThreshold {
 		v.Status.Conditions = types.SetCondition(v.Status.Conditions,
 			longhorn.VolumeConditionTypeTooManySnapshots, longhorn.ConditionStatusTrue,
-			longhorn.VolumeConditionReasonTooManySnapshots, fmt.Sprintf("Snapshots count is %v over the warning threshold %v", len(e.Status.Snapshots), VolumeSnapshotsWarningThreshold))
+			longhorn.VolumeConditionReasonTooManySnapshots,
+			fmt.Sprintf("Snapshots count is %v over the warning threshold %v", numSnapshots,
+				VolumeSnapshotsWarningThreshold))
 	} else {
 		v.Status.Conditions = types.SetCondition(v.Status.Conditions,
 			longhorn.VolumeConditionTypeTooManySnapshots, longhorn.ConditionStatusFalse,
