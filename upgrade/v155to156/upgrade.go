fb3d22a85417f492ea57ff2f35ebd71db429af70
Phan Le <phan.le@suse.com>
2024-06-28 17:45:55 -0700

    Remove headless services for instance manager pods
    longhorn-8844
    
    Signed-off-by: Phan Le <phan.le@suse.com>
    (cherry picked from commit 8ee07d1a61bc14baeb9e89d8b00eeff9d813b873)

---
+++ upgrade/v155to156/upgrade.go
@@ -0,0 +1,45 @@
+package v155to156
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clientset "k8s.io/client-go/kubernetes"
+
+	lhclientset "github.com/longhorn/longhorn-manager/k8s/pkg/client/clientset/versioned"
+)
+
+const (
+	upgradeLogPrefix = "upgrade from v1.5.5 to v1.5.6: "
+)
+
+func UpgradeResources(namespace string, lhClient *lhclientset.Clientset, kubeClient *clientset.Clientset, resourceMaps map[string]interface{}) error {
+	if err := deleteInstanceManagerServices(namespace, kubeClient); err != nil {
+		return err
+	}
+	return nil
+}
+
+func UpgradeResourcesStatus(namespace string, lhClient *lhclientset.Clientset, kubeClient *clientset.Clientset, resourceMaps map[string]interface{}) error {
+	// Currently there are no resources status to upgrade. See previous Longhorn
+	// versions for examples.
+	return nil
+}
+
+func deleteInstanceManagerServices(namespace string, kubeClient *clientset.Clientset) (err error) {
+	defer func() {
+		err = errors.Wrapf(err, upgradeLogPrefix+"delete instance manager service failed")
+	}()
+
+	instanceManagerServiceNames := []string{"longhorn-engine-manager", "longhorn-replica-manager"}
+
+	for _, serviceName := range instanceManagerServiceNames {
+		if err := kubeClient.CoreV1().Services(namespace).Delete(context.TODO(), serviceName, metav1.DeleteOptions{}); err != nil && !apierrors.IsNotFound(err) {
+			return err
+		}
+	}
+	return nil
+}
