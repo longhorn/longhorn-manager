47859d951d435b311dad9ed2058383a52d00a379
Derek Su <derek.su@suse.com>
2024-07-29 04:27:48 +0000

    fix(api): remove OfflineReplicaRebuilding
    Longhorn 9090
    
    Signed-off-by: Derek Su <derek.su@suse.com>
    (cherry picked from commit eef8e4e5a63450ca7a736fb59e2a2d613ecf2de8)

--- manager/volume.go
+++ manager/volume.go
@@ -877,27 +877,6 @@ func (m *VolumeManager) UpdateSnapshotDataIntegrity(name string, value string) (
 	return v, nil
 }
 
-func (m *VolumeManager) UpdateOfflineReplicaRebuilding(name string, value string) (v *longhorn.Volume, err error) {
-	defer func() {
-		err = errors.Wrapf(err, "unable to update offline replica rebuilding for volume %v", name)
-	}()
-
-	v, err = m.ds.GetVolume(name)
-	if err != nil {
-		return nil, err
-	}
-
-	oldValue := v.Spec.OfflineReplicaRebuilding
-	v.Spec.OfflineReplicaRebuilding = longhorn.OfflineReplicaRebuilding(value)
-
-	v, err = m.ds.UpdateVolume(v)
-	if err != nil {
-		return nil, err
-	}
-	logrus.Infof("Updated volume %v offline replica rebuilding from %v to %v", v.Name, oldValue, v.Spec.OfflineReplicaRebuilding)
-	return v, nil
-}
-
 func (m *VolumeManager) UpdateBackupCompressionMethod(name string, value string) (v *longhorn.Volume, err error) {
 	defer func() {
 		err = errors.Wrapf(err, "unable to update backup compression method for volume %v", name)
