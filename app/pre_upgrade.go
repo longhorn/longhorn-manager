e8cf7fb1299e8328504939e6c8fb1aa8ffa12d9b
James Munson <james.munson@suse.com>
2024-10-11 15:56:07 -0600

    fix: add events when pre- or post-upgrade check fails
    Signed-off-by: James Munson <james.munson@suse.com>

--- app/pre_upgrade.go
+++ app/pre_upgrade.go
@@ -5,12 +5,22 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/record"
 
+	corev1 "k8s.io/api/core/v1"
+
+	"github.com/longhorn/longhorn-manager/constant"
 	"github.com/longhorn/longhorn-manager/types"
 
+	longhorn "github.com/longhorn/longhorn-manager/k8s/pkg/apis/longhorn/v1beta2"
 	lhclientset "github.com/longhorn/longhorn-manager/k8s/pkg/client/clientset/versioned"
 	upgradeutil "github.com/longhorn/longhorn-manager/upgrade/util"
+)
+
+const (
+	PreUpgradeEventer = "longhorn-pre-upgrade"
 )
 
 func PreUpgradeCmd() cli.Command {
@@ -29,8 +39,8 @@ func PreUpgradeCmd() cli.Command {
 			},
 		},
 		Action: func(c *cli.Context) {
-			logrus.Infof("Running pre-upgrade...")
-			defer logrus.Infof("Completed pre-upgrade.")
+			logrus.Info("Running pre-upgrade...")
+			defer logrus.Info("Completed pre-upgrade.")
 
 			if err := preUpgrade(c); err != nil {
 				logrus.WithError(err).Fatalf("Failed to run pre-upgrade")
@@ -47,17 +57,61 @@ func preUpgrade(c *cli.Context) error {
 		return errors.Wrap(err, "failed to get client config")
 	}
 
+	eventBroadcaster, err := createEventBroadcaster(config)
+	if err != nil {
+		return errors.Wrap(err, "failed to create event broadcaster")
+	}
+	defer eventBroadcaster.Shutdown()
+
+	scheme := runtime.NewScheme()
+	if err := longhorn.SchemeBuilder.AddToScheme(scheme); err != nil {
+		return errors.Wrap(err, "failed to create scheme")
+	}
+
+	eventRecorder := eventBroadcaster.NewRecorder(scheme, corev1.EventSource{Component: PreUpgradeEventer})
+
 	lhClient, err := lhclientset.NewForConfig(config)
 	if err != nil {
 		return errors.Wrap(err, "failed to get clientset")
 	}
 
-	if err := upgradeutil.CheckUpgradePath(namespace, lhClient, nil, true); err != nil {
+	err = newPreUpgrader(namespace, lhClient, eventRecorder).Run()
+	if err != nil {
+		logrus.Warnf("Done with Run() ... err is %v", err)
+	}
+
+	return err
+}
+
+type preUpgrader struct {
+	namespace     string
+	lhClient      lhclientset.Interface
+	eventRecorder record.EventRecorder
+}
+
+func newPreUpgrader(namespace string, lhClient lhclientset.Interface, eventRecorder record.EventRecorder) *preUpgrader {
+	return &preUpgrader{namespace, lhClient, eventRecorder}
+}
+
+func (u *preUpgrader) Run() error {
+	var err error
+	defer func() {
+		if err != nil {
+			u.eventRecorder.Event(&corev1.ObjectReference{Namespace: u.namespace, Name: PreUpgradeEventer},
+				corev1.EventTypeWarning, constant.EventReasonFailedUpgradePreCheck, err.Error())
+		} else {
+			u.eventRecorder.Event(&corev1.ObjectReference{Namespace: u.namespace, Name: PreUpgradeEventer},
+				corev1.EventTypeNormal, constant.EventReasonPassedUpgradeCheck, "pre-upgrade check passed")
+		}
+	}()
+
+	if err = upgradeutil.CheckUpgradePath(u.namespace, u.lhClient, u.eventRecorder, true); err != nil {
 		return err
 	}
 
-	if err := environmentCheck(); err != nil {
-		return errors.Wrap(err, "failed to check environment, please make sure you have iscsiadm/open-iscsi installed on the host")
+	if err = environmentCheck(); err != nil {
+		err = errors.Wrap(err, "failed to check environment, please make sure you have iscsiadm/open-iscsi installed on the host")
+		return err
 	}
 
 	return nil
