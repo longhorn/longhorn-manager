8fa8bcf602657ff863685a84b04d7e15a7206d8f
James Munson <james.munson@suse.com>
2024-10-23 21:17:14 -0600

    fix(upgrade): add short sleep to allow shutdown and event to flush before panic
    Signed-off-by: James Munson <james.munson@suse.com>
    (cherry picked from commit 235b1ae23f6ad4d26dc56063c95358145e937c09)

--- app/pre_upgrade.go
+++ app/pre_upgrade.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"time"
+
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
@@ -61,7 +63,11 @@ func preUpgrade(c *cli.Context) error {
 	if err != nil {
 		return errors.Wrap(err, "failed to create event broadcaster")
 	}
-	defer eventBroadcaster.Shutdown()
+	defer func() {
+		eventBroadcaster.Shutdown()
+		// Allow a little time for the event to flush, but not greatly delay response to the calling job.
+		time.Sleep(5 * time.Second)
+	}()
 
 	scheme := runtime.NewScheme()
 	if err := longhorn.SchemeBuilder.AddToScheme(scheme); err != nil {
