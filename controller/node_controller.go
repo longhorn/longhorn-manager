6235be914ae7281962a4c4313e3c92014d830550
Derek Su <derek.su@suse.com>
2024-11-19 16:21:05 +0800

    feat(v2 upgrade/controller): set schedulable condition when node is requested data engine upgrade
    Longhorn 9104
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- controller/node_controller.go
+++ controller/node_controller.go
@@ -2204,15 +2204,29 @@ func (nc *NodeController) setReadyConditionForKubeNode(node *longhorn.Node, kube
 func (nc *NodeController) SetSchedulableCondition(node *longhorn.Node, kubeNode *corev1.Node,
 	disableSchedulingOnCordonedNode bool) {
 	kubeSpec := kubeNode.Spec
-	if disableSchedulingOnCordonedNode &&
-		kubeSpec.Unschedulable {
+	reason := ""
+	message := ""
+	disableScheduling := false
+
+	if disableSchedulingOnCordonedNode && kubeSpec.Unschedulable {
+		disableScheduling = true
+		reason = string(longhorn.NodeConditionReasonKubernetesNodeCordoned)
+		message = fmt.Sprintf("Node %v is cordoned", node.Name)
+	} else if node.Spec.DataEngineUpgradeRequested {
+		disableScheduling = true
+		reason = string(longhorn.NodeConditionReasonNodeDataEngineUpgradeRequested)
+		message = fmt.Sprintf("Data engine of node %v is being upgraded", node.Name)
+	}
+
+	if disableScheduling {
 		node.Status.Conditions =
 			types.SetConditionAndRecord(node.Status.Conditions,
 				longhorn.NodeConditionTypeSchedulable,
 				longhorn.ConditionStatusFalse,
-				string(longhorn.NodeConditionReasonKubernetesNodeCordoned),
-				fmt.Sprintf("Node %v is cordoned", node.Name),
-				nc.eventRecorder, node,
+				reason,
+				message,
+				nc.eventRecorder,
+				node,
 				corev1.EventTypeNormal)
 	} else {
 		node.Status.Conditions =
@@ -2221,7 +2235,8 @@ func (nc *NodeController) SetSchedulableCondition(node *longhorn.Node, kubeNode 
 				longhorn.ConditionStatusTrue,
 				"",
 				"",
-				nc.eventRecorder, node,
+				nc.eventRecorder,
+				node,
 				corev1.EventTypeNormal)
 	}
 }
