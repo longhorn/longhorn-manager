5c67c8b0513a71a86cf030dc58fcae67811fbf9e
James Lu <jamesluhz@gmail.com>
2024-12-27 16:23:20 +0800

    fix(backup): modify updating backup target from ConfigMap
    And add missing changes when fixing 7159.
    
    ref: longhorn/longhorn 5411, 7159, 10043
    
    Signed-off-by: James Lu <james.lu@suse.com>
    (cherry picked from commit 0dafa291ad036470ae7783d21666ac4d5cf0dbea)

--- webhook/resources/backuptarget/validator.go
+++ webhook/resources/backuptarget/validator.go
@@ -153,6 +153,17 @@ func (b *backupTargetValidator) validateCredentialSecret(secretName string) erro
 	for _, checkKey := range checkKeyList {
 		if value, ok := secret.Data[checkKey]; ok {
 			if strings.TrimSpace(string(value)) != string(value) {
+				//ref: longhorn/longhorn#7159: appropriate warning messages for credential data
+				switch {
+				case strings.TrimLeft(string(value), " ") != string(value):
+					return fmt.Errorf("invalid leading white space in %s", checkKey)
+				case strings.TrimRight(string(value), " ") != string(value):
+					return fmt.Errorf("invalid trailing white space in %s", checkKey)
+				case strings.TrimLeft(string(value), "\n") != string(value):
+					return fmt.Errorf("invalid leading new line in %s", checkKey)
+				case strings.TrimRight(string(value), "\n") != string(value):
+					return fmt.Errorf("invalid trailing new line in %s", checkKey)
+				}
 				return fmt.Errorf("there is space or new line in %s", checkKey)
 			}
 		}
