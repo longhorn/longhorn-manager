3ea1dd992f2a2421333e8c35d971d19aca8ba2b0
Derek Su <derek.su@suse.com>
2025-02-07 10:49:03 +0800

    fix: empty string of backup.status.size
    The data type of backup.status.size is string. If backup is failed and
    the size is not set to "0". The metrics collect will fail to parse it.
    
    - The default value of backup.status.size should be "0"
    - Update the existing backups with backup.status.size == "" to "0"
    
    Longhorn 10358
    
    Signed-off-by: Derek Su <derek.su@suse.com>
    (cherry picked from commit fb0a416df28ead5be8e850f21b0c0d8cd7ff7882)
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- k8s/pkg/apis/longhorn/v1beta2/backup.go
+++ k8s/pkg/apis/longhorn/v1beta2/backup.go
@@ -84,7 +84,7 @@ type BackupStatus struct {
 	// +optional
 	BackupCreatedAt string `json:"backupCreatedAt"`
 	// The snapshot size.
-	// +optional
+	// +kubebuilder:default="0"
 	Size string `json:"size"`
 	// The labels of snapshot backup.
 	// +optional
---
+++ upgrade/v180to181/upgrade.go
@@ -0,0 +1,51 @@
+package v180to181
+
+import (
+	"github.com/pkg/errors"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	clientset "k8s.io/client-go/kubernetes"
+
+	lhclientset "github.com/longhorn/longhorn-manager/k8s/pkg/client/clientset/versioned"
+	upgradeutil "github.com/longhorn/longhorn-manager/upgrade/util"
+)
+
+const (
+	upgradeLogPrefix = "upgrade from v1.8.0 to v1.8.1: "
+)
+
+func UpgradeResources(namespace string, lhClient *lhclientset.Clientset, kubeClient *clientset.Clientset, resourceMaps map[string]interface{}) error {
+	if resourceMaps == nil {
+		return errors.New("resourceMaps cannot be nil")
+	}
+
+	return nil
+}
+
+func UpgradeResourcesStatus(namespace string, lhClient *lhclientset.Clientset, kubeClient *clientset.Clientset, resourceMaps map[string]interface{}) error {
+	if resourceMaps == nil {
+		return errors.New("resourceMaps cannot be nil")
+	}
+	return upgradeBackupStatus(namespace, lhClient, resourceMaps)
+}
+
+func upgradeBackupStatus(namespace string, lhClient *lhclientset.Clientset, resourceMaps map[string]interface{}) (err error) {
+	defer func() {
+		err = errors.Wrapf(err, upgradeLogPrefix+"upgrade backups failed")
+	}()
+
+	backupMap, err := upgradeutil.ListAndUpdateBackupsInProvidedCache(namespace, lhClient, resourceMaps)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil
+		}
+		return errors.Wrapf(err, "failed to list all existing Longhorn backups during the backup status upgrade")
+	}
+
+	for _, b := range backupMap {
+		if b.Status.Size == "" {
+			b.Status.Size = "0"
+		}
+	}
+	return nil
+}
