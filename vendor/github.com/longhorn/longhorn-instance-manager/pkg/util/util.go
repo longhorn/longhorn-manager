def5e77d3f7c0f3fdc3fa928d0d4a9859f623b24
Esteban Ricardo Mirizio <ermirizio@gmail.com>
2025-04-08 16:30:20 -0300

    fix: adjust default command timeout from 30 minutes to 3 minutes

--- vendor/github.com/longhorn/longhorn-instance-manager/pkg/util/util.go
+++ vendor/github.com/longhorn/longhorn-instance-manager/pkg/util/util.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	DefaulCmdTimeout = time.Minute * 30 // 30 minutes by default
+	DefaulCmdTimeout = time.Minute * 3 // 3 minutes by default
 
 	GRPCHealthProbe = "/usr/local/bin/grpc_health_probe"
 )
