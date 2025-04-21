84707d10156ac3763f4d5fc63e138eae6d3ef9f1
Chin-Ya Huang <chin-ya.huang@suse.com>
2025-04-21 17:06:31 +0800

    fix(uninstall): delete support-bunble before deleting manager
    longhorn/longhorn-10731
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- datastore/longhorn.go
+++ datastore/longhorn.go
@@ -23,11 +23,10 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/validation"
 
-	k8sruntime "k8s.io/apimachinery/pkg/runtime"
-
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8sruntime "k8s.io/apimachinery/pkg/runtime"
 
 	"github.com/longhorn/longhorn-manager/csi/crypto"
 	"github.com/longhorn/longhorn-manager/types"
