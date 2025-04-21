07c3456a39b7f6566d7e2e0cecf35338b6e50e47
Jack Lin <jack.lin@suse.com>
2025-04-21 12:11:10 +0800

    fix: fix empty diskUUID crashed
    ref: longhorn/longhorn 10749
    
    Signed-off-by: Jack Lin <jack.lin@suse.com>
    (cherry picked from commit 97ed2e62d74c32ead1e1ea9a2daee1aa144debc4)
    
    # Conflicts:
    #	datastore/longhorn.go

--- datastore/longhorn.go
+++ datastore/longhorn.go
@@ -5666,3 +5666,24 @@ func (s *DataStore) IsStorageNetworkForRWXVolume() (bool, error) {
 
 	return types.IsStorageNetworkForRWXVolume(storageNetworkSetting, storageNetworkForRWXVolumeEnabled), nil
 }
+<<<<<<< HEAD
+=======
+
+func (s *DataStore) GetAllDiskUUIDFirstFourChar() (map[string]bool, error) {
+	firstFourCharSet := make(map[string]bool)
+	nodes, err := s.ListNodesRO()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, node := range nodes {
+		for _, diskStatus := range node.Status.DiskStatus {
+			if len(diskStatus.DiskUUID) >= 4 {
+				firstFourCharSet[diskStatus.DiskUUID[:4]] = true
+			}
+		}
+	}
+
+	return firstFourCharSet, nil
+}
+>>>>>>> 97ed2e62 (fix: fix empty diskUUID crashed)
