2a7316165cee952d116cff697548dade6a82df5f
bachmanity1 <bachmanity138@gmail.com>
2025-04-29 16:26:20 +0900

    feat(csi): validate disk and use reserved storage for computing available capacity
    Signed-off-by: bachmanity1 <bachmanity138@gmail.com>
    
    feat(csi): return disk with max avail. capacity when several disks are available
    
    Signed-off-by: bachmanity1 <bachmanity138@gmail.com>
    
    feat(csi): update tests
    
    Signed-off-by: bachmanity1 <bachmanity138@gmail.com>
    
    feat(csi): add todo item
    
    Signed-off-by: bachmanity1 <bachmanity138@gmail.com>
    
    feat(csi): add disk validation logic
    
    Signed-off-by: bachmanity1 <bachmanity138@gmail.com>
    
    feat(csi): update unit tests
    
    Signed-off-by: bachmanity1 <bachmanity138@gmail.com>
    
    feat(csi): use allow-empty-disk-selector-volume setting
    
    Signed-off-by: bachmanity1 <bachmanity138@gmail.com>

--- csi/controlller_server_test.go
+++ csi/controlller_server_test.go
@@ -3,6 +3,8 @@ package csi
 import (
 	"context"
 	"fmt"
+	"github.com/longhorn/longhorn-manager/types"
+	"strings"
 	"testing"
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
@@ -16,6 +18,11 @@ import (
 	lhfake "github.com/longhorn/longhorn-manager/k8s/pkg/client/clientset/versioned/fake"
 )
 
+type disk struct {
+	spec   longhorn.DiskSpec
+	status longhorn.DiskStatus
+}
+
 func TestGetCapacity(t *testing.T) {
 	cs := &ControllerServer{
 		lhNamespace: "longhorn-system-test",
@@ -25,13 +32,20 @@ func TestGetCapacity(t *testing.T) {
 		nodeID            string
 		createNode        bool
 		dataEngine        string
+		diskSelector      string
 		availableCapacity int64
+		disks             []*disk
 		err               error
 	}{
 		{
 			nodeID:     "node-0",
 			dataEngine: "v1",
 			err:        status.Errorf(codes.NotFound, "node node-0 not found"),
+		},
+		{
+			nodeID:     "node-0",
+			createNode: true,
+			err:        status.Errorf(codes.InvalidArgument, "storage class parameters missing 'dataEngine' key"),
 		},
 		{
 			nodeID:     "node-0",
@@ -43,18 +57,62 @@ func TestGetCapacity(t *testing.T) {
 			nodeID:            "node-0",
 			createNode:        true,
 			dataEngine:        "v1",
-			availableCapacity: 1028,
+			availableCapacity: 0,
 		},
 		{
 			nodeID:            "node-0",
 			createNode:        true,
 			dataEngine:        "v2",
-			availableCapacity: 1028,
+			availableCapacity: 0,
+		},
+		{
+			nodeID:            "node-0",
+			createNode:        true,
+			dataEngine:        "v1",
+			availableCapacity: 1150,
+			disks:             []*disk{newDisk(1450, 300, "ssd", false, true, true, false), newDisk(1000, 500, "", false, true, true, false), newDisk(2000, 100, "", true, true, true, false)},
+		},
+		{
+			nodeID:            "node-0",
+			createNode:        true,
+			dataEngine:        "v2",
+			availableCapacity: 1650,
+			disks:             []*disk{newDisk(1950, 300, "", true, true, true, false), newDisk(1500, 500, "", true, true, true, false), newDisk(2000, 100, "", false, true, true, false)},
+		},
+		{
+			nodeID:            "node-0",
+			createNode:        true,
+			dataEngine:        "v2",
+			diskSelector:      "ssd,fast",
+			availableCapacity: 1000,
+			disks:             []*disk{newDisk(1100, 100, "fast,nvmf,ssd,hot", true, true, true, false), newDisk(2500, 500, "ssd,slow,green", true, true, true, false), newDisk(2000, 100, "hdd,fast", true, true, true, false)},
+		},
+		{
+			nodeID:            "node-0",
+			createNode:        true,
+			dataEngine:        "v2",
+			availableCapacity: 400,
+			disks:             []*disk{newDisk(1100, 100, "ssd", true, false, true, false), newDisk(500, 100, "hdd", true, true, true, false)},
+		},
+		{
+			nodeID:            "node-0",
+			createNode:        true,
+			dataEngine:        "v2",
+			availableCapacity: 400,
+			disks:             []*disk{newDisk(1100, 100, "ssd", true, true, false, false), newDisk(500, 100, "hdd", true, true, true, false)},
+		},
+		{
+			nodeID:            "node-0",
+			createNode:        true,
+			dataEngine:        "v2",
+			availableCapacity: 400,
+			disks:             []*disk{newDisk(1100, 100, "ssd", true, true, true, true), newDisk(500, 100, "hdd", true, true, true, false)},
 		},
 	} {
 		cs.lhClient = lhfake.NewSimpleClientset()
+		cs.lhClient.LonghornV1beta2().Settings(cs.lhNamespace).Create(context.TODO(), newSetting(string(types.SettingNameAllowEmptyDiskSelectorVolume), "true"), metav1.CreateOptions{})
 		if test.createNode {
-			node := newNode(test.nodeID, cs.lhNamespace, test.dataEngine, test.availableCapacity)
+			node := newNode(test.nodeID, cs.lhNamespace, test.disks)
 			_, err := cs.lhClient.LonghornV1beta2().Nodes(cs.lhNamespace).Create(context.TODO(), node, metav1.CreateOptions{})
 			if err != nil {
 				t.Errorf("failed to create mock node: %v", err)
@@ -66,10 +124,12 @@ func TestGetCapacity(t *testing.T) {
 					nodeTopologyKey: test.nodeID,
 				},
 			},
-			Parameters: map[string]string{
-				"dataEngine": test.dataEngine,
-			},
+			Parameters: map[string]string{},
 		}
+		if test.dataEngine != "" {
+			req.Parameters["dataEngine"] = test.dataEngine
+		}
+		req.Parameters["diskSelector"] = test.diskSelector
 		res, err := cs.GetCapacity(context.TODO(), req)
 
 		expectedStatus := status.Convert(test.err)
@@ -80,41 +140,7 @@ func TestGetCapacity(t *testing.T) {
 			t.Errorf("expected error message: '%s', but got: '%s'", expectedStatus.Message(), actualStatus.Message())
 		}
 		if res != nil && res.AvailableCapacity != test.availableCapacity {
-			t.Errorf("expected available capacity: %d, but got: %d", res.AvailableCapacity, test.availableCapacity)
-		}
-	}
-}
-
-func TestParseDataEngine(t *testing.T) {
-	for _, test := range []struct {
-		parameters map[string]string
-		err        error
-		dataEngine string
-	}{
-		{
-			err: fmt.Errorf("missing storage class parameters"),
-		},
-		{
-			parameters: map[string]string{},
-			err:        fmt.Errorf("storage class parameters missing data engine key"),
-		},
-		{
-			parameters: map[string]string{
-				"dataEngine": "v1",
-			},
-			dataEngine: "v1",
-		},
-		{
-			parameters: map[string]string{
-				"dataEngine": "v5",
-			},
-			dataEngine: "v5",
-		},
-	} {
-		dataEngine, err := parseDataEngine(test.parameters)
-		checkError(t, test.err, err)
-		if test.dataEngine != string(dataEngine) {
-			t.Errorf("expected dataEngine: %s, but got: %s", test.dataEngine, dataEngine)
+			t.Errorf("expected available capacity: %d, but got: %d", test.availableCapacity, res.AvailableCapacity)
 		}
 	}
 }
@@ -174,23 +200,54 @@ func checkError(t *testing.T, expected, actual error) {
 	}
 }
 
-func newNode(name, namespace, dataEngine string, storageSize int64) *longhorn.Node {
-	diskType := longhorn.DiskTypeFilesystem
-	if dataEngine == "v2" {
-		diskType = longhorn.DiskTypeBlock
+func newDisk(storageAvailable, storageReserved int64, tags string, isBlockType, isCondOk, allowScheduling, evictionRequested bool) *disk {
+	disk := &disk{
+		spec: longhorn.DiskSpec{
+			StorageReserved:   storageReserved,
+			Tags:              strings.Split(tags, ","),
+			AllowScheduling:   allowScheduling,
+			EvictionRequested: evictionRequested,
+		},
+		status: longhorn.DiskStatus{
+			StorageAvailable: storageAvailable,
+			Type:             longhorn.DiskTypeFilesystem,
+		},
 	}
-	return &longhorn.Node{
+	if isBlockType {
+		disk.status.Type = longhorn.DiskTypeBlock
+	}
+	if isCondOk {
+		disk.status.Conditions = []longhorn.Condition{{Type: longhorn.DiskConditionTypeSchedulable, Status: longhorn.ConditionStatusTrue}}
+	}
+	return disk
+}
+
+func newNode(name, namespace string, disks []*disk) *longhorn.Node {
+	node := &longhorn.Node{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
 			Namespace: namespace,
 		},
-		Status: longhorn.NodeStatus{
-			DiskStatus: map[string]*longhorn.DiskStatus{
-				"disk": {
-					StorageAvailable: storageSize,
-					Type:             diskType,
-				},
-			},
+		Spec: longhorn.NodeSpec{
+			Disks: map[string]longhorn.DiskSpec{},
 		},
+		Status: longhorn.NodeStatus{
+			DiskStatus: map[string]*longhorn.DiskStatus{},
+		},
+	}
+	for i, disk := range disks {
+		name := fmt.Sprintf("disk-%d", i)
+		node.Spec.Disks[name] = disk.spec
+		node.Status.DiskStatus[name] = &disk.status
+	}
+	return node
+}
+
+func newSetting(name, value string) *longhorn.Setting {
+	return &longhorn.Setting{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Value: value,
 	}
 }
