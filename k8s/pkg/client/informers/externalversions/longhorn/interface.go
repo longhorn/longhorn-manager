15ce69f51988451ab71af87fbbec48553157707e
Derek Su <derek.su@suse.com>
2025-05-26 16:38:05 +0800

    feat(crd): remove v1beta1 api
    Longhorn 10249
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- k8s/pkg/client/informers/externalversions/longhorn/interface.go
+++ k8s/pkg/client/informers/externalversions/longhorn/interface.go
@@ -20,14 +20,11 @@ package longhorn
 
 import (
 	internalinterfaces "github.com/longhorn/longhorn-manager/k8s/pkg/client/informers/externalversions/internalinterfaces"
-	v1beta1 "github.com/longhorn/longhorn-manager/k8s/pkg/client/informers/externalversions/longhorn/v1beta1"
 	v1beta2 "github.com/longhorn/longhorn-manager/k8s/pkg/client/informers/externalversions/longhorn/v1beta2"
 )
 
 // Interface provides access to each of this group's versions.
 type Interface interface {
-	// V1beta1 provides access to shared informers for resources in V1beta1.
-	V1beta1() v1beta1.Interface
 	// V1beta2 provides access to shared informers for resources in V1beta2.
 	V1beta2() v1beta2.Interface
 }
@@ -41,11 +38,6 @@ type group struct {
 // New returns a new Interface.
 func New(f internalinterfaces.SharedInformerFactory, namespace string, tweakListOptions internalinterfaces.TweakListOptionsFunc) Interface {
 	return &group{factory: f, namespace: namespace, tweakListOptions: tweakListOptions}
-}
-
-// V1beta1 returns a new v1beta1.Interface.
-func (g *group) V1beta1() v1beta1.Interface {
-	return v1beta1.New(g.factory, g.namespace, g.tweakListOptions)
 }
 
 // V1beta2 returns a new v1beta2.Interface.
