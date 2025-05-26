9601bf43c2a1c34158a3a199146c3e24ad9901cf
Derek Su <derek.su@suse.com>
2025-05-26 16:38:05 +0800

    feat(crd): remove v1beta1 api
    Longhorn 10249
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- k8s/pkg/client/clientset/versioned/scheme/register.go
+++ k8s/pkg/client/clientset/versioned/scheme/register.go
@@ -19,7 +19,6 @@ limitations under the License.
 package scheme
 
 import (
-	longhornv1beta1 "github.com/longhorn/longhorn-manager/k8s/pkg/apis/longhorn/v1beta1"
 	longhornv1beta2 "github.com/longhorn/longhorn-manager/k8s/pkg/apis/longhorn/v1beta2"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
@@ -32,7 +31,6 @@ var Scheme = runtime.NewScheme()
 var Codecs = serializer.NewCodecFactory(Scheme)
 var ParameterCodec = runtime.NewParameterCodec(Scheme)
 var localSchemeBuilder = runtime.SchemeBuilder{
-	longhornv1beta1.AddToScheme,
 	longhornv1beta2.AddToScheme,
 }
 
