1c4e04786800c6e1028270a50eb84f4a4c585478
bachmanity1 <81428651+bachmanity1@users.noreply.github.com>
2025-07-09 18:57:24 +0900

    Update controller/utils_test.go
    Co-authored-by: c3y1huang <chin-ya.huang@suse.com>
    Signed-off-by: bachmanity1 <81428651+bachmanity1@users.noreply.github.com>

--- controller/utils_test.go
+++ controller/utils_test.go
@@ -1,9 +1,10 @@
 package controller
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestExponentialBackoff(t *testing.T) {
