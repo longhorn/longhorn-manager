45f56311037b7351e5f27134d37db7e009c36ea5
Raphanus Lo <yunchang.lo@suse.com>
2025-08-13 14:44:53 +0800

    fix(upgrade): remove unnecessary resource upgrade for v1.10.x
    Signed-off-by: Raphanus Lo <yunchang.lo@suse.com>

--- upgrade/v19xto1100/upgrade.go
+++ upgrade/v19xto1100/upgrade.go
@@ -18,16 +18,6 @@ const (
 	upgradeLogPrefix = "upgrade from v1.9.x to v1.10.0: "
 )
 
-type listAndUpdateFunc func(namespace string, lhClient *lhclientset.Clientset, resourceMaps map[string]interface{}) error
-type typedListAndUpdateFunc[K any] func(namespace string, lhClient *lhclientset.Clientset, resourceMaps map[string]interface{}) (map[string]*K, error)
-
-func listAndUpdateResources[K any](listUpdateFunc typedListAndUpdateFunc[K]) listAndUpdateFunc {
-	return func(namespace string, lhClient *lhclientset.Clientset, resourceMaps map[string]interface{}) error {
-		_, err := listUpdateFunc(namespace, lhClient, resourceMaps)
-		return err
-	}
-}
-
 func UpgradeResources(namespace string, lhClient *lhclientset.Clientset, kubeClient *clientset.Clientset, resourceMaps map[string]interface{}) error {
 	if resourceMaps == nil {
 		return errors.New("resourceMaps cannot be nil")
@@ -41,29 +31,8 @@ func UpgradeResources(namespace string, lhClient *lhclientset.Clientset, kubeCli
 }
 
 func updateCRs(namespace string, lhClient *lhclientset.Clientset, kubeClient *clientset.Clientset, resourceMaps map[string]interface{}) (err error) {
-	// From v1.9.0, the v1beta1 API is deprecated, and v1beta2 is the storage version. Load all resource and write back into v1beta2.
-
-	updates := map[string]listAndUpdateFunc{
-		types.LonghornKindSetting:                listAndUpdateResources(upgradeutil.ListAndUpdateSettingsInProvidedCache),
-		types.LonghornKindNode:                   listAndUpdateResources(upgradeutil.ListAndUpdateNodesInProvidedCache),
-		types.LonghornKindInstanceManager:        listAndUpdateResources(upgradeutil.ListAndUpdateInstanceManagersInProvidedCache),
-		types.LonghornKindShareManager:           listAndUpdateResources(upgradeutil.ListAndUpdateShareManagersInProvidedCache),
-		types.LonghornKindEngine:                 listAndUpdateResources(upgradeutil.ListAndUpdateEnginesInProvidedCache),
-		types.LonghornKindEngineImage:            listAndUpdateResources(upgradeutil.ListAndUpdateEngineImagesInProvidedCache),
-		types.LonghornKindReplica:                listAndUpdateResources(upgradeutil.ListAndUpdateReplicasInProvidedCache),
-		types.LonghornKindVolume:                 listAndUpdateResources(upgradeutil.ListAndUpdateVolumesInProvidedCache),
-		types.LonghornKindBackupVolume:           listAndUpdateResources(upgradeutil.ListAndUpdateBackupVolumesInProvidedCache),
-		types.LonghornKindBackup:                 listAndUpdateResources(upgradeutil.ListAndUpdateBackupsInProvidedCache),
-		types.LonghornKindBackupTarget:           listAndUpdateResources(upgradeutil.ListAndUpdateBackupTargetsInProvidedCache),
-		types.LonghornKindBackingImageManager:    listAndUpdateResources(upgradeutil.ListAndUpdateBackingImageManagersInProvidedCache),
-		types.LonghornKindBackingImageDataSource: listAndUpdateResources(upgradeutil.ListAndUpdateBackingImageDataSourcesInProvidedCache),
-		types.LonghornKindBackingImage:           listAndUpdateResources(upgradeutil.ListAndUpdateBackingImagesInProvidedCache),
-		types.LonghornKindRecurringJob:           listAndUpdateResources(upgradeutil.ListAndUpdateRecurringJobsInProvidedCache),
-	}
-	for resourceKind, listUpdateFunc := range updates {
-		if err := listUpdateFunc(namespace, lhClient, resourceMaps); err != nil {
-			return errors.Wrapf(err, upgradeLogPrefix+"failed to list all existing Longhorn %s during the %s upgrade", resourceKind, resourceKind)
-		}
+	if resourceMaps == nil {
+		return errors.New("resourceMaps cannot be nil")
 	}
 
 	if err := upgradeSettings(namespace, lhClient, resourceMaps); err != nil {
