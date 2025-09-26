c77bd83ddc59c13c8879340c13c4a21b9af09dbd
Chin-Ya Huang <chin-ya.huang@suse.com>
2025-09-26 08:39:07 +0800

    feat(setting): deprecate SettingNameStorageNetworkForRWXVolumeEnabled
    longhorn/longhorn-10269
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- controller/setting_controller.go
+++ controller/setting_controller.go
@@ -1640,7 +1640,6 @@ func (info *ClusterInfo) collectSettings() error {
 		types.SettingNameSnapshotDataIntegrity:                                    true,
 		types.SettingNameSnapshotDataIntegrityImmediateCheckAfterSnapshotCreation: true,
 		types.SettingNameStorageMinimalAvailablePercentage:                        true,
-		types.SettingNameStorageNetworkForRWXVolumeEnabled:                        true,
 		types.SettingNameStorageOverProvisioningPercentage:                        true,
 		types.SettingNameStorageReservedPercentageForDefaultDisk:                  true,
 		types.SettingNameSupportBundleFailedHistoryLimit:                          true,
