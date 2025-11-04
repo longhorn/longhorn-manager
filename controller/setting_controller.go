dac86b21ac5279883e3c5410aa45210c70dd20e1
James Lu <james.lu@suse.com>
2025-11-04 15:29:54 +0800

    fix(metric): add backup target metric
    Collect backup target drivers count:
    - longhorn_backup_target_s3_count
    - longhorn_backup_target_nfs_count
    - longhorn_backup_target_cifs_count
    - longhorn_backup_target_azblob_count
    - longhorn_backup_target_unknown_count
    
    ref: longhorn/longhorn 5411, 12073
    
    Signed-off-by: James Lu <james.lu@suse.com>
    (cherry picked from commit dd8dce6130a3e412768a4848ed18603e3f10e1e6)

--- controller/setting_controller.go
+++ controller/setting_controller.go
@@ -1393,6 +1393,7 @@ const (
 	ClusterInfoVolumeNumOfReplicas    = util.StructName("LonghornVolumeNumberOfReplicas")
 	ClusterInfoVolumeNumOfSnapshots   = util.StructName("LonghornVolumeNumberOfSnapshots")
 
+	ClusterInfoBackupTargetSchemeCountFmt                = "LonghornBackupTarget%sCount"
 	ClusterInfoPodAvgCPUUsageFmt                         = "Longhorn%sAverageCpuUsageMilliCores"
 	ClusterInfoPodAvgMemoryUsageFmt                      = "Longhorn%sAverageMemoryUsageBytes"
 	ClusterInfoSettingFmt                                = "LonghornSetting%s"
@@ -1482,6 +1483,10 @@ func (info *ClusterInfo) collectClusterScope() {
 
 	if err := info.collectOrphanInfo(); err != nil {
 		info.logger.WithError(err).Warn("Failed to collect Longhorn orphan info")
+	}
+
+	if err := info.collectBackupTargetInfo(); err != nil {
+		info.logger.WithError(err).Warn("Failed to collect Longhorn backup target info")
 	}
 }
 
@@ -1842,10 +1847,26 @@ func (info *ClusterInfo) collectBackingImageInfo() error {
 }
 
 func (info *ClusterInfo) collectOrphanInfo() error {
-	orphans, err := info.ds.ListOrphans()
+	orphans, err := info.ds.ListOrphansRO()
 	if err == nil {
 		info.structFields.fields.Append(ClusterInfoOrphanCount, len(orphans))
 	}
+	return err
+}
+
+func (info *ClusterInfo) collectBackupTargetInfo() error {
+	backupTargets, err := info.ds.ListBackupTargetsRO()
+	if err != nil {
+		return errors.Wrapf(err, "failed to list Longhorn Backup Targets")
+	}
+
+	backupTargetDriverCountStruct := make(map[util.StructName]int, len(backupTargets))
+	for _, backupTarget := range backupTargets {
+		backupTargetScheme := types.GetBackupTargetSchemeFromURL(backupTarget.Spec.BackupTargetURL)
+		backupTargetDriverCountStruct[util.StructName(fmt.Sprintf(ClusterInfoBackupTargetSchemeCountFmt, util.ConvertToCamel(backupTargetScheme, "-")))]++
+	}
+	info.structFields.fields.AppendCounted(backupTargetDriverCountStruct)
+
 	return err
 }
 
