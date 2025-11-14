1366c4f5fdd1c683b90299f2b39f0a8740efb4a0
Chin-Ya Huang <chin-ya.huang@suse.com>
2025-11-14 09:17:01 +0800

    chore: run k8s/generate_code.sh
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- k8s/pkg/client/applyconfiguration/longhorn/v1beta2/volume.go
+++ k8s/pkg/client/applyconfiguration/longhorn/v1beta2/volume.go
@@ -43,6 +43,7 @@ func Volume(name, namespace string) *VolumeApplyConfiguration {
 	b.WithAPIVersion("longhorn.io/v1beta2")
 	return b
 }
+func (b VolumeApplyConfiguration) IsApplyConfiguration() {}
 
 // WithKind sets the Kind field in the declarative configuration to the given value
 // and returns the receiver, so that objects can be built by chaining "With" function invocations.
@@ -218,8 +219,24 @@ func (b *VolumeApplyConfiguration) WithStatus(value *VolumeStatusApplyConfigurat
 	return b
 }
 
+// GetKind retrieves the value of the Kind field in the declarative configuration.
+func (b *VolumeApplyConfiguration) GetKind() *string {
+	return b.TypeMetaApplyConfiguration.Kind
+}
+
+// GetAPIVersion retrieves the value of the APIVersion field in the declarative configuration.
+func (b *VolumeApplyConfiguration) GetAPIVersion() *string {
+	return b.TypeMetaApplyConfiguration.APIVersion
+}
+
 // GetName retrieves the value of the Name field in the declarative configuration.
 func (b *VolumeApplyConfiguration) GetName() *string {
 	b.ensureObjectMetaApplyConfigurationExists()
 	return b.ObjectMetaApplyConfiguration.Name
+}
+
+// GetNamespace retrieves the value of the Namespace field in the declarative configuration.
+func (b *VolumeApplyConfiguration) GetNamespace() *string {
+	b.ensureObjectMetaApplyConfigurationExists()
+	return b.ObjectMetaApplyConfiguration.Namespace
 }
--- k8s/pkg/client/informers/externalversions/longhorn/v1beta1/backingimagedatasource.go
+++ k8s/pkg/client/informers/externalversions/longhorn/v1beta1/backingimagedatasource.go
@@ -62,13 +62,25 @@ func NewFilteredBackingImageDataSourceInformer(client versioned.Interface, names
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.LonghornV1beta1().BackingImageDataSources(namespace).List(context.TODO(), options)
+				return client.LonghornV1beta1().BackingImageDataSources(namespace).List(context.Background(), options)
 			},
 			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.LonghornV1beta1().BackingImageDataSources(namespace).Watch(context.TODO(), options)
+				return client.LonghornV1beta1().BackingImageDataSources(namespace).Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options v1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.LonghornV1beta1().BackingImageDataSources(namespace).List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options v1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.LonghornV1beta1().BackingImageDataSources(namespace).Watch(ctx, options)
 			},
 		},
 		&apislonghornv1beta1.BackingImageDataSource{},
