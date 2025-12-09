1e7059e96cc015e0bb6629740bb8181ff19ed6c7
Alexander Semenov <sasforever@gmail.com>
2025-12-09 12:59:15 +0300

    feat: add copy settings to upgrade - from cache
    Signed-off-by: Alexander Semenov <sasforever@gmail.com>

--- upgrade/v110xto1110/upgrade.go
+++ upgrade/v110xto1110/upgrade.go
@@ -3,6 +3,7 @@ package v110xto1110
 import (
 	"github.com/cockroachdb/errors"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	clientset "k8s.io/client-go/kubernetes"
 
 	lhclientset "github.com/longhorn/longhorn-manager/k8s/pkg/client/clientset/versioned"
@@ -26,8 +27,26 @@ func UpgradeResources(namespace string, lhClient *lhclientset.Clientset, kubeCli
 }
 
 func updateCRs(namespace string, lhClient *lhclientset.Clientset, kubeClient *clientset.Clientset, resourceMaps map[string]interface{}) (err error) {
+	if err := upgradeSettings(namespace, lhClient, resourceMaps); err != nil {
+		return err
+	}
+	return nil
+}
 
-	if err := upgradeutil.CopyCSISidecarSettings(namespace, lhClient); err != nil {
+func upgradeSettings(namespace string, lhClient *lhclientset.Clientset, resourceMaps map[string]interface{}) (err error) {
+	defer func() {
+		err = errors.Wrapf(err, upgradeLogPrefix+"upgrade setting failed")
+	}()
+
+	settingMap, err := upgradeutil.ListAndUpdateSettingsInProvidedCache(namespace, lhClient, resourceMaps)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil
+		}
+		return errors.Wrapf(err, "failed to list all existing Longhorn settings during the setting upgrade")
+	}
+
+	if err := upgradeutil.CopyCSISidecarSettings(namespace, lhClient, settingMap); err != nil {
 		return err
 	}
 
