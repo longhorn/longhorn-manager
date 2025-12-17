dd2ae550e9bb6f681826082244398e5d85a208e6
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-12-17 21:10:48 +0100

    fix(csi): ignored new capabily API

--- csi/controller_server.go
+++ csi/controller_server.go
@@ -90,11 +90,13 @@ func NewControllerServer(apiClient *longhornclient.RancherClient, nodeID string)
 				csi.ControllerServiceCapability_RPC_CREATE_DELETE_SNAPSHOT,
 				csi.ControllerServiceCapability_RPC_CLONE_VOLUME,
 				csi.ControllerServiceCapability_RPC_GET_CAPACITY,
+				csi.ControllerServiceCapability_RPC_SINGLE_NODE_MULTI_WRITER,
 			}),
 		accessModes: getVolumeCapabilityAccessModes(
 			[]csi.VolumeCapability_AccessMode_Mode{
 				csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER,
 				csi.VolumeCapability_AccessMode_SINGLE_NODE_SINGLE_WRITER,
+				csi.VolumeCapability_AccessMode_SINGLE_NODE_MULTI_WRITER,
 				csi.VolumeCapability_AccessMode_MULTI_NODE_MULTI_WRITER,
 			}),
 		log:         logrus.StandardLogger().WithField("component", "csi-controller-server"),
