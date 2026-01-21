38da3f07b9312ff679eb57f4a011b53d01f31172
bachmanity1 <bachmanity138@gmail.com>
2026-01-21 16:44:47 +0900

    refactor(csi): use k8s topology constants and limit exposed keys
    Signed-off-by: bachmanity1 <bachmanity138@gmail.com>

--- csi/util.go
+++ csi/util.go
@@ -19,6 +19,7 @@ import (
 
 	"k8s.io/mount-utils"
 
+	corev1 "k8s.io/api/core/v1"
 	utilexec "k8s.io/utils/exec"
 
 	"github.com/longhorn/longhorn-manager/types"
@@ -36,8 +37,6 @@ const (
 	defaultForceUmountTimeout = 30 * time.Second
 
 	tempTestMountPointValidStatusFile = ".longhorn-volume-mount-point-test.tmp"
-
-	nodeTopologyKey = "kubernetes.io/hostname"
 )
 
 // NewForcedParamsExec creates a osExecutor that allows for adding additional params to later occurring Run calls
@@ -616,9 +615,9 @@ func parseNodeID(topology *csi.Topology) (string, error) {
 	if topology == nil || topology.Segments == nil {
 		return "", fmt.Errorf("missing accessible topology request parameter")
 	}
-	nodeId, ok := topology.Segments[nodeTopologyKey]
+	nodeId, ok := topology.Segments[corev1.LabelHostname]
 	if !ok {
-		return "", fmt.Errorf("accessible topology request parameter is missing %s key", nodeTopologyKey)
+		return "", fmt.Errorf("accessible topology request parameter is missing %s key", corev1.LabelHostname)
 	}
 	return nodeId, nil
 }
@@ -640,4 +639,8 @@ func isNotMountedError(err error) bool {
 	}
 	return strings.Contains(err.Error(), "not mounted") ||
 		strings.Contains(err.Error(), "no mount point specified")
+}
+
+func isTopologyKey(key string) bool {
+	return key == corev1.LabelHostname || key == corev1.LabelTopologyZone || key == corev1.LabelTopologyRegion || strings.Contains(key, "longhorn")
 }
