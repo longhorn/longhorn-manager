dda66099c142da452a2cf4d9a18eb42e1074125d
Chin-Ya Huang <chin-ya.huang@suse.com>
2026-02-25 13:11:13 +0800

    fix(datastore): skip migratable volume is RWX detachment check
    longhorn/longhorn-12644
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>
    (cherry picked from commit df3c8477c7e8e5b2fc333d3021eb643f273c3622)

--- datastore/longhorn.go
+++ datastore/longhorn.go
@@ -774,6 +774,10 @@ func (s *DataStore) AreAllRWXVolumesDetached() (bool, error) {
 			continue
 		}
 
+		if volume.Spec.Migratable {
+			continue
+		}
+
 		volumeAttachment, err := s.GetLHVolumeAttachmentByVolumeName(volume.Name)
 		if err != nil && !apierrors.IsNotFound(err) {
 			return false, err
