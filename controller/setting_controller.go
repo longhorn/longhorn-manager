012c84d0bfa0cf54119c9c0afd56b8bee7ddce2a
David Cheng <david.cheng@suse.com>
2026-03-09 11:56:20 +0800

    feat: add distro in check upgrade request
    Rancher: 116
    
    Signed-off-by: David Cheng <david.cheng@suse.com>

--- controller/setting_controller.go
+++ controller/setting_controller.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"reflect"
 	"slices"
 	"sort"
@@ -48,6 +49,8 @@ import (
 const (
 	VersionTagLatest = "latest"
 	VersionTagStable = "stable"
+
+	DefaultEnvDistroUnknown = "unknown"
 )
 
 var (
@@ -1252,7 +1255,8 @@ func (sc *SettingController) CheckLatestAndStableLonghornVersions() (string, str
 		version = "dev"
 	}
 	req := &CheckUpgradeRequest{
-		AppVersion:     version,
+		AppVersion: version,
+
 		ExtraTagInfo:   extraTagInfo,
 		ExtraFieldInfo: extraFieldInfo,
 	}
@@ -1519,6 +1523,7 @@ const (
 	ClusterInfoHostArch          = util.StructName("HostArch")
 	ClusterInfoHostKernelRelease = util.StructName("HostKernelRelease")
 	ClusterInfoHostOsDistro      = util.StructName("HostOsDistro")
+	ClusterInfoLonghornDistro    = util.StructName("LonghornDistro")
 
 	ClusterInfoLonghornImageRegistry = util.StructName("LonghornImageRegistry")
 
@@ -1979,6 +1984,8 @@ func (info *ClusterInfo) collectBackupTargetInfo() error {
 }
 
 func (info *ClusterInfo) collectNodeScope() {
+	info.collectLonghornDistro()
+
 	if err := info.collectHostArch(); err != nil {
 		info.logger.WithError(err).Warn("Failed to collect host architecture")
 	}
@@ -1998,6 +2005,15 @@ func (info *ClusterInfo) collectNodeScope() {
 	if err := info.collectKubernetesNodeProvider(); err != nil {
 		info.logger.WithError(err).Warn("Failed to collect node provider")
 	}
+}
+
+func (info *ClusterInfo) collectLonghornDistro() {
+	distro := strings.TrimSpace(os.Getenv(types.EnvDistro))
+	if distro == "" {
+		distro = DefaultEnvDistroUnknown
+	}
+
+	info.structFields.tags.Append(ClusterInfoLonghornDistro, strings.ToLower(distro))
 }
 
 func (info *ClusterInfo) collectHostArch() error {
