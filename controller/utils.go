0593aacd7cff08b1708db89297f2704a36b8db62
David Cheng <david.cheng@suse.com>
2026-05-19 13:58:29 +0800

    fix: allow live engine upgrade for same-commit revisioned engine images
    Signed-off-by: David Cheng <david.cheng@suse.com>
    (cherry picked from commit a6b529e1914e58953e49edb3d9d6a46f8477bdd2)
    
    # Conflicts:
    #	controller/engine_controller_test.go
    #	controller/utils.go

--- controller/utils.go
+++ controller/utils.go
@@ -2,6 +2,12 @@ package controller
 
 import (
 	"context"
+<<<<<<< HEAD
+=======
+	"regexp"
+	"strconv"
+	"strings"
+>>>>>>> a6b529e1 (fix: allow live engine upgrade for same-commit revisioned engine images)
 	"time"
 
 	"github.com/cockroachdb/errors"
@@ -25,6 +31,13 @@ const (
 	podRecreateInitBackoff = 1 * time.Second
 	podRecreateMaxBackoff  = 120 * time.Second
 	backoffGCPeriod        = 12 * time.Hour
+
+	// Matches revisioned engine image tags such as 1.10.2-4.12 or 1.10.2-4.20.
+	engineImageRevisionTagPattern = `.+-\d+\.\d+$`
+)
+
+var (
+	engineImageRevisionTagRegex = regexp.MustCompile(engineImageRevisionTagPattern)
 )
 
 // newBackoff returns a flowcontrol.Backoff and starts a background GC loop.
@@ -216,3 +229,54 @@ func enqueueAfterDelay(queue workqueue.TypedRateLimitingInterface[any], obj inte
 	queue.AddAfter(key, delay)
 	return nil
 }
+<<<<<<< HEAD
+=======
+
+// getAwsIAMRoleArnFromSecret retrieves the AWS IAM Role ARN from the specified secret in the given namespace.
+// returns the AWS IAM Role ARN string.
+func getAwsIAMRoleArnFromSecret(ds *datastore.DataStore, namespace, secretName string) (string, error) {
+	secret, err := ds.GetSecretRO(namespace, secretName)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return "", nil
+		}
+		return "", err
+	}
+
+	if secret == nil || secret.Data == nil {
+		return "", nil
+	}
+
+	if arn, ok := secret.Data[types.AWSIAMRoleArn]; ok {
+		return string(arn), nil
+	}
+	// Key not found; clear the annotation if needed.
+	return "", nil
+}
+
+func getCorrectedEncryptedVolumeSize(volumeSizeStr string, labels map[string]string) (string, error) {
+	if encrypted, exists := labels[types.LonghornLabelVolumeEncrypted]; exists && encrypted == types.LonghornLabelValueEnabled {
+		volumeSize, err := strconv.ParseInt(volumeSizeStr, 10, 64)
+		if err != nil {
+			return "", errors.Wrapf(err, "failed to convert volume size: %v", volumeSizeStr)
+		}
+		correctedSize := volumeSize - lhtypes.Luks2EncryptionHeaderSize
+		if correctedSize < 0 {
+			return "", errors.Errorf("corrected volume size is negative: %d", correctedSize)
+		}
+		return strconv.FormatInt(correctedSize, 10), nil
+	}
+	return volumeSizeStr, nil
+}
+
+func isRevisionedEngineImage(image string) bool {
+	lastSlashIndex := strings.LastIndex(image, "/")
+	lastColonIndex := strings.LastIndex(image, ":")
+	if lastColonIndex <= lastSlashIndex {
+		return false
+	}
+
+	tag := image[lastColonIndex+1:]
+	return engineImageRevisionTagRegex.MatchString(tag)
+}
+>>>>>>> a6b529e1 (fix: allow live engine upgrade for same-commit revisioned engine images)
