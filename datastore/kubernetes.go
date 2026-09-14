fc6a5144f28f3adab8de72b311ba19a6cf730e22
David Cheng <david.cheng@suse.com>
2026-09-14 08:51:24 +0800

    feat(datastore): add instance manager upgrade support
    - Add datastore accessors for instance manager upgrades and their rolling-upgrade control.
    - Add data-engine-specific live-upgrade settings and validation helpers.
    - Mark unsupported source versions so v1.12.1 offline upgrades remain allowed
      while live upgrade is rejected.
    
    Longhorn: 9104
    
    Signed-off-by: David Cheng <david.cheng@suse.com>
    (cherry picked from commit ef5de4931a3cbcf62f6e218b6c9c3974eb588277)

--- datastore/kubernetes.go
+++ datastore/kubernetes.go
@@ -27,6 +27,7 @@ import (
 	storagev1 "k8s.io/api/storage/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	patchtypes "k8s.io/apimachinery/pkg/types"
 
 	"github.com/longhorn/longhorn-manager/types"
 
@@ -218,6 +219,11 @@ func (s *DataStore) GetPDBRO(name string) (*policyv1.PodDisruptionBudget, error)
 	return s.podDisruptionBudgetLister.PodDisruptionBudgets(s.namespace).Get(name)
 }
 
+// UpdatePDB updates a PodDisruptionBudget in the Longhorn namespace.
+func (s *DataStore) UpdatePDB(obj *policyv1.PodDisruptionBudget) (*policyv1.PodDisruptionBudget, error) {
+	return s.kubeClient.PolicyV1().PodDisruptionBudgets(s.namespace).Update(context.TODO(), obj, metav1.UpdateOptions{})
+}
+
 // ListPDBsRO gets a map of PDB in s.namespace
 // This function returns direct reference to the internal cache objects and should not be mutated.
 // Consider using this function when you can guarantee read only access and don't want the overhead of deep copies
@@ -247,6 +253,11 @@ func (s *DataStore) DeletePod(name string) error {
 // UpdatePod updates Pod for the given Pod object and namespace
 func (s *DataStore) UpdatePod(obj *corev1.Pod) (*corev1.Pod, error) {
 	return s.kubeClient.CoreV1().Pods(s.namespace).Update(context.TODO(), obj, metav1.UpdateOptions{})
+}
+
+// PatchPod patches Pod for the given name and namespace.
+func (s *DataStore) PatchPod(name string, patch []byte) (*corev1.Pod, error) {
+	return s.kubeClient.CoreV1().Pods(s.namespace).Patch(context.TODO(), name, patchtypes.StrategicMergePatchType, patch, metav1.PatchOptions{})
 }
 
 // CreateLease creates a Lease resource for the given CreateLease object
