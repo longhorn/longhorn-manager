ce5ada61e1fd649b3b3a80aff002eb483ed1141f
Raphanus Lo <yunchang.lo@suse.com>
2026-09-16 19:03:20 +0800

    fix(csi): use a dedicated controller service account
    longhorn-14020
    
    Deploy the CSI attacher, provisioner, resizer, and snapshotter with the
    dedicated controller identity instead of inheriting manager permissions.
    Keep the node plugin and driver deployer on their existing identities.
    
    Signed-off-by: Raphanus Lo <yunchang.lo@suse.com>

--- types/deploy.go
+++ types/deploy.go
@@ -27,6 +27,8 @@ const (
 	CSIResizerName     = "csi-resizer"
 	CSISnapshotterName = "csi-snapshotter"
 	CSIPluginName      = "longhorn-csi-plugin"
+
+	CSIControllerServiceAccountName = "longhorn-csi-service-account"
 )
 
 // AddGoCoverDirToPod adds GOCOVERDIR env and host path volume to a pod.
