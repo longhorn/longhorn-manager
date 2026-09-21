8bb4b300131fb0a3fa782e71c725a1ed3d61be17
Derek Su <derek.su@suse.com>
2026-09-21 16:43:31 +0000

    fix(restore): retry restore on backupstore lock conflict
    A backup deletion or retention lock only blocks a restore for the lock
    duration, but isReplicaRestoreFailedLockError only matched the pre-rework
    "failed lock" message, so the current backupstore conflict was recorded as a
    restore error. That failed every restoring replica and left the DR volume
    Faulted instead of retrying.
    
    Longhron 13685
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- controller/engine_controller.go
+++ controller/engine_controller.go
@@ -31,6 +31,8 @@ import (
 	clientset "k8s.io/client-go/kubernetes"
 	v1core "k8s.io/client-go/kubernetes/typed/core/v1"
 
+	"github.com/longhorn/backupstore"
+
 	lhtypes "github.com/longhorn/go-common-libs/types"
 	etypes "github.com/longhorn/longhorn-engine/pkg/types"
 	imapi "github.com/longhorn/longhorn-instance-manager/pkg/api"
@@ -2017,8 +2019,12 @@ func handleRestoreError(log logrus.FieldLogger, engine *longhorn.Engine, rsMap m
 }
 
 func isReplicaRestoreFailedLockError(err *imclient.ReplicaError) bool {
-	failedLock := regexp.MustCompile(restoreGetLockFailedPatternMsg)
-	return failedLock.MatchString(err.Error())
+	if backupstore.IsLockConflictError(err) {
+		return true
+	}
+
+	// Engines predating the backupstore lock error rework report the conflict this way.
+	return regexp.MustCompile(restoreGetLockFailedPatternMsg).MatchString(err.Error())
 }
 
 func handleRestoreErrorForCompatibleEngine(log logrus.FieldLogger, engine *longhorn.Engine, rsMap map[string]*longhorn.RestoreStatus, backoff *flowcontrol.Backoff, err error) error {
